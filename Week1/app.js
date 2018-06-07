ce690754f6e3e7322abc3c08a4c055804c507986
Meazer <meazer.m@gmail.com>
2018-06-07 21:31:36 +0200

    update homework week1

--- Week1/app.js
+++ Week1/app.js
@@ -4,109 +4,134 @@ function main() {
 
     const url = 'https://api.github.com/orgs/HackYourFuture/repos?per_page=100';
 
+    // initial page: create header, info and contributors sections.
     const root = document.getElementById('root');
     const header = createAndAppend('header', root);
     let h2 = createAndAppend('h2', header);
     h2.innerText = 'HYF Repositories';
-    const infoSection = createAndAppend('section', root);
-    infoSection.id = 'info-section';
-    const contributorsSection = createAndAppend('section', root);
-    contributorsSection.id = 'contributors-section';
+    const infoSection = createAndAppend('section', root, 'id', 'info-section');
+    const contributorsSection = createAndAppend('section', root, 'id', 'contributors-section');
     h2 = createAndAppend('h2', contributorsSection);
     h2.innerText = 'Contributions';
-    const div = createAndAppend('div', contributorsSection);
-    div.id = 'contributions-container';
-    const table = createAndAppend('table', infoSection);
-    createRow(0, 'Repository:');
-    createRow(1, 'Description:');
-    createRow(2, 'Forks:');
-    createRow(3, 'Update:');
+    const div = createAndAppend('div', contributorsSection, 'id', 'contributions-container');
+    const table = createAndAppend('table', infoSection); // create info table 
+    createRows('Repository:', 'Description:', 'Forks:', 'Update:'); //create 4 rows * 2 cell
 
-    function createRow(num, name) {
-        let row = table.insertRow(num);
-        let cell1 = row.insertCell(0);
-        let cell2 = row.insertCell(1);
-        cell1.innerText = name;
-    }
+    fetchJSON(url, renderReposList);
 
-    fetchJSON(url, createReposList);
+    function createRows() {
 
-    function createInfo(rep) {
-
-        if (rep) {
-            document.querySelector('tr:nth-child(1) td:nth-child(2)').innerHTML =
-                '<a href=' + rep.html_url + ' target="_blank">' + rep.name + '</a>';
-            document.querySelector('tr:nth-child(2) td:nth-child(2)').innerText = rep.description;
-            document.querySelector('tr:nth-child(3) td:nth-child(2)').innerText = rep.forks;
-            document.querySelector('tr:nth-child(4) td:nth-child(2)').innerText = rep.updated_at.substring(0, 10);
-        } else {
-            for (let cell of document.querySelectorAll('td:nth-child(2)'))
-                cell.innerText = '';
-        }
-    }
-
-    function createContributions(contributions) {
-        const ele = document.getElementById('contributions-container');
-        while (ele.firstChild) {
-            ele.removeChild(ele.firstChild);
-        }
-        for (let cont of contributions) {
-            const div = createAndAppend('div', ele);
-            const a = createAndAppend('a', div);
-            a.href = cont.html_url;
-            a.target = '_blank';
-            const img = createAndAppend('img', a);
-            img.src = cont.avatar_url;
-            img.width = '50';
-            const name = createAndAppend('p', div);
-            name.innerText = cont.login;
-            const num = createAndAppend('span', name);
-            num.innerText = cont.contributions;
-            num.className = 'num';
+        for (let i = 0; i < arguments.length; i++) {
+            const row = table.insertRow(i);
+            const cell1 = row.insertCell(0);
+            row.insertCell(1);
+            cell1.innerText = arguments[i];
         }
 
-    }
-
-
-    function createReposList(reposObj) {
-
-        const selectList = createAndAppend('select', header);
-        selectList.id = 'selectList';
-        const selectOption = createAndAppend('option', selectList);
-        selectOption.innerText = 'Select a Repository';
-        selectOption.value = -1;
-        selectList.onchange = function () {
-            const value = document.getElementById('selectList').value;
-            createInfo(reposObj[value]);
-            console.log(reposObj[value].contributors_url);
-            fetchJSON(reposObj[value].contributors_url, createContributions);
-        }
-        for (let rep in reposObj) {
-            const selectOption = createAndAppend('option', selectList);
-            selectOption.innerText = reposObj[rep].name;
-            selectOption.value = rep;
-        }
-
-    }
-
-    function createAndAppend(tag, parent) {
-        const element = document.createElement(tag);
-        parent.appendChild(element);
-        return element;
     }
 
     function fetchJSON(url, callback) {
+        // create a XMLHttpRequest object to get a url and send it
         const XHR = new XMLHttpRequest();
         XHR.open('GET', url, true);
         XHR.send();
-        XHR.onreadystatechange = function (response) {
-            if (XHR.readyState == 4 && XHR.status == 200) {
-                const response = JSON.parse(XHR.responseText);
-                callback(response);
+        // when XHR state change call a function check if it DONE
+        XHR.onreadystatechange = () => {
+
+            if (XHR.readyState == 4) {
+
+                if (XHR.status < 400) { // call the passed function if the request is OK
+                    callback(JSON.parse(XHR.responseText));
+                } else {// if there is an error create error object and consol log error message 
+                    const error = new Error(xhr.statusText);
+                    console.error(error.message);
+                }
+
             }
         }
+
     }
 
+
+    function renderReposList(reposObj) {
+
+        const header = document.querySelector('header');
+        //create list and append it to header
+        const selectList = createAndAppend('select', header, 'id', 'selectList');
+        //default hidden option 'Select a Repository'
+        const selectOption = createAndAppend('option', selectList, 'selected', '', 'disabled', '');
+        selectOption.setAttribute('hidden', '');
+        selectOption.innerText = 'Select a Repository';
+
+        for (let rep in reposObj) {
+            // add an option for each repo with value that is his index at reopsObject 
+            const selectOption = createAndAppend('option', selectList, 'value', rep);
+            selectOption.innerText = reposObj[rep].name;
+
+        }
+        //add a listener when select a new option
+        selectList.onchange = () => {
+            //get the value of the selected option
+            const value = document.getElementById('selectList').value;
+            renderInfo(reposObj[value]);// call renderInfo and pass the selected repo to show his info
+            //call fetchJSON to fetch repo's contributors and pass renderContributions to show them
+            fetchJSON(reposObj[value].contributors_url, renderContributions);
+
+        }
+
+    }
+
+    function renderInfo(rep) {
+
+        // add repo data to the appropriate cells in info table.
+        const cell = document.querySelector('#info-section tr:nth-child(1) td:nth-child(2)');
+        cell.innerText = '';
+        const link = createAndAppend('a', cell, 'href', rep.html_url, 'target', '_blank');
+        link.innerText = rep.name;
+        document.querySelector('#info-section tr:nth-child(2) td:nth-child(2)').innerText = rep.description;
+        document.querySelector('#info-section tr:nth-child(3) td:nth-child(2)').innerText = rep.forks;
+        document.querySelector('#info-section tr:nth-child(4) td:nth-child(2)').innerText = rep.updated_at.substring(0, 10);
+
+    }
+
+    function renderContributions(contributions) {
+
+        const ele = document.getElementById('contributions-container');
+
+        while (ele.firstChild) { // empty contributions-container
+            ele.removeChild(ele.firstChild); // while contributions-container has a child delete it
+        }
+
+        for (let cont of contributions) {
+            // create a div for each contribution with it's data
+            const div = createAndAppend('div', ele);
+            const link = createAndAppend('a', div, 'href', cont.html_url, 'target', '_blank');
+            const img = createAndAppend('img', link, 'src', cont.avatar_url, 'alt', cont.login);
+            const name = createAndAppend('p', div);
+            name.innerText = cont.login;
+            const num = createAndAppend('span', name, 'class', 'num');
+            num.innerText = cont.contributions;
+
+        }
+
+    }
+
+    function createAndAppend(tag, parent, atr1, value1, atr2, value2) {
+
+        const element = document.createElement(tag);
+        //add attributes if it's passed
+        if (atr1) {
+            element.setAttribute(atr1, value1);
+        }
+
+        if (atr2) {
+            element.setAttribute(atr2, value2);
+        }
+
+        parent.appendChild(element);
+        return element;
+
+    }
 
 }
 
