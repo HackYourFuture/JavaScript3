0b7209d4c529655e945fb00032352f0e5158e142
Husam Lahm <37015049+ThreePalmTrees@users.noreply.github.com>
2018-06-16 23:04:35 +0200

    code refactoring

--- Week2/homeweork/app.js
+++ Week2/homeweork/app.js
@@ -2,16 +2,7 @@
 
 const root = document.getElementById("root");
 
-const token = "74549feb01fee4e294cadd88f2aff4f9e2eb2c02";
-const hyfGithubRepositories = `https://api.github.com/orgs/HackYourFuture/repos?access_token=${token}&per_page=100`;
-
-function renderOptionTag(repos) {
-  repos.map(repo => {
-    let optionTag = createAndAppend("option", selectTag);
-    optionTag.value = repo.name;
-    optionTag.textContent = repo.name;
-  });
-}
+const hyfGithubRepositories = `https://api.github.com/orgs/HackYourFuture/repos?per_page=100`;
 
 let header = createAndAppend("header", root);
 let selectTag = createAndAppend("select", header);
@@ -38,6 +29,16 @@ text.parentNode.insertBefore(text, repoDisplayName);
 text.id = "repositoryText";
 const description = createAndAppend("p", canvas);
 
+function createAndAppend(tag, parent) {
+  let newTag = document.createElement(tag);
+  if (parent) {
+    parent.appendChild(newTag);
+  } else {
+    document.body.appendChild(newTag);
+  }
+  return newTag;
+}
+
 function getData(apiEndPoint) {
   return new Promise((resolve, reject) => {
     const xhr = new XMLHttpRequest();
@@ -60,79 +61,81 @@ function getData(apiEndPoint) {
     xhr.send();
   });
 }
+function main() {
+  function renderOptionTags(repos) {
+    repos.map(repo => {
+      let optionTag = createAndAppend("option", selectTag);
+      optionTag.value = repo.name;
+      optionTag.textContent = repo.name;
+    });
+  }
 
-function renderHtmlElements() {
-  getData(hyfGithubRepositories).then(repositories => {
-    renderOptionTag(repositories);
-    selectTag.addEventListener("change", () => {
-      repositories.forEach(repository => {
-        infoContainer.id = "infoContainer";
-        if (ul.firstChild) {
-          ul.removeChild(ul.firstChild);
-        }
-
-        if (selectTag.value == repository.name) {
-          text.id = "shown";
-          repoDisplayName.href = repository.svn_url;
-          repoDisplayName.textContent = selectTag.value;
-          repoDisplayName.target = "_blank";
-          repoDisplayName.id = "repositoryDisplayName";
-          forks.textContent = `Forks: ${repository.forks}`;
-          lastUpdated.textContent = `Updated: ${repository.updated_at.replace(
-            /T.*Z/,
-            ``
-          )}`;
-
-          if (repository.description !== null) {
-            description.textContent = repository.description;
-            description.id = "description";
-            repoDisplayName.textContent = repository.name;
-            description.style.background = "#222";
-          } else {
-            description.textContent = "No description";
-            description.style.background = "#555";
+  function renderHtmlElements() {
+    getData(hyfGithubRepositories).then(repositories => {
+      renderOptionTags(repositories);
+      selectTag.addEventListener("change", () => {
+        repositories.forEach(repository => {
+          infoContainer.id = "infoContainer";
+          if (ul.firstChild) {
+            ul.removeChild(ul.firstChild);
           }
 
-          getData(repository.contributors_url).then(contributorsInformation => {
-            renderContributors(contributorsInformation);
-          });
-        }
+          if (selectTag.value == repository.name) {
+            text.id = "shown";
+            repoDisplayName.href = repository.svn_url;
+            repoDisplayName.textContent = selectTag.value;
+            repoDisplayName.target = "_blank";
+            repoDisplayName.id = "repositoryDisplayName";
+            forks.textContent = `Forks: ${repository.forks}`;
+            lastUpdated.textContent = `Updated: ${repository.updated_at.replace(
+              /T.*Z/,
+              ``
+            )}`;
+
+            if (repository.description !== null) {
+              description.textContent = repository.description;
+              description.id = "description";
+              repoDisplayName.textContent = repository.name;
+              description.style.background = "#222";
+            } else {
+              description.textContent = "No description";
+              description.style.background = "#555";
+            }
+
+            getData(repository.contributors_url).then(
+              contributorsInformation => {
+                renderContributors(contributorsInformation);
+              }
+            );
+          }
+        });
       });
     });
-  });
-}
-renderHtmlElements();
-
-function renderContributors(contributorsURL) {
-  contributorsURL.forEach(contributor => {
-    let li = document.createElement("li");
-    ul.appendChild(li);
-
-    let clickableImage = createAndAppend("a", li);
-    clickableImage.href = contributor.html_url;
-    clickableImage.target = "_blank";
-
-    let contributorImage = createAndAppend("img", clickableImage);
-    contributorImage.src = contributor.avatar_url;
-
-    let contributorName = createAndAppend("a", li);
-    contributorName.id = "contributorName";
-    contributorName.textContent = contributor.login;
-    contributorName.href = contributor.html_url;
-    contributorName.target = "_blank";
-
-    let contributionsCount = createAndAppend("p", li);
-    contributionsCount.id = "contributions";
-    contributionsCount.textContent = contributor.contributions;
-  });
-}
-
-function createAndAppend(tag, parent) {
-  let newTag = document.createElement(tag);
-  if (parent) {
-    parent.appendChild(newTag);
-  } else {
-    document.body.appendChild(newTag);
   }
-  return newTag;
+  renderHtmlElements();
+
+  function renderContributors(contributorsURL) {
+    contributorsURL.forEach(contributor => {
+      let li = document.createElement("li");
+      ul.appendChild(li);
+
+      let clickableImage = createAndAppend("a", li);
+      clickableImage.href = contributor.html_url;
+      clickableImage.target = "_blank";
+
+      let contributorImage = createAndAppend("img", clickableImage);
+      contributorImage.src = contributor.avatar_url;
+
+      let contributorName = createAndAppend("a", li);
+      contributorName.id = "contributorName";
+      contributorName.textContent = contributor.login;
+      contributorName.href = contributor.html_url;
+      contributorName.target = "_blank";
+
+      let contributionsCount = createAndAppend("p", li);
+      contributionsCount.id = "contributions";
+      contributionsCount.textContent = contributor.contributions;
+    });
+  }
 }
+main();
