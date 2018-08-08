c71cd2f5a5b76c9af2360d2065774dabf136e53e
Shadi Amr <35688557+shadoo77@users.noreply.github.com>
2018-08-08 13:29:34 +0200

    Shadi's homework Javascript3 - week1 (all finished)

--- homework/src/index.js
+++ homework/src/index.js
@@ -36,10 +36,118 @@
       if (err) {
         createAndAppend('div', root, { html: err.message, class: 'alert-error' });
       } else {
-        createAndAppend('pre', root, { html: JSON.stringify(data, null, 2) });
+        //createAndAppend('pre', root, { html: JSON.stringify(data, null, 2) });
+        const header = createAndAppend('header', root);
+        createAndAppend('p', header, {html: 'HYF Repositories', class: 'header-lable'});
+        const select = createAndAppend('select', header, { id: 'selectMenu' });
+        /* &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& */
+        data.sort((a, b) => a.name.localeCompare(b.name, 'fr', {ignorePunctuation: true}));
+        data.forEach((reopsitory, index) => {
+          createAndAppend('option', select, {html: reopsitory.name, value: index});
+        });
+        /* &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& */
+        const container = createAndAppend('div', root, { id: 'container'});
+        contentLeft(container, data, select);
+        contentRight(container, data, select);
       }
     });
   }
+
+
+
+
+
+  // function who creates a left content section
+  function contentLeft(container, data, select) {
+    const leftSection = createAndAppend('div', container, { id: 'leftSection', class: 'whiteframe' });
+    showTableRepo(leftSection, data, 0);
+
+    select.addEventListener('change', (event) => {
+      document.getElementById('leftSection').innerHTML = "";
+      const theEvent = event.target.value;
+      showTableRepo(leftSection, data, theEvent);
+    });
+  }
+
+  
+
+  // drow the table
+function showTableRepo(leftSection, data, dataIndex) {
+  // create table of the contents
+  const lTable = createAndAppend('table', leftSection);
+  // line one
+  const tr1 = createAndAppend('tr', lTable);
+  createAndAppend('td', tr1, {html: 'Repository :', class: 'lable'});
+  const td1 = createAndAppend('td', tr1);
+  let nameLink = '<a href="' + data[dataIndex].html_url + '" target="_blank">' + data[dataIndex].name + '</a>';
+  createAndAppend('p', td1, {html: nameLink });
+  // line two
+  const tr2 = createAndAppend('tr', lTable);
+  createAndAppend('td', tr2, {html: 'Description :', class: 'lable'});
+  const td2 = createAndAppend('td', tr2);
+  createAndAppend('p', td2, {html: data[dataIndex].description});
+  // line three
+  const tr3 = createAndAppend('tr', lTable);
+  createAndAppend('td', tr3, {html: 'Forks :', class: 'lable'});
+  const td3 = createAndAppend('td', tr3);
+  createAndAppend('p', td3, {html: data[dataIndex].forks});
+  // line four
+  const tr4 = createAndAppend('tr', lTable);
+  createAndAppend('td', tr4, {html: 'Updated :', class: 'lable'});
+  const td4 = createAndAppend('td', tr4);
+  const defaultElDate = data[dataIndex].updated_at;
+  const defaultDate = new Date(defaultElDate);
+  createAndAppend('p', td4, {html: defaultDate});
+}
+
+
+
+  // function who creates a right content section
+  function contentRight(container, data, select) {
+    const rightSection = createAndAppend('div', container, { id: 'rightSection', class: 'whiteframe' });
+    const ContributionsLable = createAndAppend('h5', rightSection, {class: 'ContributionsLable'});
+    ContributionsLable.innerHTML = "Contributions :";
+    let contrubutorsURL = data[0].contributors_url;
+    fetchJSON(contrubutorsURL, (err, subData) => {
+      if (err) {
+        createAndAppend('div', rightSection, { html: err.message, class: 'alert-error' });
+      } else {
+        showContributors(rightSection, subData);
+      }
+    });
+    select.addEventListener('change', (event) => {
+      document.getElementById('rightSection').innerHTML = "";
+      ContributionsLable.innerHTML = "Contributions :";
+      const theEvent = event.target.value;
+      let contrubutorsURL = data[theEvent].contributors_url;
+      fetchJSON(contrubutorsURL, (err, subData) => {
+        if (err) {
+          createAndAppend('div', rightSection, { html: err.message, class: 'alert-error' });
+        } else {
+          showContributors(rightSection, subData);
+        }
+      });
+    });
+  }
+
+
+
+
+/*  ************************************ */
+function showContributors(rightSection, subData) {
+  subData.forEach(el => {
+    const contrSection = createAndAppend('section', rightSection);
+    const contrMainLink = createAndAppend('a', contrSection);
+    contrMainLink.setAttribute('href', el.html_url);
+    const contrAvater = createAndAppend('img', contrMainLink, {class: 'avater'});
+    contrAvater.setAttribute('src', el.avatar_url);
+    const contrDataDiv = createAndAppend('div', contrMainLink, {class: 'contrData'});
+    createAndAppend('div', contrDataDiv, {html: el.login});
+    createAndAppend('div', contrDataDiv, {html: el.contributions, class: 'contributor-badge'});
+  });
+}
+
+
 
   const HYF_REPOS_URL = 'https://api.github.com/orgs/HackYourFuture/repos?per_page=100';
 
