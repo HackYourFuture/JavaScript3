3af302ee300ce17043d4c9eefbe2b2fd4338189f
acimanx <acimanx@gmail.com>
2018-08-19 15:08:04 +0200

    fixed

--- homework/src/index.js
+++ homework/src/index.js
@@ -1,77 +1,119 @@
-
-"use strict"
+"use strict";
 {
-  (function App() {
-    document.getElementById("root").innerHTML = `
-    <header><div class=selectfield><select id=selectmenu></select></div></header>
-    <main>
-    <section class=left>
-    <div class=scy id=info-box>
-    <div class=description>"No description"</div>
-    <div class=forks>"No forks"</div>
-    <div class=updated>"No data"</div>
-    </div>
-    <div id=size style=width:0></div>
-    </section>
-    <section class=right>
-    <div id=contributors><div class=card>
-    <a href="#">
-    <figure><img src="https://avatars1.githubusercontent.com/u/39067270?s=400&u=b8a97121f8e8623492a3fd16de13cfa780409246&v=4"></figure>
-    </a>
-    <h2>Acimanx</h2>
-    <p>contributions:53</p><div class=repos-count>123</div>
-    </div>
-    </div>
-    </section>
-    </main>`
-  })()
-
-  document.addEventListener("DOMContentLoaded", function (event) {
-
-    const url = "https://api.github.com/orgs/HackYourFuture/repos?per_page=10"
-
-    fetchJSON(url, function (err, data) {
-      if (err) {
-        document.getElementById("main").innerHTML = err
+  function main() {
+    fetchJSON("https://api.github.com/orgs/HackYourFuture/repos", (err, data) => {
+      function rend(changeEvent) {
+        main.innerHTML = "";
+        let sectL = crnap("section", main, { class: "left", id: "maininfo" });
+        const sectR = crnap("section", main, { class: "right", id: "contribInfo" });
+        let sectLinfobox = crnap("div", sectL, { id: "info-box", class: "scy" });
+        sectL = crnap("div", sectLinfobox, {
+          html:
+            null == changeEvent.full_name
+              ? "No name"
+              : `Repo name: <a href="${changeEvent.html_url}">${changeEvent.full_name}</a>`,
+          class: "name"
+        });
+        crnap("div", sectLinfobox, {
+          html:
+            null == changeEvent.description
+              ? "No description"
+              : `Description: ${changeEvent.description}`,
+          class: "description"
+        });
+        crnap("div", sectLinfobox, {
+          html: null == changeEvent.forks ? "No forks" : `Forks: ${changeEvent.forks}`,
+          class: "forks"
+        });
+        crnap("div", sectLinfobox, {
+          html:
+            null == changeEvent.updated_at
+              ? "Update is unknown"
+              : `Updated: ${new Date(changeEvent.updated_at).toLocaleString()}`,
+          class: "updated"
+        });
+        sectLinfobox = crnap("div", sectLinfobox, {
+          html: null == changeEvent.size ? "No size" : `Size: ${changeEvent.size}`,
+          class: "size"
+        });
+        fetchJSON(changeEvent.contributors_url, (err, contributors) => {
+          let fetchToDo = contributors.length;
+          contributors.forEach(contributor => {
+            fetchJSON(contributor.url, (err, user) => {
+              Object.assign(contributor, user);
+              --fetchToDo;
+              0 === fetchToDo &&
+                (console.log(contributors),
+                  contributors.forEach(contrUser => {
+                    let sectRcontrs = crnap("div", sectR, { id: "contributors" });
+                    sectRcontrs = crnap("div", sectRcontrs, { class: "card" });
+                    let secRimglink = crnap("a", sectRcontrs, { href: contrUser.html_url });
+                    secRimglink = crnap("figure", secRimglink, {});
+                    crnap("img", secRimglink, { src: contrUser.avatar_url });
+                    crnap("h2", sectRcontrs, { html: null == contrUser.name ? "" : `name: ${contrUser.name}` });
+                    crnap("p", sectRcontrs, { html: null == contrUser.login ? "" : `nickname: ${contrUser.login}` });
+                    crnap("p", sectRcontrs, {
+                      html: null == contrUser.location ? "" : `location: ${contrUser.location}`
+                    });
+                    crnap("p", sectRcontrs, {
+                      html: null == contrUser.followers ? "" : `followers: ${contrUser.followers}`
+                    });
+                    crnap("p", sectRcontrs, {
+                      html: null == contrUser.company ? "" : `company: ${contrUser.company}`
+                    });
+                    crnap("p", sectRcontrs, {
+                      html: null == contrUser.contributions ? "" : `contributions: ${contrUser.contributions}`
+                    });
+                    crnap("p", sectRcontrs, {
+                      html:
+                        null == contrUser.updated_at
+                          ? ""
+                          : `last github visit: ${new Date(contrUser.updated_at).toLocaleString()}`
+                    });
+                  }));
+            });
+          });
+        });
       }
-      else {
-        document.getElementById("selectmenu").innerHTML = `<option value=""disabled id=first selected>Choose your option</option>${data.map((x, b) => `<option value=${b} class=select>${x.name}</option>`).join("")}`
-        document.getElementById("selectmenu").addEventListener("change", event => {
-          document.getElementById("info-box").innerHTML = `<div class=description>${null == data[event.target.value].description ? "No description" : data[event.target.value].description}</div><div class=forks>${null == data[event.target.value].forks ? "No forks" : data[event.target.value].forks}</div><div class=updated>${null == data[event.target.value].updated_at ? "No data" : data[event.target.value].updated_at}</div>`
-          let repositories
-          let repoData
-          fetchJSON(data[event.target.value].contributors_url, (err, contributorData) => {
-            repositories = contributorData
-            repoData = repositories.forEach(({ login }) => {
-              let usersData
-              fetchJSON(`https://api.github.com/users/${login}`, (err, fetchUsersData) => {
-                usersData = fetchUsersData.name
-                console.log(usersData)
-              })
-            })
-            console.log(repoData)
-            outputPageContent(repoData)
-          })
-          function outputPageContent() {
-            document.getElementById("contributors").innerHTML = repositories.map(a => `<div class=card><a href=${a.html_url} target=_blank><figure><img src=${a.avatar_url}></figure></a><h2>${a.login}</h2><p id=reposdata>${console.log(repoData)}<p>contributions: ${a.contributions}<div class=repos-count></div></div>`).join("")
-          }
-        }, !1)
-      }
-    })
-  })
-
-  function fetchJSON(url, cb) {
-    const xhr = new XMLHttpRequest()
-    xhr.open('GET', url)
-    xhr.responseType = 'json'
-    xhr.onload = () => {
-      if (xhr.status < 400) {
-        cb(null, xhr.response)
-      } else {
-        cb(new Error(`Network error: ${xhr.status} - ${xhr.statusText}`))
-      }
-    }
-    xhr.onerror = () => cb(new Error('Network request failed'))
-    xhr.send()
+      const root = document.getElementById("root");
+      let header = crnap("header", root, {});
+      header = crnap("div", header, { class: "selectfield" });
+      const menu = crnap("select", header, { id: "selectmenu" });
+      var main = crnap("main", root, { id: "main" });
+      err
+        ? crnap("div", root, { a: err.message, b: "alert-error" })
+        : (data
+          .sort((b, a) => b.name.localeCompare(a.name))
+          .forEach((a, d) =>
+            crnap("option", menu, { value: d, b: "select", html: a.name })
+          ),
+          menu.addEventListener("change", event => {
+            event = event.target.value;
+            rend(data[event]);
+          }));
+    });
   }
+  function crnap(name, prnt, opts) {
+    opts = void 0 === opts ? {} : opts;
+    const elem = document.createElement(name);
+    prnt.appendChild(elem);
+    Object.keys(opts).forEach(key => {
+      const val = opts[key];
+      "html" === key ? (elem.innerHTML = val) : elem.setAttribute(key, val);
+    });
+    return elem;
+  }
+  function fetchJSON(url, cb) {
+    const xhr = new XMLHttpRequest();
+    xhr.open("GET", url);
+    xhr.responseType = "json";
+    xhr.onload = () => {
+      400 > xhr.status
+        ? cb(null, xhr.response)
+        : cb(Error(`Network error: ${xhr.status} - ${xhr.statusText}`));
+    };
+    xhr.onerror = () => cb(Error("Network request failed"));
+    xhr.send();
+  }
+  window.onload = () => main();
 }
