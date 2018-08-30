2a037d367fc2566a72ef68a61c17872594b95f4c
salar <salar.demok@gmail.com>
2018-08-30 14:59:49 +0200

    JS WEEk1

--- homework/src/index.js
+++ homework/src/index.js
@@ -1,10 +1,10 @@
-'use strict';
+"use strict";
 
 {
   function fetchJSON(url, cb) {
     const xhr = new XMLHttpRequest();
-    xhr.open('GET', url);
-    xhr.responseType = 'json';
+    xhr.open("GET", url);
+    xhr.responseType = "json";
     xhr.onload = () => {
       if (xhr.status < 400) {
         cb(null, xhr.response);
@@ -12,16 +12,16 @@
         cb(new Error(`Network error: ${xhr.status} - ${xhr.statusText}`));
       }
     };
-    xhr.onerror = () => cb(new Error('Network request failed'));
+    xhr.onerror = () => cb(new Error("Network request failed"));
     xhr.send();
   }
 
   function createAndAppend(name, parent, options = {}) {
     const elem = document.createElement(name);
     parent.appendChild(elem);
-    Object.keys(options).forEach((key) => {
+    Object.keys(options).forEach(key => {
       const value = options[key];
-      if (key === 'html') {
+      if (key === "text") {
         elem.innerHTML = value;
       } else {
         elem.setAttribute(key, value);
@@ -31,17 +31,66 @@
   }
 
   function main(url) {
-    fetchJSON(url, (err, data) => {
-      const root = document.getElementById('root');
-      if (err) {
-        createAndAppend('div', root, { html: err.message, class: 'alert-error' });
+    const root = document.getElementById("root");
+    const div = createAndAppend("div", root, { class: "container" });
+    const header = createAndAppend("p", div, {
+      text: "HYF Repositories",
+      class: "header"
+    });
+    const table = createAndAppend("table", root, { class: "table" });
+    const select = createAndAppend("select", header, { class: "select" });
+
+    const tbody = createAndAppend("tbody", table);
+    const tr = createAndAppend("tr", tbody);
+
+    createAndAppend("td", tr, tbody, { text: "Repository: ", class: "label" });
+    const repository = createAndAppend("a", tbody);
+    const description = createAndAppend("tr", tbody);
+    const forks = createAndAppend("tr", tbody, { class: "label" });
+
+    const updated = createAndAppend("tr", tbody, { class: "label" });
+
+    fetchJSON(url, (error, repositories) => {
+      if (error) {
+        createAndAppend("div", root, {
+          text: error.message,
+          class: "alert-error"
+        });
       } else {
-        createAndAppend('pre', root, { html: JSON.stringify(data, null, 2) });
+        repositories.sort((a, b) => a.name.localeCompare(b.name));
       }
+
+      repositories.forEach((element, index) => {
+        createAndAppend("option", select, {
+          text: element.name,
+          value: index
+        });
+      });
+
+      function repositoryBox() {
+        const index = select.value;
+        repository.innerText = select[index].textContent;
+        repository.setAttribute("href", repositories[index].html_url);
+        repository.setAttribute("target", "_blank");
+
+        forks.innerHTML = "Forks: " + repositories[index].forks;
+        description.innerHTML =
+          "Description: " + repositories[index].description;
+        updated.innerHTML =
+          "Updated: " +
+          new Date(repositories[index].updated_at).toLocaleString();
+      }
+
+      repositoryBox();
+
+      select.addEventListener("change", event => {
+        repositoryBox();
+      });
     });
   }
 
-  const HYF_REPOS_URL = 'https://api.github.com/orgs/HackYourFuture/repos?per_page=100';
+  const HYF_REPOS_URL =
+    "https://api.github.com/orgs/HackYourFuture/repos?per_page=100";
 
   window.onload = () => main(HYF_REPOS_URL);
 }
