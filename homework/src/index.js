a18e9b0e5fc44e18401ebf38b9ae6a4edeb276e3
Karam <ra4mos@yahoo.com>
2018-10-18 20:52:20 +0200

    js3 week1

--- homework/src/index.js
+++ homework/src/index.js
@@ -36,8 +36,63 @@
       if (err) {
         createAndAppend('div', root, { html: err.message, class: 'alert-error' });
       } else {
-        createAndAppend('pre', root, { html: JSON.stringify(data, null, 2) });
+        const topDiv = createAndAppend('div', root, { class: 'topDiv' });
+        const header = createAndAppend('header', topDiv, { class: 'heading' });
+        createAndAppend('h1', header, { html: 'HYF Repositories ', class: 'h1-text' });
+        const selectPanel = createAndAppend('select', header, { class: 'select-panel' });
+        data.sort(function (a, b) { return a.name.localeCompare(b.name); });
+        for (let i = 0; i < data.length; i++) {
+          createAndAppend('option', selectPanel, { value: i, html: data[i].name });
+        }
+        const LeftDiv = createAndAppend('div', root, { class: 'leftDiv' });
+        const rightDiv = createAndAppend('div', root, { class: 'rightDiv' });
+        selectPanel.addEventListener('change', () => {
+          const value = data[selectPanel.selectedIndex].url;
+          fetchJSON(value, (err, data) => {
+            renderReposInfo(LeftDiv, data, value);
+          });
+          const contributors = data[selectPanel.selectedIndex].contributors_url;
+          fetchJSON(contributors, (err, data) => {
+            renderContributorsInfo(rightDiv, data, contributors);
+          });
+        });
+        fetchJSON(data[0].url, (err, data) => {
+          renderReposInfo(LeftDiv, data);
+        });
+        fetchJSON(data[0].contributors_url, (err, data) => {
+          renderContributorsInfo(rightDiv, data);
+        });
       }
+    });
+  }
+
+  function addRow(label, value, tbody) {
+    const row = createAndAppend('tr', tbody);
+    createAndAppend('td', row, { html: label + ':  ', class: 'label' });
+    createAndAppend('td', row, { html: value, class: 'repo-name', target: '_blank' });
+  }
+
+  function renderReposInfo(leftDiv, data) {
+    leftDiv.innerHTML = '';
+    const table = createAndAppend('table', leftDiv, { class: 'table-item' });
+    const tbody = createAndAppend('tbody', table);
+    addRow('Repository', data.name.link(data.html_url), tbody);
+    addRow('Description', data.description, tbody);
+    addRow('Forks', data.forks, tbody);
+    addRow('Updated', data.updated_at, tbody);
+  }
+
+  function renderContributorsInfo(rightDiv, data) {
+    rightDiv.innerHTML = '';
+    const rightTable = createAndAppend('div', rightDiv, { class: 'right-table' });
+    createAndAppend('p', rightTable, { html: 'Contributions', class: 'cont-title' });
+    const ul = createAndAppend('ul', rightTable, { class: 'contributors-list' });
+    data.forEach(contributor => {
+      const contributorLink = createAndAppend('a', ul, { href: (`${contributor.html_url}`), target: '_blank', class: 'c-link' });
+      const li = createAndAppend('li', contributorLink, { class: 'list-item' });
+      createAndAppend('img', li, { src: contributor.avatar_url, class: 'avatar' });
+      createAndAppend('div', li, { html: contributor.login, class: 'name-div' });
+      createAndAppend('div', li, { html: contributor.contributions, class: 'badge-number-div' });
     });
   }
 
