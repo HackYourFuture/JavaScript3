e81ebb15c61bd8c9e20e12a4c545c09ea5ebae1c
RimSebai <40184200+RimSebai@users.noreply.github.com>
2018-10-21 20:19:18 +0200

    Update index.js
    I did use the new extensions to improve my code ..I am working in the contributors part

--- homework/src/index.js
+++ homework/src/index.js
@@ -1,22 +1,7 @@
 'use strict';
 
 {
-  function fetchJSON(url, cb) {
-    const xhr = new XMLHttpRequest();
-    xhr.open('GET', url);
-    xhr.responseType = 'json';
-    xhr.onload = () => {
-      if (xhr.status < 400) {
-        cb(null, xhr.response);
-      } else {
-        cb(new Error(`Network error: ${xhr.status} - ${xhr.statusText}`));
-      }
-    };
-    xhr.onerror = () => cb(new Error('Network request failed'));
-    xhr.send();
-  }
-
-  function createAndAppend(name, parent, options = {}) {
+function createAndAppend(name, parent, options = {}) {
     const elem = document.createElement(name);
     parent.appendChild(elem);
     Object.keys(options).forEach((key) => {
@@ -26,58 +11,53 @@
       } else {
         elem.setAttribute(key, value);
       }
-
     });
     return elem;
   }
-
-  // 
- function repo(data){
-  data.sort((a,b) =>  a.name.localeCompare(b.name));
-  const div = createAndAppend ("div",root,{id:"mainCon"});
-  const p = createAndAppend("p",div,{text:"HYF  Repositories"})
-  const select = createAndAppend("select", div );
- 
-  for (let i = 0; i < data.length ; i++){
-  createAndAppend ("option", select,{text:data[i].name ,value:i});
+  function repo(data) {
+    data.sort((a, b) => a.name.localeCompare(b.name));
+    const root = document.getElementById('root');
+    const div = createAndAppend("div", root, { id: "mainCon" });
+    createAndAppend("p", div, { text: "HYF  Repositories" });
+    const select = createAndAppend("select", div);
+    for (let i = 0; i < data.length; i++) {
+      createAndAppend("option", select, { text: data[i].name, value: i });
+    }
+    select.addEventListener("change", () => repoInfo(data, select));
+    repoInfo(data, select) + contributors(data, select);
   }
-  select.addEventListener("change", () => repoInfo(data,select));
-  repoInfo(data,select) + conterpeuters(data,select);
- }
-
-
-function repoInfo(data,select ){
-  if (document.getElementById("info")) {
-    document.getElementById("info").remove();
+  function repoInfo(data, select) {
+    if (document.getElementById("info")) {
+      document.getElementById("info").remove();
+    }
+    const link = data[select.value].html_url;
+    const name = data[select.value].name;
+    const description = data[select.value].description;
+    const forks = data[select.value].forks;
+    const newDate = new Date(data[select.value].updated_at);
+    const root = document.getElementById('root');
+    const div = createAndAppend("div", root, { id: "info" });
+    const ul = createAndAppend("ul", div);
+    const href = createAndAppend("li", ul, { text: ": ", class: "repository_details" });
+    createAndAppend("a", href, { text: name, href: link });
+    createAndAppend("li", ul, { text: "Description: " + description, class: "repository_details" });
+    createAndAppend("li", ul, { text: "Forks: " + forks, class: "repository_details" });
+    createAndAppend("li", ul, { text: "Updated: " + newDate.toLocaleString('en-GB', { timeZone: 'UTC' }), class: "repository_details" });
   }
-  const link = data[select.value].html_url;
-  const name = data[select.value].name;
-  const description = data[select.value].description;
-  const forks = data[select.value].forks;
-  const newDate =  new Date (data[select.value].updated_at);
-  const div = createAndAppend ("div" , root, {id:"info"});
-  const ul = createAndAppend("ul", div);
-  const href = createAndAppend ("li", ul,{text:"repository: ", class:"repodetails"});
-  const a = createAndAppend("a", href ,{text:name ,href:link});
-  createAndAppend ("li", ul,{text:"Descrition: " + description,class:"repodetails"});
-  createAndAppend ("li", ul,{text:"Forks: " + forks,class:"repodetails"});
-  createAndAppend ("li", ul,{text:"Updated: " + newDate.toLocaleString('en-GB', { timeZone: 'UTC' }),class:"repodetails"});
-}
+  function contributors(data, select) {
+    if (document.getElementById("contributors")) {
+      document.getElementById("contributors").remove();
+    }
+    const root = document.getElementById('root');
+    const div = createAndAppend("div", root, { id: "contributors" });
+    const ul = createAndAppend("ul", div);
+    const x = data[select.value].contributors_url;
+  }
 
-function conterpeuters (data,select){ 
-if (document.getElementById("contributors")) {
-  document.getElementById("contributors").remove();
-}
-
-const div = createAndAppend ("div" , root, {id:"contributors"});
-const ul = createAndAppend("ul", div);
-const x = data[select.value].contributors_url;
-
-}
+  function renderError(error) {
+    console.log(error);
+  }
   
-
-
-
   function main(url) {
     fetchJSON(url, (err, data) => {
       const root = document.getElementById('root');
