41903c2fc417d9d550252a513bdc1a46362f206d
joudi6 <joudi.marjana2@gmail.com>
2018-10-25 16:50:07 +0200

    pre virsion of JS3 W1 H.W

--- homework/src/index.js
+++ homework/src/index.js
@@ -30,18 +30,74 @@
     return elem;
   }
 
+  function renderRepository(repository, container) {
+    container.innerHTML = '';
+    const table = createAndAppend('table', container);
+    const tbody = createAndAppend('tbody', table);
+    const tr1 = createAndAppend('tr', tbody);
+    createAndAppend('td', tr1, {
+      text: 'Repository:'
+    });
+    const td2 = createAndAppend('td', tr1);
+    createAndAppend('a', td2, {
+      href: repository.html_url,
+      target: '_blank',
+      text: repository.name
+    });
+    const tr2 = createAndAppend('tr', tbody);
+    createAndAppend('td', tr2, {
+      text: 'Description:'
+    });
+    createAndAppend('td', tr2, {
+      text: repository.description
+    });
+    const tr3 = createAndAppend('tr', tbody);
+    createAndAppend('td', tr3, {
+      text: 'Forks:'
+    });
+    createAndAppend('td', tr3, {
+      text: repository.forks
+    });
+    const tr4 = createAndAppend('tr', tbody);
+    createAndAppend('td', tr4, {
+      text: 'Updated:'
+    });
+    createAndAppend('td', tr4, {
+      text: repository.updated_at
+    });
+  }
+
   function main(url) {
-    fetchJSON(url, (err, data) => {
-      const root = document.getElementById('root');
+    fetchJSON(url, (err, repositories) => {
       if (err) {
         createAndAppend('div', root, { text: err.message, class: 'alert-error' });
       } else {
-        createAndAppend('pre', root, { text: JSON.stringify(data, null, 2) });
+        console.log(repositories);
+        const root = document.getElementById('root');
+        const select = createAndAppend('select', root);
+        const leftHandContainer = createAndAppend('div', root, { //div is container in func
+          id: 'left-hand'
+        });
+        repositories.forEach((repository, index) => {
+          createAndAppend('option', select, {
+            text: repository.name,
+            value: index
+          });
+        });
+        select.addEventListener('change', () => {
+          const index = select.value;
+          const repository = repositories[index];
+          renderRepository(repository, leftHandContainer);
+        });
+        renderRepository(repositories[0], leftHandContainer);
       }
     });
   }
+
 
   const HYF_REPOS_URL = 'https://api.github.com/orgs/HackYourFuture/repos?per_page=100';
 
   window.onload = () => main(HYF_REPOS_URL);
 }
+
+
