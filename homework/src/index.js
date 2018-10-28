424deaa65cbad862ec9965c5f2219e516520a53d
Ahmad Kawara <kawara@Ahmad.local>
2018-10-28 19:03:14 +0100

    Improves it.

--- homework/src/index.js
+++ homework/src/index.js
@@ -1,66 +1,65 @@
 "use strick";
 
+function fetchJSON(url) {
+  return new Promise((resolve, reject) => {
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', url);
+    xhr.responseType = "json"
+    xhr.onload = () => {
+      if (xhr.status === 200) {
+        resolve(xhr.response);
+      } else { reject(`Error: ${xhr.status} - ${xhr.statusText}`); }
+    };
+    xhr.onerror = () => reject("Network request failed");
+    xhr.send();
+  });
+}
+
+function createEl(name, parent, obj) {
+  const elem = document.createElement(name);
+  parent.appendChild(elem);
+  if (obj) {
+    if (obj.text) { elem.innerHTML = obj.text; }
+    if (obj.id) { elem.setAttribute(obj.id, obj.val); }
+  }
+  return elem;
+}
+
 function load(url) {
-  const root = document.getElementById("root");
-
-  function fetchJSON(url) {
-    return new Promise((resolve, reject) => {
-      const xhr = new XMLHttpRequest();
-      xhr.open('GET', url);
-      xhr.responseType = "json"
-      xhr.onload = () => {
-        if (xhr.status === 200) {
-          resolve(xhr.response);
-        } else { reject(`Error: ${xhr.status} - ${xhr.statusText}`); }
-      };
-      xhr.onerror = () => reject("Network request failed");
-      xhr.send();
-    });
-  }
-
-  function createEl(name, parent, obj) {
-    const elem = document.createElement(name);
-    parent.appendChild(elem);
-    if (obj) {
-      if (obj.text) { elem.innerHTML = obj.text; }
-      if (obj.id) { elem.setAttribute(obj.id, obj.val); }
-    }
-    return elem;
-  }
-
-  fetchJSON(url).then(data => {
+  fetchJSON(url).then(repos => {
     const header = createEl("header", root);
     createEl("h3", header, { text: "HYF Repositories" });
     const select = createEl("select", header);
-    let names = [];
-    data.forEach(el => {
-      let name = el.name.charAt(0).toUpperCase() + el.name.slice(1);
-      names.push(name);
+    const names = repos.map(repo => {
+      return repo.name.charAt(0).toUpperCase() + repo.name.slice(1);
     });
     names.sort().forEach((name, i) => {
       createEl("option", select, { text: name }).value = i;
     });
-    repos(data, select);
+    renderRepo(repos, select);
   }).catch(error => {
-    createEl("div", root, { text: `Network Error: ${error}`, id: "id", val: "error" });
+    createEl("div", root, {
+      text: `Network Error: ${error}`, id: "id", val: "error"
+    });
   });
 
-  function repos(data, select) {
+  function renderRepo(repos, select) {
     let article = createEl("article", root);
-
     select.addEventListener("change", onSelect);
     function onSelect() {
       let seled = select.options[select.selectedIndex].text.toUpperCase();
-      data.forEach((repo) => {
+      repos.forEach((repo) => {
         let repoName = repo.name.toUpperCase();
         if (seled === repoName) {
           article.innerHTML = "";
           repoDetails(repo, article);
-          fetchJSON(repo.contributors_url).then(info => {
-            contributors(info, article);
+          fetchJSON(repo.contributors_url).then(contributors => {
+            renderContributors(contributors, article);
           }).catch(error => {
             article.innerHTML = "";
-            createEl("div", article, { text: `Network Error: ${error}`, id: "id", val: "error" });
+            createEl("div", article, {
+              text: `Network Error: ${error}`, id: "id", val: "error"
+            });
           });
         }
       });
@@ -82,19 +81,19 @@ function load(url) {
     createEl("tr", table, { text: `<th>Updated:</th><td>${details.updated_at}</td>` });
   }
 
-  function contributors(info, parent) {
+  function renderContributors(contributors, parent) {
     let ul = createEl("ul", parent);
-    for (let i = 0; i < info.length; i++) {
+    contributors.forEach(contributor => {
       let li = createEl("li", ul);
-      let a = createEl("a", li, { id: "href", val: info[i].html_url });
+      let a = createEl("a", li, { id: "href", val: contributor.html_url });
       a.setAttribute("target", "_blank");
-      createEl("img", a, { id: "src", val: info[i].avatar_url });
-      a.innerHTML += `<p>${info[i].login}</p><span>${info[i].contributions}</span>`;
-    }
+      createEl("img", a, { id: "src", val: contributor.avatar_url });
+      a.innerHTML += `<p>${contributor.login}</p><span>${contributor.contributions}</span>`;
+    });
   }
-
-
 }
 
+const root = document.getElementById("root");
 const HYF_REPOS_URL = 'https://api.github.com/orgs/HackYourFuture/repos?per_page=100';
+
 window.onload = () => load(HYF_REPOS_URL);
