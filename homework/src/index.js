e4b68b3201efb2d23edfc192c2ad95599cf52df8
Karam <ra4mos@yahoo.com>
2018-11-08 13:57:48 +0100

    updated hw

--- homework/src/index.js
+++ homework/src/index.js
@@ -33,84 +33,81 @@
     return elem;
   }
 
-  function main(url) {
-    fetchJSON(url)
-      .then(data => {
-        const root = document.getElementById('root');
-        const topDiv = createAndAppend('div', root, { class: 'topDiv' });
-        const header = createAndAppend('header', topDiv, { class: 'heading' });
-        createAndAppend('h1', header, { html: 'HYF Repositories ', class: 'h1-text' });
-        const selectPanel = createAndAppend('select', header, { class: 'select-panel' });
-        data.sort(function (a, b) { return a.name.localeCompare(b.name); });
-        for (let i = 0; i < data.length; i++) {
-          createAndAppend('option', selectPanel, { value: i, html: data[i].name });
-        }
-        const leftDiv = createAndAppend('div', root, { class: 'leftDiv' });
-        const rightDiv = createAndAppend('div', root, { class: 'rightDiv' });
-        selectPanel.addEventListener('change', () => {
-          const selectedValue = data[selectPanel.selectedIndex].url;
-          fetchJSON(selectedValue)
-            .then(data => {
-              renderReposInfo(leftDiv, data, selectedValue);
-            });
-
-          const contributorsLink = data[selectPanel.selectedIndex].contributors_url;
-          fetchJSON(contributorsLink)
-            .then(data => {
-              renderContributorsInfo(rightDiv, data, contributorsLink);
-            });
-
-        });
-
-        fetchJSON(data[0].url)
-          .then(data => {
-            renderReposInfo(leftDiv, data);
-          });
-
-        fetchJSON(data[0].contributors_url)
-          .then(data => {
-            renderContributorsInfo(rightDiv, data);
-          });
-      })
-      .catch(err => {
-        const root = document.getElementById('root');
-        createAndAppend('div', root, { html: err.message, class: 'alert-error' });
-      });
-  }
-
   function addRow(label, value, tbody) {
     const row = createAndAppend('tr', tbody);
     createAndAppend('td', row, { html: label + ':  ', class: 'label' });
     createAndAppend('td', row, { html: value, class: 'repo-name' });
   }
 
-  function renderReposInfo(leftDiv, data) {
+  function renderReposInfo(leftDiv, selectedRepo) {
     leftDiv.innerHTML = '';
     const table = createAndAppend('table', leftDiv, { class: 'table-item' });
     const tbody = createAndAppend('tbody', table);
     const row = createAndAppend('tr', tbody);
     createAndAppend('td', row, { html: 'Repository' + ':  ', class: 'label' });
     const td = createAndAppend('td', row);
-    createAndAppend('a', td, { html: data.name, href: data.html_url, target: '_blank', id: 'link' });
-    if (data.description !== null) {
-      addRow('Description', data.description, tbody);
+    createAndAppend('a', td, { html: selectedRepo.name, href: selectedRepo.html_url, target: '_blank', id: 'link' });
+    if (selectedRepo.description !== null) {
+      addRow('Description', selectedRepo.description, tbody);
     }
-    addRow('Forks', data.forks, tbody);
-    addRow('Updated', new Date(data.updated_at).toLocaleString(), tbody);
+    addRow('Forks', selectedRepo.forks, tbody);
+    addRow('Updated', new Date(selectedRepo.updated_at).toLocaleString(), tbody);
   }
 
-  function renderContributorsInfo(rightDiv, data) {
+  function renderContributorsInfo(rightDiv, contributors) {
     rightDiv.innerHTML = '';
     const rightTable = createAndAppend('div', rightDiv, { class: 'right-table' });
     createAndAppend('p', rightTable, { html: 'Contributions', class: 'cont-title' });
     const ul = createAndAppend('ul', rightTable, { class: 'contributors-list' });
-    data.forEach(contributor => {
-      const contributorLink = createAndAppend('a', ul, { href: (`${contributor.html_url}`), target: '_blank', class: 'c-link' });
+    contributors.forEach(contributor => {
+      const link = createAndAppend('li', ul, { class: 'link' });
+      const contributorLink = createAndAppend('a', link, { href: (`${contributor.html_url}`), target: '_blank', class: 'c-link' });
       const li = createAndAppend('li', contributorLink, { class: 'list-item' });
       createAndAppend('img', li, { src: contributor.avatar_url, class: 'avatar' });
       createAndAppend('div', li, { html: contributor.login, class: 'name-div' });
       createAndAppend('div', li, { html: contributor.contributions, class: 'badge-number-div' });
     });
+  }
+
+  function main(url) {
+    fetchJSON(url)
+      .then(repositories => {
+        const root = document.getElementById('root');
+        const topDiv = createAndAppend('div', root, { class: 'topDiv' });
+        const header = createAndAppend('header', topDiv, { class: 'heading' });
+        createAndAppend('h1', header, { html: 'HYF Repositories ', class: 'h1-text' });
+        const selectPanel = createAndAppend('select', header, { class: 'select-panel' });
+        repositories.sort(function (a, b) { return a.name.localeCompare(b.name); });
+        repositories.forEach((repository, i) => {
+          createAndAppend('option', selectPanel, { value: i, html: repository.name });
+        });
+        const leftDiv = createAndAppend('div', root, { class: 'leftDiv' });
+        const rightDiv = createAndAppend('div', root, { class: 'rightDiv' });
+        selectPanel.addEventListener('change', () => {
+          const selectedRepoUrl = repositories[selectPanel.selectedIndex];
+          const contributorsLink = repositories[selectPanel.selectedIndex].contributors_url;
+          renderReposInfo(leftDiv, selectedRepoUrl, contributorsLink);
+
+          fetchJSON(contributorsLink)
+            .then(contributors => {
+              renderContributorsInfo(rightDiv, contributors, contributorsLink);
+            }).catch(err => {
+              const root = document.getElementById('root');
+              createAndAppend('div', root, { html: err.message, class: 'alert-error' });
+            });
+
+        });
+
+        renderReposInfo(leftDiv, repositories[0]);
+        fetchJSON(repositories[0].contributors_url)
+          .then(contributors => {
+            renderContributorsInfo(rightDiv, contributors);
+          });
+      })
+      .catch(err => {
+        const root = document.getElementById('root');
+        createAndAppend('div', root, { html: err.message, class: 'alert-error' });
+      });
   }
 
   const HYF_REPOS_URL = 'https://api.github.com/orgs/HackYourFuture/repos?per_page=100';
