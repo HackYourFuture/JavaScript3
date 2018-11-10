f12739645413c01b5a2cd7f5bfc6cd735633805f
salar <salar.demok@gmail.com>
2018-11-10 15:34:42 +0100

    Added some changes

--- homework/src/App.js
+++ homework/src/App.js
@@ -21,8 +21,9 @@ class App {
       class: "header"
     });
     Util.createAndAppend("p", this.header, { text: "HYF Repositories" });
-    this.select = Util.createAndAppend("select", this.header, {
-      class: "select"
+    const select = Util.createAndAppend("select", this.header, {
+      class: "select",
+      "aria-label": "HYF Repositories"
     });
     this.container = Util.createAndAppend("div", this.root, {
       class: "container"
@@ -36,17 +37,22 @@ class App {
 
     try {
       const repos = await Util.fetchJSON(url);
-      this.repos = repos.map(repo => new Repository(repo));
-      repos.sort((a, b) => a.name.localeCompare(b.name));
+      this.repos = repos
+        .sort((a, b) => a.name.localeCompare(b.name))
+        .map(repo => new Repository(repo));
+
       repos.forEach((Repository, index) => {
-        Util.createAndAppend("option", this.select, {
+        Util.createAndAppend("option", select, {
           text: Repository.name,
           value: index
         });
       });
-      this.select.addEventListener("change", () => {
-        this.fetchContributorsAndRender(this.select.value);
-        this.fetchContributorsAndRender(0);
+
+      this.fetchContributorsAndRender(0);
+
+      select.addEventListener("change", event => {
+        const index = event.target.value;
+        this.fetchContributorsAndRender(index);
       });
     } catch (error) {
       this.renderError(error);
@@ -72,7 +78,6 @@ class App {
     try {
       const repo = this.repos[index];
       const contributors = await repo.fetchContributors();
-
       const container = document.getElementById("container");
       this.clearContainer(container);
 
@@ -95,9 +100,12 @@ class App {
    * Render an error to the DOM.
    * @param {Error} error An Error object describing the error.
    */
-  renderError(error, parent) {
-    parent.innerHTML = "";
-    Util.createAndAppend("div", parent, { text: error, class: alert - error });
+  renderError(error) {
+    const container = document.getElementById("container");
+    Util.createAndAppend("div", container, {
+      text: error.message,
+      class: "alert-error"
+    });
     this.renderError(error);
   }
 }
