a4404969e35faf91f49f884388d3eedf2be38e77
Seif Tabaja <saif_tabaja@hotmail.com>
2018-11-17 00:32:38 +0100

    wrapped each repo name inside an anchor tag

--- homework/src/index.js
+++ homework/src/index.js
@@ -115,7 +115,7 @@
   function printRepoInfo(arr, option, name, description, forks, update) {
     for (let i = 0; i < arr.length; i++) {
       if (option == i) {
-        repositoryName.innerHTML = "<a href= '#'>" + name[i] + "</a>";
+        repositoryName.innerHTML = "<a href= '#' target='_blank'>" + name[i] + "</a>";
         descriptionText.innerHTML = description[i];
         forkText.innerHTML = forks[i]
         updateText.innerHTML = update[i];
