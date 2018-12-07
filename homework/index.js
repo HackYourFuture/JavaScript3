6cf2c0c6235cbce7777375209d65c3b8ae0de42f
Wael-Alhomsi <waelhom2@gmail.com>
2018-12-07 16:17:14 +0100

    added homework week1

--- homework/index.js
+++ homework/index.js
@@ -30,13 +30,109 @@
     return elem;
   }
 
+  function buildRepositoryInfo(repositoryObject, parent) {
+    const repositoryTable = createAndAppend('table', parent, {
+      id: 'repository-table',
+    });
+    const repositoryTableBody = createAndAppend('tbody', repositoryTable, {});
+    const repositoryTableRow1 = createAndAppend('tr', repositoryTableBody, {});
+    createAndAppend('th', repositoryTableRow1, { text: 'Repository:' });
+    const tableName = createAndAppend('td', repositoryTableRow1, {});
+    createAndAppend('a', tableName, {
+      text: repositoryObject.name,
+      href: repositoryObject.html_url,
+      target: '_blank',
+    });
+    const repositoryTableRow2 = createAndAppend('tr', repositoryTableBody, {});
+    createAndAppend('th', repositoryTableRow2, { text: 'Description:' });
+    createAndAppend('td', repositoryTableRow2, { text: repositoryObject.description });
+    const repositoryTableRow3 = createAndAppend('tr', repositoryTableBody, {});
+    createAndAppend('th', repositoryTableRow3, { text: 'Forks:' });
+    createAndAppend('td', repositoryTableRow3, { text: repositoryObject.forks_count });
+    const repositoryTableRow4 = createAndAppend('tr', repositoryTableBody, {});
+    createAndAppend('th', repositoryTableRow4, { text: 'Last updated:' });
+    createAndAppend('td', repositoryTableRow4, { text: repositoryObject.updated_at });
+  }
+
+  function buildRepositoryContributors(contributorsArray, parent) {
+    createAndAppend('h2', parent, { text: 'Contributions' });
+    const contributorsList = createAndAppend('ul', parent, { id: 'contributors-list' });
+    contributorsArray.forEach(element => {
+      const contributorListItem = createAndAppend('li', contributorsList, {
+        class: 'contributor-list-item',
+      });
+      const contributorContainer = createAndAppend('div', contributorListItem, {
+        class: 'contributor-container',
+      });
+      createAndAppend('img', contributorContainer, {
+        src: element.avatar_url,
+        alt: 'contributor avatar',
+        class: 'contributor-avatar',
+      });
+      const contributorInfoContainer = createAndAppend('div', contributorContainer, {
+        class: 'contributor-info-container',
+      });
+      const contributorNameContainer = createAndAppend('div', contributorInfoContainer, {});
+      const contributorName = createAndAppend('h3', contributorNameContainer, {});
+      createAndAppend('a', contributorName, {
+        text: element.login,
+        href: element.html_url,
+        target: '_blank',
+      });
+      const contributorNumberOfContributionsContainer = createAndAppend(
+        'div',
+        contributorInfoContainer,
+        {},
+      );
+      createAndAppend('h3', contributorNumberOfContributionsContainer, {
+        text: element.contributions,
+      });
+    });
+  }
+
+  function fetchRepositoryContributors(url, parent) {
+    fetchJSON(url, (err, data) => {
+      if (err) {
+        createAndAppend('h2', parent, { text: err.message, class: 'alert-error' });
+      } else {
+        buildRepositoryContributors(data, parent);
+      }
+    });
+  }
+
+  function startUpAndBuildSelectList(arr, parentContainer) {
+    const leftContainer = createAndAppend('div', parentContainer, { id: 'left-container' });
+    const rightContainer = createAndAppend('div', parentContainer, { id: 'right-container' });
+    createAndAppend('img', leftContainer, { src: './hyf.png', id: 'hyf-logo', alt: 'logo image' });
+    createAndAppend('p', leftContainer, { text: '"Refugee code school in Amsterdam"' });
+    createAndAppend('h4', leftContainer, { text: 'Select a repository to display information:' });
+    const selectMenu = createAndAppend('select', leftContainer, { id: 'select-menu' });
+    arr.sort((a, b) => a.name.localeCompare(b.name, 'en', { sensitivity: 'base' }));
+    for (let i = 0; i < arr.length; i++) {
+      createAndAppend('option', selectMenu, { text: arr[i].name, value: i });
+    }
+    const repositoryInfoSection = createAndAppend('div', leftContainer, {
+      id: 'repository-info-section',
+    });
+
+    buildRepositoryInfo(arr[0], repositoryInfoSection);
+    fetchRepositoryContributors(arr[0].contributors_url, rightContainer);
+
+    selectMenu.addEventListener('change', () => {
+      repositoryInfoSection.innerHTML = '';
+      buildRepositoryInfo(arr[event.target.value], repositoryInfoSection);
+      rightContainer.innerHTML = '';
+      fetchRepositoryContributors(arr[event.target.value].contributors_url, rightContainer);
+    });
+  }
+
   function main(url) {
     fetchJSON(url, (err, data) => {
       const root = document.getElementById('root');
       if (err) {
-        createAndAppend('div', root, { text: err.message, class: 'alert-error' });
+        createAndAppend('h1', root, { text: err.message, class: 'alert-error' });
       } else {
-        createAndAppend('pre', root, { text: JSON.stringify(data, null, 2) });
+        startUpAndBuildSelectList(data, root);
       }
     });
   }
