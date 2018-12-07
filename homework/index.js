6dc5132f4767d03490c657340e9aa2e268ac8229
F.Mohamed <mohamedfatuma980@gmail.com>
2018-12-07 21:26:23 +0100

    week1 wip

--- homework/index.js
+++ homework/index.js
@@ -30,16 +30,82 @@
     return elem;
   }
 
-  function main(url) {
-    fetchJSON(url, (err, data) => {
-      const root = document.getElementById('root');
-      if (err) {
-        createAndAppend('div', root, { text: err.message, class: 'alert-error' });
+  function renderRepository(leftContainer, repository) {
+    const tableLeft = createAndAppend('table', leftContainer, { id: 'repo-description' });
+    const tr1 = createAndAppend('tr', tableLeft, { class: 'descriptions' });
+
+    createAndAppend('td', tr1, { class: 'col1_data', text: 'Repository:' });
+    createAndAppend('td', tr1, { class: 'col2_data', text: repository.name });
+
+    const tr2 = createAndAppend('tr', tableLeft, { class: 'descriptions' });
+
+    createAndAppend('td', tr2, { class: 'col1_data', text: 'Description:' });
+    createAndAppend('td', tr2, { class: 'col2_data', text: repository.description });
+
+    const tr3 = createAndAppend('tr', tableLeft, { class: 'descriptions' });
+
+    createAndAppend('td', tr3, { class: 'col1_data', text: 'Forks:' });
+    createAndAppend('td', tr3, { class: 'col2_data', text: repository.forks });
+
+    const tr4 = createAndAppend('tr', tableLeft, { class: 'descriptions' });
+
+    createAndAppend('td', tr4, { class: 'col1_data', text: 'Updated:' });
+    createAndAppend('td', tr4, { class: 'col2_data', text: repository.updated_at });
+
+    // const tableRight = createAndAppend('th', rightContainer, { id: 'title' });
+    // const th = createAndAppend('th', tableRight, { class: 'th_rightT' });
+    // createAndAppend('p', th, { class: 'p_th', text: 'Contributions' });
+  }
+
+  function renderContributors(rightContainer, url) {
+    fetchJSON(url, (err, contributors) => {
+      if (err !== null) {
+        createAndAppend('div', rightContainer, { text: err.message, class: 'alert-error' });
       } else {
-        createAndAppend('pre', root, { text: JSON.stringify(data, null, 2) });
+        const ul = createAndAppend('ul', rightContainer);
+        contributors.forEach(contributor => {
+          const li = createAndAppend('li', ul);
+          createAndAppend('a', li, {
+            text: contributor.login,
+            href: contributor.html_url,
+            target: '_blank',
+          });
+        });
       }
     });
   }
+
+  function main(url) {
+    const root = document.getElementById('root');
+    fetchJSON(url, (err, repositories) => {
+      if (err !== null) {
+        createAndAppend('div', root, { text: err.message, class: 'alert-error' });
+      } else {
+        const header = createAndAppend('header', root, { id: 'header', text: 'HYF Repositories' });
+
+        const select = createAndAppend('select', header, { id: 'repo_options' });
+        repositories.forEach(repository => {
+          createAndAppend('option', select, { text: repository.name });
+        });
+
+        const container = createAndAppend('div', root, { id: 'container' });
+        const leftContainer = createAndAppend('div', container, { id: 'left_container' });
+        const rightContainer = createAndAppend('div', container, { id: 'right_container' });
+
+        select.addEventListener('change', () => {
+          leftContainer.innerHTML = '';
+          rightContainer.innerHTML = '';
+          const index = select.selectedIndex;
+          renderRepository(leftContainer, repositories[index]);
+          renderContributors(rightContainer, repositories[index].contributors_url);
+        });
+
+        renderRepository(leftContainer, repositories[0]);
+        renderContributors(rightContainer, repositories[0].contributors_url);
+      }
+    });
+  }
+
   const HYF_REPOS_URL = 'https://api.github.com/orgs/HackYourFuture/repos?per_page=100';
 
   window.onload = () => main(HYF_REPOS_URL);
