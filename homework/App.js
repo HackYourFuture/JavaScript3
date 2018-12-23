962921a7519ada501b889fc15d9d501642166eb4
Yaacob <yyaacob0@gmail.com>
2018-12-23 14:48:20 +0100

    fixed the handling errors, added handling errors on page, and fixed some styling

--- homework/App.js
+++ homework/App.js
@@ -9,21 +9,21 @@ class App {
 
   async initialize(url) {
     const root = document.getElementById('root');
-
+    const imgContainer = Util.createAndAppend('div', root, { class: 'logo' });
+    Util.createAndAppend('img', imgContainer, {
+      src: './hyf.png',
+      id: 'hyf-logo',
+      alt: 'logo image',
+    });
+    const header = Util.createAndAppend('header', root, { class: 'header' });
+    Util.createAndAppend('p', header, { html: 'HYF Repositories' });
+    const select = Util.createAndAppend('select', header, {
+      class: 'selectBox',
+      'aria-label': 'HYF Repositories',
+    });
+    Util.createAndAppend('div', root, { id: 'container' });
     try {
       const fetch = await Util.fetchJSON(url);
-      const imgContainer = Util.createAndAppend('div', root, { class: 'logo' });
-      Util.createAndAppend('img', imgContainer, {
-        src: './hyf.png',
-        id: 'hyf-logo',
-        alt: 'logo image',
-      });
-      const header = Util.createAndAppend('header', root, { class: 'header' });
-      Util.createAndAppend('p', header, { html: 'HYF Repositories' });
-      const select = Util.createAndAppend('select', header, {
-        class: 'selectBox',
-        'aria-label': 'HYF Repositories',
-      });
       select.addEventListener('change', () => this.fetchContributorsAndRender(select.value));
       Util.createAndAppend('div', root, { id: 'container' });
       this.repositories = fetch
@@ -44,10 +44,9 @@ class App {
     try {
       const contributors = await repository.fetchContributors();
       container.innerHTML = '';
-      const leftDiv = Util.createAndAppend('div', container, { class: 'left-div borderbox' });
-
+      const leftDiv = Util.createAndAppend('div', container, { class: 'leftDiv borderbox' });
       const rightDiv = Util.createAndAppend('div', container, {
-        class: 'right-div borderbox',
+        class: 'rightDiv borderbox',
       });
       Util.createAndAppend('p', rightDiv, { html: 'Contributions', class: 'contributorTitle' });
       const contributorList = Util.createAndAppend('ul', rightDiv, { class: 'contributorList' });
@@ -55,15 +54,15 @@ class App {
       contributors
         .map(contributor => new Contributor(contributor))
         .forEach(contributor => contributor.render(contributorList));
-    } catch (err) {
-      this.renderError(err);
+    } catch (error) {
+      this.renderError(error);
     }
   }
 
   renderError(error) {
     const container = document.getElementById('container');
     container.innerHTML = '';
-    Util.createAndAppend('div', container, { html: error.message });
+    Util.createAndAppend('div', container, { html: error.message, class: 'error onPage' });
   }
 }
 const HYF_REPOS_URL = 'https://api.github.com/orgs/HackYourFuture/repos?per_page=100';
