4dc517f4eb39b98871b8b2f71527ea4faed70485
Joost Lubach <joostlubach@gmail.com>
2019-01-27 11:33:30 +0100

    Added 'hackyourinfo' app - a small web server for students to POST info to as a learning aid in Week 1.
    Usage:
    
    - Deploy somehwere (e.g. using now: `now -p -n hackyourinfo`)
    - Give URL to users
    - Endpoints:
    
    POST /:name.json : Post any JSON body to save info for a student.
    GET /:name.json : Retrieve that student JSON body
    GET /_all.json : Retrieve all students' info
    DELETE /:name.json : Delete one student's info.
    DELETE /_all.json : Start over

---
+++ apps/hackyourinfo/index.js
@@ -0,0 +1,101 @@
+const express = require('express')
+const fs = require('fs-extra')
+const path = require('path')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+
+const rootDir = path.resolve(__dirname)
+const infoDir = path.join(rootDir, 'info')
+
+const app = express()
+
+app.use(bodyParser.json())
+app.use(cors())
+
+app.post('/:name.json', async (request, response) => {
+  try {
+    const filename = path.join(infoDir, `${request.params.name}.json`)
+    await fs.writeFile(filename, JSON.stringify(request.body, null, 2))
+    response.statusCode = 200
+    response.json({ok: true})
+  } catch (error) {
+    response.statusCode = 500
+    response.json({error: "An error occurred"})
+    console.error(error)
+  }
+
+  response.end()
+})
+
+app.get('/_all.json', async (request, response) => {
+  try {
+    const files = await fs.readdir(infoDir)
+    const promises = files.map(file => fs.readFile(path.join(infoDir, file), 'utf8').then(raw => [file, raw]))
+    const raws = await Promise.all(promises)
+
+    const result = {}
+    for (const [file, raw] of raws) {
+      result[file.replace(/\.json$/, '')] = JSON.parse(raw)
+    }
+    response.json(result)
+  } catch (error) {
+    response.statusCode = 500
+    response.json({error: "An error occurred"})
+    console.error(error)
+  }
+  response.end()
+})
+
+app.get('/:name.json', async (request, response) => {
+  try {
+    const filename = path.join(infoDir, `${request.params.name}.json`)
+    const raw = await fs.readFile(filename, 'utf8')
+    response.json(JSON.parse(raw))
+  } catch (error) {
+    if (error.code === 'ENOENT') {
+      response.statusCode = 404
+      response.json({error: "File not found"})
+    } else {
+      response.statusCode = 500
+      response.json({error: "An error occurred"})
+      console.error(error)
+    }
+  }
+  response.end()
+})
+
+app.delete('/_all.json', async (request, response) => {
+  try {
+    await fs.emptyDir(infoDir)
+    response.json({ok: true})
+  } catch (error) {
+    response.statusCode = 500
+    response.json({error: "An error occurred"})
+    console.error(error)
+  }
+  response.end()
+})
+
+app.delete('/:name.json', async (request, response) => {
+  try {
+    await fs.unlink(path.join(infoDir, `${request.params.name}.json`))
+    response.json({ok: true})
+  } catch (error) {
+    if (error.code !== 'ENOENT') {
+      response.statusCode = 500
+      response.json({error: "An error occurred"})
+      console.error(error)
+    } else {
+      response.json({ok: true})
+    }
+  }
+  response.end()
+})
+
+app.use((request, response) => {
+  response.statusCode = 404
+  response.json({error: "Not found"})
+  response.end()
+})
+
+app.listen(process.env.PORT || 80)
