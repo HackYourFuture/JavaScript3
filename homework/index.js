5ea24044988349d1fe0fa1227e1ed59c1348c4ad
AhmadAbdulhaleem <ahmadhaleem1992@gmail.com>
2019-01-29 15:47:22 +0100

    JS3 Github API Homework

--- homework/index.js
+++ homework/index.js
@@ -36,8 +36,92 @@
       if (err) {
         createAndAppend('div', root, { text: err.message, class: 'alert-error' });
       } else {
-        createAndAppend('pre', root, { text: JSON.stringify(data, null, 2) });
+        const navDiv = createAndAppend('div', root, { class: 'nav' });
+        const navHeader = createAndAppend('div', navDiv, { class: 'nav-header' });
+        const navTitle = createAndAppend('div', navHeader, { class: 'nav-title' });
+        navTitle.innerText = 'HYF Repositories';
+        const selectEl = createAndAppend('select', navDiv, { id: 'getRepoData' });
+
+        data.sort((a, b) => (a.name.toLowerCase() < b.name.toLowerCase() ? -1 : 1));
+
+        data.forEach(res => {
+          createAndAppend('option', selectEl, { text: res.name, value: res.id });
+        });
       }
+      const selectRepo = document.getElementById('getRepoData');
+      const container = createAndAppend('div', root, { class: 'container' });
+
+      selectRepo.addEventListener('change', () => {
+        fetchJSON(url, (error, repData) => {
+          container.innerHTML = '';
+          const filteredData = repData.filter(el => el.id === Number(selectRepo.value));
+          const leftSection = createAndAppend('div', container, { class: 'repInfo-side' });
+          const repositoryName = createAndAppend('div', leftSection, { class: 'repName' });
+          const repositoryDescription = createAndAppend('div', leftSection, {
+            class: 'repDescription',
+          });
+
+          const repositoryForks = createAndAppend('div', leftSection, { class: 'repForks' });
+          const repositoryDate = createAndAppend('div', leftSection, { class: 'repDate' });
+
+          filteredData.forEach(allData => {
+            createAndAppend('span', repositoryName, { text: 'Repository Name:' });
+            createAndAppend('a', repositoryName, {
+              text: allData.name,
+              href: allData.html_url,
+              target: '_blank',
+            });
+
+            createAndAppend('span', repositoryDescription, { text: 'Description:' });
+            createAndAppend('p', repositoryDescription, { text: allData.description });
+
+            createAndAppend('span', repositoryForks, { text: 'Forks:' });
+            createAndAppend('p', repositoryForks, { text: allData.forks });
+
+            createAndAppend('span', repositoryDate, { text: 'Date:' });
+            createAndAppend('p', repositoryDate, { text: allData.updated_at });
+
+            const contributorSection = createAndAppend('div', container, {
+              class: 'ContInfo-side',
+            });
+
+            createAndAppend('h4', contributorSection, {
+              text: 'Contributions',
+              class: 'contributorHeader',
+            });
+
+            const contributorWrapper = createAndAppend('div', contributorSection, {
+              class: 'contributorWrapper',
+            });
+
+            fetchJSON(allData.contributors_url, (er, contData) => {
+              contData.forEach(contributor => {
+                const contributorInfo = createAndAppend('a', contributorWrapper, {
+                  href: contributor.html_url,
+                  target: '_blank',
+                });
+                const contributorDiv = createAndAppend('div', contributorInfo, {
+                  class: 'contributor',
+                });
+                createAndAppend('img', contributorDiv, {
+                  class: 'contImage',
+                  src: contributor.avatar_url,
+                });
+                createAndAppend('h4', contributorDiv, {
+                  class: 'contName',
+                  text: contributor.login,
+                });
+                createAndAppend('span', contributorDiv, {
+                  class: 'contNumber',
+                  text: contributor.contributions,
+                });
+
+                createAndAppend('hr', contributorWrapper, { class: 'horizontalLine' });
+              });
+            });
+          });
+        });
+      });
     });
   }
 
