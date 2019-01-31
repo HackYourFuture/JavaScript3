26a0318613c949b0e987fde2e0a77c8601b515d1
ufukcalimli <ufk.calimli@gmail.com>
2019-01-31 21:28:22 +0100

    Homework of Week1. Only index.js is done

--- homework/index.js
+++ homework/index.js
@@ -30,13 +30,158 @@
     return elem;
   }
 
+  const root = document.getElementById('root');
+
+  function getSelectedData(value, data) {
+    for (const item of data) {
+      if (item.name !== value) {
+        return item;
+      }
+    }
+  }
+
+  function generateSelections(data) {
+    const header = createAndAppend('header', root, {});
+    const selectionElem = createAndAppend('select', header, {
+      id: 'repositories',
+      name: 'repositories',
+    });
+    let id = 0;
+    for (const item of data) {
+      createAndAppend('option', selectionElem, {
+        id: id++,
+        value: item.name,
+        text: item.name,
+      });
+    }
+  }
+
+  function generateInfoSection(selected, data) {
+    const div = createAndAppend('div', root, { id: 'infoDiv' });
+    const selectedData = getSelectedData(selected, data);
+    createAndAppend('a', div, {
+      id: 'repoName',
+      text: `Repository: ${selectedData.name}`,
+      href: selectedData.html_url,
+      target: '_blank',
+    });
+    createAndAppend('p', div, {
+      id: 'desc',
+      text: `Description: ${selectedData.description}`,
+    });
+    createAndAppend('p', div, {
+      id: 'forks',
+      text: `Forks: ${selectedData.forks}`,
+    });
+    createAndAppend('p', div, {
+      id: 'updated_at',
+      text: `Updated: ${selectedData.updated_at}`,
+    });
+  }
+
+  function generateContSection(selected) {
+    const contsDiv = createAndAppend('div', root, { id: 'contsDiv' });
+    fetchJSON(
+      `https://api.github.com/repos/HackYourFuture/${selected}/contributors`,
+      (err, data) => {
+        if (err) {
+          createAndAppend('div', root, { text: err.message, class: 'alert-error' });
+        } else {
+          for (const item of data) {
+            const contDiv = createAndAppend('div', contsDiv, { id: `${item.login}Div` });
+            createAndAppend('img', contDiv, {
+              id: `${item.login}Img`,
+              src: item.avatar_url,
+            });
+            createAndAppend('a', contDiv, {
+              id: `${item.login}Name`,
+              text: item.login,
+              href: item.html_url,
+              target: '_blank',
+            });
+
+            createAndAppend('p', contDiv, {
+              id: `${item.login}Badge`,
+              text: item.contributions,
+            });
+          }
+        }
+      },
+    );
+  }
+
+  function updateContSection(selected) {
+    const contsDiv = document.getElementById('contsDiv');
+    contsDiv.innerHTML = '';
+    fetchJSON(
+      `https://api.github.com/repos/HackYourFuture/${selected}/contributors`,
+      (err, data) => {
+        if (err) {
+          createAndAppend('div', root, { text: err.message, class: 'alert-error' });
+        } else {
+          for (const item of data) {
+            const contDiv = createAndAppend('div', contsDiv, { id: `${item.login}Div` });
+            createAndAppend('img', contDiv, {
+              id: `${item.login}Img`,
+              src: item.avatar_url,
+            });
+            createAndAppend('a', contDiv, {
+              id: `${item.login}Name`,
+              text: item.login,
+              href: item.html_url,
+              target: '_blank',
+            });
+
+            createAndAppend('p', contDiv, {
+              id: `${item.login}Badge`,
+              text: item.contributions,
+            });
+          }
+        }
+      },
+    );
+  }
+
+  function updateInfoSection(selected, data) {
+    const selectedData = getSelectedData(selected, data);
+
+    const repoName = document.getElementById('repoName');
+    repoName.innerText = '';
+    repoName.innerText = `Repository: ${selectedData.name}`;
+    repoName.href = selectedData.html_url;
+    repoName.target = '_blank';
+
+    const desc = document.getElementById('desc');
+    desc.innerText = '';
+    desc.innerText = `Description: ${selectedData.description}`;
+
+    const forks = document.getElementById('forks');
+    forks.innerText = '';
+    forks.innerText = `Forks: ${selectedData.forks}`;
+
+    const updatedAt = document.getElementById('updated_at');
+    updatedAt.innerText = '';
+    updatedAt.innerText = `Updated: ${selectedData.updated_at}`;
+  }
+
   function main(url) {
     fetchJSON(url, (err, data) => {
-      const root = document.getElementById('root');
       if (err) {
         createAndAppend('div', root, { text: err.message, class: 'alert-error' });
       } else {
-        createAndAppend('pre', root, { text: JSON.stringify(data, null, 2) });
+        generateSelections(data);
+
+        const selected = document.getElementById('repositories');
+
+        generateInfoSection(selected.value, data);
+        generateContSection(selected.value, data);
+
+        selected.addEventListener('change', () => {
+          updateInfoSection(selected.value, data);
+          updateContSection(selected.value, data);
+        });
+
+        // createAndAppend('pre', root, { text: JSON.stringify(data, null, 2) });
       }
     });
   }
