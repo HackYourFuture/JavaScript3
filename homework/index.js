2eb0a6ce91487ddf97df462516f218b825eed962
YaseirAlkhwalda <yaseir.alkhwalda@gmail.com>
2019-02-01 04:42:45 +0100

    Week1

--- homework/index.js
+++ homework/index.js
@@ -30,13 +30,84 @@
     return elem;
   }
 
+  // main function
   function main(url) {
-    fetchJSON(url, (err, data) => {
+    fetchJSON(url, (err, repositories) => {
       const root = document.getElementById('root');
       if (err) {
         createAndAppend('div', root, { text: err.message, class: 'alert-error' });
       } else {
-        createAndAppend('pre', root, { text: JSON.stringify(data, null, 2) });
+        const header = createAndAppend('div', root, { class: 'header' });
+        createAndAppend('p', header, { text: 'HYF Repositories' });
+        const container = createAndAppend('div', root, { id: 'container' });
+        const leftDiv = createAndAppend('div', container, {
+          class: 'left-div',
+        });
+        const rightDiv = createAndAppend('div', container, {
+          class: 'right-div',
+        });
+        const select = createAndAppend('select', header, { class: 'repo-selector' });
+        repositories.sort((a, b) => a.name.localeCompare(b.name));
+        repositories.forEach(repository => {
+          createAndAppend('option', select, { text: repository.name });
+        });
+        function createRepositoryDescription(repository) {
+          const table = createAndAppend('table', leftDiv);
+          const tBody = createAndAppend('tbody', table);
+          const details = ['Repository:', 'Description:', 'Forks:', 'Updated:'];
+          for (let detail of details) {
+            const tr = createAndAppend('tr', tBody);
+            const firstTd = createAndAppend('td', tr, { class: 'label' });
+            createAndAppend('td', tr);
+            firstTd.innerText = detail;
+          }
+          const secondTd = document.querySelector('td:nth-child(2)');
+          const link = createAndAppend('a', secondTd, {
+            href: repository.html_url,
+            target: '_blank',
+          });
+          link.innerText = repository.name;
+          document.getElementsByTagName('td')[3].innerText = repository.description;
+          document.getElementsByTagName('td')[5].innerText = repository.forks;
+          document.getElementsByTagName('td')[7].innerText = new Date(
+            repository.updated_at,
+          ).toLocaleDateString();
+        }
+        createRepositoryDescription(repositories[0]);
+        // create right div
+        function createContributorsSide(contributors) {
+          contributors.forEach(contributor => {
+            const contributorInfo = createAndAppend('a', rightDiv, {
+              href: contributor.html_url,
+              target: '_blank',
+            });
+            const contributorDiv = createAndAppend('div', contributorInfo, {
+              class: 'contributor',
+            });
+            createAndAppend('img', contributorDiv, { src: contributor.avatar_url });
+            createAndAppend('p', contributorDiv, { text: contributor.login });
+            createAndAppend('div', contributorDiv, {
+              class: 'contributor-badge',
+              text: contributor.contributions,
+            });
+          });
+        }
+        function createContributors(url) {
+          fetchJSON(url, (err, contributors) => {
+            createAndAppend('p', rightDiv, { text: 'Contributions: ' });
+            createContributorsSide(contributors);
+          });
+        }
+        createContributors(repositories[0].contributors_url);
+
+        // add event listener
+        select.addEventListener('change', () => {
+          leftDiv.innerText = '';
+          rightDiv.innerText = '';
+          const index = select.selectedIndex;
+          createRepositoryDescription(repositories[index]);
+          createContributors(repositories[index].contributors_url);
+        });
       }
     });
   }
