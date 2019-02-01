89f886e7fb39125bfcd8a7b5122e25769cf19456
alquinex <alquinex2010@hotmail.com>
2019-02-01 08:36:33 +0100

    JavaScript3 homework week1

--- homework/index.js
+++ homework/index.js
@@ -36,10 +36,76 @@
       if (err) {
         createAndAppend('div', root, { text: err.message, class: 'alert-error' });
       } else {
-        createAndAppend('pre', root, { text: JSON.stringify(data, null, 2) });
-      }
+
+        const h1 = createAndAppend('h1', root, { id: 'h1' });
+        createAndAppend('p', h1, { text: 'HYF Repositories' });
+        const select = createAndAppend('select', h1, { id: 'select' });
+
+        select.addEventListener(
+          'change', () => {
+            const selected = select.selectedIndex;
+            const repositoryValue = data[selected].name;
+            const repositoryLink = data[selected].html_url;
+            if (repositoryValue === null) {
+              document.getElementById('property_0').innerText = '-';
+            } else {
+              document.getElementById('property_0').innerText = repositoryValue;
+              document.getElementById('property_0').setAttribute('href', repositoryLink);
+              document.getElementById('property_0').setAttribute('target', '_blank');
+            }
+
+            const descriptionValue = data[selected].description;
+            if (descriptionValue === null) {
+              document.getElementById('property_1').innerText = '-';
+            } else {
+              document.getElementById('property_1').innerText = descriptionValue;
+            }
+            const forksValue = data[selected].forks;
+            if (forksValue === null) {
+              document.getElementById('property_2').innerText = '-';
+            } else {
+              document.getElementById('property_2').innerText = forksValue;
+            }
+            const updatedValue = new Date(data[selected].updated_at);
+            if (updatedValue === null) {
+              document.getElementById('property_3').innerText = '-';
+            } else {
+              document.getElementById('property_3').innerText = updatedValue;
+            }
+          },
+          false,
+        );
+
+        const sortedData = data.sort((a, b) => a.name.localeCompare(b.name));
+        sortedData.forEach((repo, index) => {
+          createAndAppend('option', select, { text: repo.name, value: index });
+        });
+
+        const columnLeft = createAndAppend('div', root, { id: 'info_panel' });
+        const columnTableLeft = createAndAppend('table', columnLeft);
+        const columnTableLeftProperties = ['Repository:', 'Description:', 'Forks:', 'Updated:'];
+        const columnTableLeftFirstTr = createAndAppend('tr', columnTableLeft);
+        createAndAppend('td', columnTableLeftFirstTr, { text: columnTableLeftProperties[0] });
+        const repositoryLink = createAndAppend('td', columnTableLeftFirstTr);
+        createAndAppend('a', repositoryLink, { id: `property_${0}` });
+
+        for (let i = 0; i < 4; i++) {
+          const columnTableLeftTr = createAndAppend('tr', columnTableLeft);
+          createAndAppend('td', columnTableLeftTr, { id: `property_${i}` });
+          createAndAppend('td', columnTableLeftTr, { id: `property_${i}` });
+        }
+
+        document.getElementById('property_0').innerText = data[0].name;
+        document.getElementById('property_0').setAttribute('href', data[0].html_url);
+        document.getElementById('property_0').setAttribute('target', '_blank');
+        document.getElementById('property_1').innerText = data[0].description;
+        document.getElementById('property_2').innerText = data[0].forks;
+        document.getElementById('property_3').innerText = new Date(data[0].updated_at);
+        }
     });
   }
+
+  
 
   const HYF_REPOS_URL = 'https://api.github.com/orgs/HackYourFuture/repos?per_page=100';
 
