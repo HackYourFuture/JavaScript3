d48c974fd326c456d20e69e00df8e8dd16cffd88
Ayh0o0m <44189059+Ayh0o0m@users.noreply.github.com>
2019-02-01 01:49:45 +0100

    week1

--- homework/index.js
+++ homework/index.js
@@ -36,8 +36,70 @@
       if (err) {
         createAndAppend('div', root, { text: err.message, class: 'alert-error' });
       } else {
-        createAndAppend('pre', root, { text: JSON.stringify(data, null, 2) });
+        createAndAppend('header', root, { id: 'header', class: 'header_box' });
+        const header = document.getElementById('header');
+        createAndAppend('p', header, { text: 'HYF Repositories' });
+        const hyfRepos = createAndAppend('select', header, { id: 'hyf_repos' });
+
+        data.sort((a, b) => (a.name.toLowerCase() < b.name.toLowerCase() ? -1 : 1));
+        data.forEach(repo => {
+          createAndAppend('option', hyfRepos, { text: repo.name, value: repo.id });
+        });
       }
+      const selectData = document.getElementById('hyf_repos');
+      const bodyContainer = createAndAppend('div', root, {
+        class: 'body_container_box',
+        id: 'body_container',
+      });
+      const leftDiv = createAndAppend('div', bodyContainer, {
+        class: 'body_left_box',
+        id: 'left_div',
+      });
+      const rightDiv = createAndAppend('div', bodyContainer, {
+        class: 'body_right_box',
+        id: 'right_div',
+      });
+
+      selectData.addEventListener('change', () => {
+        fetchJSON(url, (error, repoData) => {
+          leftDiv.innerHTML = '';
+          const output = repoData.filter(el => Number(selectData.value) === el.id);
+          output.forEach(res => {
+            const repoDiv = createAndAppend('div', leftDiv, { id: 'repo_div' });
+            const descriptionDiv = createAndAppend('div', leftDiv, { id: 'description_div' });
+            const forksDiv = createAndAppend('div', leftDiv, { id: 'forks_div' });
+            const updatedDiv = createAndAppend('div', leftDiv, { id: 'updated_div' });
+
+            createAndAppend('span', repoDiv, { text: 'Repository: ', class: 'bold' });
+            createAndAppend('a', repoDiv, { text: res.name, href: res.html_url, target: '_blank' });
+            createAndAppend('span', descriptionDiv, { text: 'Description:     ', class: 'bold' });
+            createAndAppend('span', descriptionDiv, { text: res.description });
+            createAndAppend('span', forksDiv, { text: 'Forks:     ', class: 'bold' });
+            createAndAppend('span', forksDiv, { text: res.forks });
+            createAndAppend('span', updatedDiv, { text: 'Updated:     ', class: 'bold' });
+            createAndAppend('span', updatedDiv, { text: res.updated_at });
+
+            fetchJSON(res.contributors_url, (er, contributorData) => {
+              rightDiv.innerHTML = '';
+              createAndAppend('p', rightDiv, {
+                text: 'Contributions',
+                id: 'contributors_header',
+                class: 'bold',
+              });
+              const contributorsList = createAndAppend('ul', rightDiv, {
+                class: 'contributors_list',
+              });
+              contributorData.forEach(contributor => {
+                createAndAppend('img', contributorsList, {
+                  class: 'cont_imgs',
+                  src: contributor.avatar_url,
+                });
+                createAndAppend('li', contributorsList, { text: contributor.login });
+              });
+            });
+          });
+        });
+      });
     });
   }
 
