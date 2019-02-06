12dc30e60ac81f621411a14d94891baab9ade99f
mohamdmahdi <mohamd.mahdi@gmail.com>
2019-02-06 14:06:14 +0100

    homework week1

--- homework/index.js
+++ homework/index.js
@@ -1,6 +1,53 @@
+/* eslint-disable consistent-return */
+
 'use strict';
 
 {
+  function createAndAppend(name, parent, options = {}) {
+    const elem = document.createElement(name);
+    parent.appendChild(elem);
+    Object.keys(options).forEach(key => {
+      const value = options[key];
+      if (key === 'text') {
+        elem.textContent = value;
+      } else {
+        elem.setAttribute(key, value);
+      }
+    });
+    // const elem = document.createElement(name);
+    // parent.appendChild(elem);
+    return elem;
+  }
+
+  function buildRepositoryContributors(contributors, parent) {
+    createAndAppend('label', parent, { text: 'Contributors', id: 'titleContributor' });
+    contributors.forEach(contributor => {
+      const link = createAndAppend('a', parent, { href: contributor.html_url, target: '_blank' });
+      const contributorData = createAndAppend('div', link, { class: 'contributor' });
+      createAndAppend('img', contributorData, {
+        src: contributor.avatar_url,
+        alt: contributor.login,
+      });
+      createAndAppend('label', contributorData, { text: contributor.login, id: 'name' });
+      createAndAppend('label', contributorData, {
+        text: contributor.contributions,
+        id: 'numberOfContribution',
+      });
+    });
+    return parent;
+  }
+
+  function fitchRepositoryContributors(url, parent) {
+    // eslint-disable-next-line no-use-before-define
+    fetchJSON(url, (err, data) => {
+      if (err) {
+        createAndAppend('div', parent, { text: err.message, class: 'alert-error' });
+      } else {
+        buildRepositoryContributors(data, parent);
+      }
+    });
+  }
+
   function fetchJSON(url, cb) {
     const xhr = new XMLHttpRequest();
     xhr.open('GET', url);
@@ -16,27 +63,63 @@
     xhr.send();
   }
 
-  function createAndAppend(name, parent, options = {}) {
-    const elem = document.createElement(name);
-    parent.appendChild(elem);
-    Object.keys(options).forEach(key => {
-      const value = options[key];
-      if (key === 'text') {
-        elem.textContent = value;
-      } else {
-        elem.setAttribute(key, value);
-      }
+  function buildRepositoryInfo(RepositoryElement, parent) {
+    const repository = createAndAppend('div', parent, { id: 'repository' });
+    const description = createAndAppend('div', parent, { id: 'description' });
+    const fork = createAndAppend('div', parent, { id: 'fork' });
+    const dateRow = createAndAppend('div', parent, { id: 'date' });
+
+    createAndAppend('label', repository, { text: 'Repository Name:' });
+    createAndAppend('a', repository, {
+      href: RepositoryElement.html_url,
+      text: RepositoryElement.name,
+      target: '_blank',
     });
-    return elem;
+    createAndAppend('label', description, { text: 'Description: ' });
+    createAndAppend('label', description, { text: RepositoryElement.description });
+    createAndAppend('label', fork, { text: 'Fork: ' });
+    createAndAppend('label', fork, { text: RepositoryElement.forks });
+    createAndAppend('label', dateRow, { text: 'Date: ' });
+    createAndAppend('label', dateRow, { text: RepositoryElement.updated_at });
+  }
+
+  function buildRepositoryList(repositories, parent) {
+    const top = createAndAppend('div', parent, { id: 'top' });
+    const body = createAndAppend('div', parent, { id: 'bodytag' });
+    const rightSide = createAndAppend('div', body, { id: 'right' });
+    createAndAppend('label', top, { text: 'Select a Repository: ' });
+    const selectRepositoryMenue = createAndAppend('select', top, { id: 'selectMenue' });
+    createAndAppend('option', selectRepositoryMenue, { hidden: 'hidden' });
+    repositories.sort((a, b) => a.name.localeCompare(b.name, 'en', { sensitivity: 'base' }));
+    repositories.forEach(repository => {
+      createAndAppend('option', selectRepositoryMenue, {
+        value: repository.id,
+        text: repository.name,
+      });
+    });
+    const respositorysection = createAndAppend('div', body, { id: 'respositoryInfo' });
+    selectRepositoryMenue.addEventListener('change', () => {
+      // eslint-disable-next-line array-callback-return
+      const selectedRespo = repositories.find(option => {
+        // eslint-disable-next-line no-restricted-globals
+        if (Number(option.id) === Number(event.target.value)) {
+          return option;
+        }
+      });
+      respositorysection.innerHTML = '';
+      buildRepositoryInfo(selectedRespo, respositorysection);
+      rightSide.innerHTML = '';
+      fitchRepositoryContributors(selectedRespo.contributors_url, rightSide);
+    });
   }
 
   function main(url) {
-    fetchJSON(url, (err, data) => {
+    fetchJSON(url, (err, repositories) => {
       const root = document.getElementById('root');
       if (err) {
         createAndAppend('div', root, { text: err.message, class: 'alert-error' });
       } else {
-        createAndAppend('pre', root, { text: JSON.stringify(data, null, 2) });
+        buildRepositoryList(repositories, root);
       }
     });
   }
