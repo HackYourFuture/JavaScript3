c41fba44f2501fd69a9a14b7758f46cf84deaa4d
alquinex <alquinex2010@hotmail.com>
2019-02-07 17:14:24 +0100

    homework week1

--- homework/index.js
+++ homework/index.js
@@ -30,19 +30,77 @@
     return elem;
   }
 
-  function dropDown(repos) {
-    const root = document.getElementById('root');
-    const header = createAndAppend('header', root, { class: 'header' });
-    const select = createAndAppend('select', header); 
-  
-    Object.keys(repos).forEach(repo => {
-      createAndAppend('option', select, {text: repo.name})
-    })
-    createAndAppend('p', header, { text: 'HYF Repositories' });
+  function renderContributors(contributors, contributorContainer) {
+    contributors.forEach(contributor => {
+      const contributorDetail = createAndAppend('div', contributorContainer, {
+        class: 'contributor-details',
+      });
+      createAndAppend('img', contributorDetail, {
+        src: contributor.avatar_url,
+        alt: contributor.login,
+        class: 'avatar',
+      });
+      createAndAppend('p', contributorDetail, { text: contributor.login });
+      createAndAppend('p', contributorDetail, { text: contributor.contributions });
+    });
   }
-  dropDown();
 
-  
+  function fetchAndRender(selectedRepo, repoContainer, contributorContainer, root) {
+    console.log('name', selectedRepo);
+    repoContainer.innerHTML = '';
+    contributorContainer.innerHTML = '';
+
+    const updatedAt = new Date(selectedRepo.updated_at);
+
+    createAndAppend('p', repoContainer, { text: `Repository: ${selectedRepo.name}` });
+    createAndAppend('p', repoContainer, { text: `Description: ${selectedRepo.description}` });
+    createAndAppend('p', repoContainer, { text: `Forks: ${selectedRepo.forks}` });
+    createAndAppend('p', repoContainer, { text: `Update: ${updatedAt.toLocaleString()}` });
+
+    fetchJSON(selectedRepo.contributors_url, (err, contributors) => {
+      if (err) {
+        createAndAppend('div', root, { text: err.message, class: 'alert-message' });
+      } else {
+        renderContributors(contributors, contributorContainer);
+      }
+    });
+  }
+
+  function dropDown(root, repos) {
+    const header = createAndAppend('header', root, { class: 'header' });
+    createAndAppend('h3', header, { text: 'HYF Repositories' });
+
+    const select = createAndAppend('select', header);
+    repos.forEach((repo, index) => {
+      createAndAppend('option', select, {
+        text: repo.name,
+        value: index,
+      });
+    });
+
+    const container = createAndAppend('div', root, { id: 'container' });
+    const repoContainer = createAndAppend('div', container, { id: 'repo-container' });
+    const contributorContainer = createAndAppend('div', container, { id: 'contributor-container' });
+
+    select.addEventListener('change', () => {
+      const selectedRepo = repos[select.value];
+      console.log('name', selectedRepo);
+      fetchAndRender(selectedRepo, repoContainer, contributorContainer, root);
+    });
+
+    fetchAndRender(repos[0], repoContainer, contributorContainer, root);
+  }
+
+  function main(url) {
+    const root = document.getElementById('root');
+    fetchJSON(url, (err, repositories) => {
+      if (err) {
+        createAndAppend('div', root, { text: err.message, class: 'alert-message' });
+      } else {
+        dropDown(root, repositories);
+      }
+    });
+  }
 
   const HYF_REPOS_URL = 'https://api.github.com/orgs/HackYourFuture/repos?per_page=100';
 
