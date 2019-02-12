015c92be4cd3f5fcd121f6bda68b72e4a5bbc55f
Soolaf <44653520+soolaf@users.noreply.github.com>
2019-02-12 23:44:24 +0100

    update homework week2

--- homework/index.js
+++ homework/index.js
@@ -1,95 +1,7 @@
 'use strict';
 
-<<<<<<< HEAD
 /* eslint-disable prettier/prettier */
 function createAndAppend(name, parent, options = {}) {
-  const elem = document.createElement(name);
-  parent.appendChild(elem);
-  Object.keys(options).forEach(key => {
-    const value = options[key];
-    if (key === 'text') {
-      elem.textContent = value;
-    } else {
-      elem.setAttribute(key, value);
-    }
-  });
-  return elem;
-}
-
-function renderRepositories(repository, container) {
-  container.innerHTML = '';
-  const table = createAndAppend('table', container);
-  const tbody = createAndAppend('tbody', table);
-  const tr1 = createAndAppend('tr', tbody);
-  createAndAppend('td', tr1, {
-    text: 'Repository:',
-  });
-  const td12 = createAndAppend('td', tr1);
-  createAndAppend('a', td12, {
-    href: repository.html_url,
-    target: '_blank',
-    text: repository.name,
-  });
-  const tr2 = createAndAppend('tr', tbody);
-  createAndAppend('td', tr2, {
-    text: 'Description:',
-  });
-  createAndAppend('td', tr2, {
-    text: repository.description,
-  });
-  const tr3 = createAndAppend('tr', tbody);
-  createAndAppend('td', tr3, {
-    text: 'Forks:',
-  });
-  createAndAppend('td', tr3, {
-    text: repository.forks,
-  });
-  const tr4 = createAndAppend('tr', tbody);
-  createAndAppend('td', tr4, {
-    text: 'Updated:',
-  });
-  createAndAppend('td', tr4, {
-    text: repository.updated_at,
-  });
-}
-
-function renderContributors(contributors, container) {
-  container.innerHTML = '';
-  createAndAppend('p', container, {
-    text: 'Contributions',
-  });
-  const ul = createAndAppend('ul', container);
-  contributors.forEach(contributor => {
-    const li = createAndAppend('li', ul);
-    const a = createAndAppend('a', li, {
-      href: contributor.html_url,
-      target: '_blank',
-      text: contributor.login,
-    });
-    createAndAppend('img', a, {
-      src: contributor.avatar_url,
-      width: 100,
-      height: 100,
-=======
-{
-  function fetchJSON(url) {
-    return new Promise((resolve, reject) => {
-      const xhr = new XMLHttpRequest();
-      xhr.open('GET', url);
-      xhr.responseType = 'json';
-      xhr.onload = () => {
-        if (xhr.status < 400) {
-          resolve(xhr.response);
-        } else {
-          reject(new Error(`Network error: ${xhr.status} - ${xhr.statusText}`));
-        }
-      };
-      xhr.onerror = () => reject(new Error('Network request failed'));
-      xhr.send();
-    });
-  }
-
-  function createAndAppend(name, parent, options = {}) {
     const elem = document.createElement(name);
     parent.appendChild(elem);
     Object.keys(options).forEach(key => {
@@ -137,15 +49,13 @@ function renderContributors(contributors, container) {
     });
     createAndAppend('td', tr4, {
       text: repository.updated_at,
->>>>>>> b83a336dbf0d2d058266edf2bb5f1a29a101d3f9
-    });
+ });
     createAndAppend('div', a, {
       text: contributor.contributions,
     });
   });
 }
 
-<<<<<<< HEAD
 function handleError(container, error) {
   createAndAppend('div', container, {
     text: error.message,
@@ -194,7 +104,6 @@ function mainPromise(root, repositories) {
       renderContributors(contributors, rightHand);
     })
     .catch(error => handleError(root, error));
-=======
   function renderContributors(contributors, container) {
     container.innerHTML = '';
     createAndAppend('p', container, {
