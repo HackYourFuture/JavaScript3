622382105bf63edcb285313fef32e6b8f4ac0cd5
AhmadAbdulhaleem <ahmadhaleem1992@gmail.com>
2019-02-12 12:41:51 +0100

    Fix week2 issues

--- homework/index.js
+++ homework/index.js
@@ -1,21 +1,6 @@
 'use strict';
 
 {
-  // function fetchJSON(url, cb) {
-  //   const xhr = new XMLHttpRequest();
-  //   xhr.open('GET', url);
-  //   xhr.responseType = 'json';
-  //   xhr.onload = () => {
-  //     if (xhr.status < 400) {
-  //       cb(null, xhr.response);
-  //     } else {
-  //       cb(new Error(`Network error: ${xhr.status} - ${xhr.statusText}`));
-  //     }
-  //   };
-  //   xhr.onerror = () => cb(new Error('Network request failed'));
-  //   xhr.send();
-  // }
-
   function createAndAppend(name, parent, options = {}) {
     const elem = document.createElement(name);
     parent.appendChild(elem);
@@ -31,18 +16,38 @@
   }
 
   const root = document.getElementById('root');
-  function fetchAllRepoData(url) {
-    return fetch(url)
-      .then(res => res.json())
-      .then(data => {
-        const navDiv = createAndAppend('div', root, { class: 'nav' });
-        const navHeader = createAndAppend('div', navDiv, { class: 'nav-header' });
-        const navTitle = createAndAppend('div', navHeader, { class: 'nav-title' });
-        navTitle.innerText = 'HYF Repositories';
-        const selectEl = createAndAppend('select', navDiv, { id: 'getRepoData' });
-        data.sort((a, b) => (a.name.toLowerCase() < b.name.toLowerCase() ? -1 : 1));
-        data.forEach(res => {
-          createAndAppend('option', selectEl, { text: res.name, value: res.id });
+
+  function fetchContributorsData(data, wrapper) {
+    fetch(data.contributors_url);
+    const contributorSection = createAndAppend('div', wrapper, { class: 'ContInfo-side' });
+    createAndAppend('h4', contributorSection, {
+      text: 'Contributions',
+      class: 'contributorHeader',
+    });
+    const contributorWrapper = createAndAppend('div', contributorSection, {
+      class: 'contributorWrapper',
+    });
+    return fetch(data.contributors_url)
+      .then(contributorData => contributorData.json())
+      .then(contData => {
+        contData.forEach(contributor => {
+          const contributorInfo = createAndAppend('a', contributorWrapper, {
+            href: contributor.html_url,
+            target: '_blank',
+            class: 'contributorWrapper',
+          });
+          const contributorDiv = createAndAppend('div', contributorInfo, {
+            class: 'contributor',
+          });
+          createAndAppend('img', contributorDiv, {
+            class: 'contImage',
+            src: contributor.avatar_url,
+          });
+          createAndAppend('h4', contributorDiv, { class: 'contName', text: contributor.login });
+          createAndAppend('span', contributorDiv, {
+            class: 'contNumber',
+            text: contributor.contributions,
+          });
         });
       })
       .catch(err => {
@@ -50,86 +55,64 @@
       });
   }
 
-  function getRepoData(url) {
-    const selectRepo = document.getElementById('getRepoData');
-    const container = createAndAppend('div', root, { class: 'container' });
-    function fetchSingleData() {
-      return fetch(url)
-        .then(res => res.json())
-        .then(repData => {
-          container.innerHTML = '';
-          const filteredData = repData.filter(el => el.id === Number(selectRepo.value));
-          const repositoriesSection = createAndAppend('div', container, { class: 'repInfo-side' });
-          const repositoryName = createAndAppend('div', repositoriesSection, { class: 'repName' });
-          const repositoryDescription = createAndAppend('div', repositoriesSection, {
-            class: 'repDescription',
-          });
-          const repositoryForks = createAndAppend('div', repositoriesSection, {
-            class: 'repForks',
-          });
-          const repositoryDate = createAndAppend('div', repositoriesSection, { class: 'repDate' });
-          filteredData.forEach(allData => {
-            createAndAppend('span', repositoryName, { text: 'Repository Name:' });
-            createAndAppend('a', repositoryName, {
-              text: allData.name,
-              href: allData.html_url,
-              target: '_blank',
-            });
-            createAndAppend('span', repositoryDescription, { text: 'Description:' });
-            createAndAppend('p', repositoryDescription, { text: allData.description });
-            createAndAppend('span', repositoryForks, { text: 'Forks:' });
-            createAndAppend('p', repositoryForks, { text: allData.forks });
-            createAndAppend('span', repositoryDate, { text: 'Date:' });
-            createAndAppend('p', repositoryDate, { text: allData.updated_at });
+  function fetchSingleRepoData(repData, selectEl, wrapper) {
+    const filteredData = repData.filter(el => el.id === Number(selectEl.value));
+    wrapper.innerHTML = '';
+    const repositoriesSection = createAndAppend('div', wrapper, { class: 'repInfo-side' });
+    const repositoryName = createAndAppend('div', repositoriesSection, { class: 'repName' });
+    const repositoryDescription = createAndAppend('div', repositoriesSection, {
+      class: 'repDescription',
+    });
+    const repositoryForks = createAndAppend('div', repositoriesSection, {
+      class: 'repForks',
+    });
+    const repositoryDate = createAndAppend('div', repositoriesSection, { class: 'repDate' });
+    filteredData.forEach(allData => {
+      createAndAppend('span', repositoryName, { text: 'Repository Name:' });
+      createAndAppend('a', repositoryName, {
+        text: allData.name,
+        href: allData.html_url,
+        target: '_blank',
+      });
+      createAndAppend('span', repositoryDescription, { text: 'Description:' });
+      createAndAppend('p', repositoryDescription, { text: allData.description });
+      createAndAppend('span', repositoryForks, { text: 'Forks:' });
+      createAndAppend('p', repositoryForks, { text: allData.forks });
+      createAndAppend('span', repositoryDate, { text: 'Date:' });
+      createAndAppend('p', repositoryDate, { text: allData.updated_at });
+      /* Fetch Contributor Data */
+      fetchContributorsData(allData, wrapper);
+    });
+  }
 
-            const contributorSection = createAndAppend('div', container, {
-              class: 'ContInfo-side',
-            });
-            createAndAppend('h4', contributorSection, {
-              text: 'Contributions',
-              class: 'contributorHeader',
-            });
-            const contributorWrapper = createAndAppend('div', contributorSection, {
-              class: 'contributorWrapper',
-            });
-            return fetch(allData.contributors_url)
-              .then(contributorData => contributorData.json())
-              .then(contData => {
-                contData.forEach(contributor => {
-                  const contributorInfo = createAndAppend('a', contributorWrapper, {
-                    href: contributor.html_url,
-                    target: '_blank',
-                    class: 'contributorWrapper',
-                  });
-                  const contributorDiv = createAndAppend('div', contributorInfo, {
-                    class: 'contributor',
-                  });
-                  createAndAppend('img', contributorDiv, {
-                    class: 'contImage',
-                    src: contributor.avatar_url,
-                  });
-                  createAndAppend('h4', contributorDiv, {
-                    class: 'contName',
-                    text: contributor.login,
-                  });
-                  createAndAppend('span', contributorDiv, {
-                    class: 'contNumber',
-                    text: contributor.contributions,
-                  });
-                });
-              })
-              .catch(err => {
-                createAndAppend('div', root, { text: err.message, class: 'alert-error' });
-              });
-          });
+  function fetchAllRepoData(url) {
+    return fetch(url)
+      .then(res => res.json())
+      .then(fetchedData => {
+        const navDiv = createAndAppend('div', root, { class: 'nav' });
+        const navHeader = createAndAppend('div', navDiv, { class: 'nav-header' });
+        const navTitle = createAndAppend('div', navHeader, { class: 'nav-title' });
+        navTitle.innerText = 'HYF Repositories';
+        const selectEl = createAndAppend('select', navDiv, { id: 'getRepoData' });
+        fetchedData.sort((a, b) => (a.name.toLowerCase() < b.name.toLowerCase() ? -1 : 1));
+        fetchedData.forEach(res => {
+          createAndAppend('option', selectEl, { text: res.name, value: res.id });
         });
-    }
-    selectRepo.addEventListener('change', fetchSingleData);
-    fetchSingleData();
+        const selectRepo = document.getElementById('getRepoData');
+        const container = createAndAppend('div', root, { class: 'container' });
+        fetchSingleRepoData(fetchedData, selectRepo, container);
+        function getData() {
+          fetchSingleRepoData(fetchedData, selectRepo, container);
+        }
+        selectRepo.addEventListener('change', getData);
+      })
+      .catch(err => {
+        createAndAppend('div', root, { text: err.message, class: 'alert-error' });
+      });
   }
 
   function main(url) {
-    fetchAllRepoData(url).then(() => getRepoData(url));
+    fetchAllRepoData(url);
   }
 
   const HYF_REPOS_URL = 'https://api.github.com/orgs/HackYourFuture/repos?per_page=100';
