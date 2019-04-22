4d1f8c1c2711f131775526c1c4dc97fb3fdd3fd4
ghifo <41265609+ghifo@users.noreply.github.com>
2019-04-23 00:05:24 +0200

    Update index.js

--- homework/index.js
+++ homework/index.js
@@ -107,19 +107,19 @@
     // list items & data
     const contributors = data => {
       data.forEach(user => {
-        const link = createAndAppend('a', ul, {
-          href: selectedRepository.html_url,
-          target: '_blank',
-          class: 'link',
+        const li = createAndAppend('li', ul, {
+          class: 'contributor-item',
         });
-        const li = createAndAppend('li', link, { class: 'contributor-item' });
+        li.addEventListener('click', () => {
+          window.open(user.html_url, '_blank');
+        });
+
         createAndAppend('img', li, { src: user.avatar_url, class: 'contributor-avatar' });
         const liDiv = createAndAppend('div', li, { class: 'contributor-data' });
         createAndAppend('div', liDiv, { text: user.login });
         createAndAppend('div', liDiv, { text: user.contributions, class: 'contributor-badge' });
       });
     };
-
     // fetchJSON(calling) to check & show the list of the right div
     fetchJSON(selectedRepository.contributors_url).then(data =>
       contributors(data, contributorsHeader),
