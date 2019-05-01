85821e955addc6c143023386938b3e093c1aa88b
WadeeaKiwan <wadeea.kiwan@hotmail.com>
2019-05-01 22:41:13 +0200

    async/await try/catch

--- homework/index.js
+++ homework/index.js
@@ -64,60 +64,59 @@
   }
 
   // Creating the contributions part
-  function createContributors(rightDiv, url) {
+  async function createContributors(rightDiv, url) {
     createAndAppend('h3', rightDiv, {
       text: 'Contributions: ',
       class: 'contributor-header',
     });
-    fetchJSON(url)
-      .then(contributors => {
-        if (!(contributors && contributors.length)) {
-          createAndAppend('div', rightDiv, {
-            text: 'No contributions found.',
-            class: 'alert-error',
-          });
-        } else {
-          const ul = createAndAppend('ul', rightDiv, {
-            class: 'contributor-list',
-          });
-          contributors.forEach(contributor => {
-            const contributorItem = createAndAppend('li', ul, {
-              class: 'contributor-item',
-            });
-            const contributorLink = createAndAppend('a', contributorItem, {
-              href: contributor.html_url,
-              target: '_blank',
-            });
-            const contributorDiv = createAndAppend('div', contributorLink, {
-              class: 'contributor',
-            });
-            createAndAppend('img', contributorDiv, {
-              src: contributor.avatar_url,
-              class: 'contributor-avatar',
-            });
-            const contributorData = createAndAppend('div', contributorDiv, {
-              class: 'contributor-data',
-            });
-            createAndAppend('div', contributorData, {
-              text: contributor.login,
-              class: 'contributor-login',
-            });
-            createAndAppend('div', contributorData, {
-              text: contributor.contributions,
-              class: 'contributor-badge',
-            });
-          });
-        }
-      })
-      .catch(err =>
+    try {
+      const contributors = await fetchJSON(url);
+      if (!(contributors && contributors.length)) {
         createAndAppend('div', rightDiv, {
-          text: err.message,
+          text: 'No contributions found.',
           class: 'alert-error',
-        }),
-      );
+        });
+      } else {
+        const ul = createAndAppend('ul', rightDiv, {
+          class: 'contributor-list',
+        });
+        contributors.forEach(contributor => {
+          const contributorItem = createAndAppend('li', ul, {
+            class: 'contributor-item',
+          });
+          const contributorLink = createAndAppend('a', contributorItem, {
+            href: contributor.html_url,
+            target: '_blank',
+          });
+          const contributorDiv = createAndAppend('div', contributorLink, {
+            class: 'contributor',
+          });
+          createAndAppend('img', contributorDiv, {
+            src: contributor.avatar_url,
+            class: 'contributor-avatar',
+          });
+          const contributorData = createAndAppend('div', contributorDiv, {
+            class: 'contributor-data',
+          });
+          createAndAppend('div', contributorData, {
+            text: contributor.login,
+            class: 'contributor-login',
+          });
+          createAndAppend('div', contributorData, {
+            text: contributor.contributions,
+            class: 'contributor-badge',
+          });
+        });
+      }
+    } catch (err) {
+      createAndAppend('div', rightDiv, {
+        text: err.message,
+        class: 'alert-error',
+      });
+    }
   }
 
-  function main(url) {
+  async function main(url) {
     const root = document.getElementById('root');
 
     // Creating the header and the select menu
@@ -131,41 +130,40 @@
     const selectMenu = createAndAppend('select', header, {
       id: 'repo-selector',
     });
-    fetchJSON(url)
-      .then(hyfRepos => {
-        // Sorting the list of repositories alphabetically on repository name.
-        hyfRepos.sort((a, b) => a.name.localeCompare(b.name));
+    try {
+      const hyfRepos = await fetchJSON(url);
+      // Sorting the list of repositories alphabetically on repository name.
+      hyfRepos.sort((a, b) => a.name.localeCompare(b.name));
 
-        // Pushing the repository name to the option element.
-        hyfRepos.forEach((hyfRepo, index) => {
-          createAndAppend('option', selectMenu, {
-            text: hyfRepo.name,
-            value: index,
-          });
+      // Pushing the repository name to the option element.
+      hyfRepos.forEach((hyfRepo, index) => {
+        createAndAppend('option', selectMenu, {
+          text: hyfRepo.name,
+          value: index,
         });
+      });
 
-        // Creating the container of main information.
-        const container = createAndAppend('main', root, { class: 'container' });
-        const leftDiv = createAndAppend('div', container, { class: 'left-div' });
-        const rightDiv = createAndAppend('div', container, { class: 'right-div' });
-        createHyfRepoDetails(leftDiv, hyfRepos[0]);
-        createContributors(rightDiv, hyfRepos[0].contributors_url);
+      // Creating the container of main information.
+      const container = createAndAppend('main', root, { class: 'container' });
+      const leftDiv = createAndAppend('div', container, { class: 'left-div' });
+      const rightDiv = createAndAppend('div', container, { class: 'right-div' });
+      createHyfRepoDetails(leftDiv, hyfRepos[0]);
+      createContributors(rightDiv, hyfRepos[0].contributors_url);
 
-        // Adding event listener
-        selectMenu.addEventListener('change', () => {
-          leftDiv.innerText = '';
-          rightDiv.innerText = '';
-          const index = selectMenu.value; // or it could be used with a method of select .selectedIndex
-          createHyfRepoDetails(leftDiv, hyfRepos[index]);
-          createContributors(rightDiv, hyfRepos[index].contributors_url);
-        });
-      })
-      .catch(err =>
-        createAndAppend('div', root, {
-          text: err.message,
-          class: 'alert-error',
-        }),
-      );
+      // Adding event listener
+      selectMenu.addEventListener('change', () => {
+        leftDiv.innerText = '';
+        rightDiv.innerText = '';
+        const index = selectMenu.value; // or it could be used with a method of select .selectedIndex
+        createHyfRepoDetails(leftDiv, hyfRepos[index]);
+        createContributors(rightDiv, hyfRepos[index].contributors_url);
+      });
+    } catch (err) {
+      createAndAppend('div', root, {
+        text: err.message,
+        class: 'alert-error',
+      });
+    }
   }
 
   const HYF_REPOS_URL = 'https://api.github.com/orgs/HackYourFuture/repos?per_page=100';
