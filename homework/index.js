dfd71604b8e80cbca53c44f90c2bb4e0b687fd86
saidcetinkaya <saidcetinkaya@github.com>
2019-05-09 17:19:36 +0200

    Said JS3 Week2

--- homework/index.js
+++ homework/index.js
@@ -1,19 +1,21 @@
 'use strict';
 
 {
-  function fetchJSON(url, cb) {
-    const xhr = new XMLHttpRequest();
-    xhr.open('GET', url);
-    xhr.responseType = 'json';
-    xhr.onload = () => {
-      if (xhr.status < 400) {
-        cb(null, xhr.response);
-      } else {
-        cb(new Error(`Network error: ${xhr.status} - ${xhr.statusText}`));
-      }
-    };
-    xhr.onerror = () => cb(new Error('Network request failed'));
-    xhr.send();
+  function fetchJSON(url) {
+    return new Promise((resolve, reject) => {
+      const xhr = new XMLHttpRequest();
+      xhr.open('GET', url);
+      xhr.responseType = 'json';
+      xhr.onload = () => {
+        if (xhr.status < 400) {
+          resolve(xhr.response);
+        } else {
+          reject(new Error(`Network error: ${xhr.status} - ${xhr.statusText}`));
+        }
+      };
+      xhr.onerror = () => reject(new Error('Network request failed'));
+      xhr.send();
+    });
   }
 
   function createAndAppend(name, parent, options = {}) {
@@ -30,78 +32,88 @@
     return elem;
   }
 
+  function removeChildren(element) {
+    while (element.firstChild) {
+      element.removeChild(element.firstChild);
+    }
+  }
+
+  function handleError(error) {
+    const root = document.getElementById('root');
+    removeChildren(root);
+    createAndAppend('div', root, { text: error.message, class: 'alert-error' });
+  }
+
   function pageConstructor(data) {
     data.sort((a, b) => a.name.localeCompare(b.name));
+    const root = document.getElementById('root');
     //HEADER
     const headerDiv = createAndAppend('div', root, { class: 'headerDiv' });
-    createAndAppend('p', headerDiv, {text:'HYF Repositories'})
-    const headerSelect = createAndAppend('select', headerDiv, {class: 'headerSelect' });
-    data.map((elem, index) => createAndAppend('option', headerSelect, { text: elem.name, value: index }));
+    createAndAppend('p', headerDiv, { text: 'HYF Repositories' });
+    const headerSelect = createAndAppend('select', headerDiv, { class: 'headerSelect' });
+    data.map((elem, index) =>
+      createAndAppend('option', headerSelect, { text: elem.name, value: index }),
+    );
     //CONTAINER
     const container = createAndAppend('div', root, { class: 'container' });
-    //REPOSİTORY INFO
+    //REPOSITORY INFO
     const repoInfoDiv = createAndAppend('div', container, { class: 'repoInfoDiv' });
     const ul = createAndAppend('ul', repoInfoDiv);
     const repository = createAndAppend('li', ul, { text: `Repository :` });
-    createAndAppend('a', repository, { text: `${data[0].name}`, href: data[0].html_url, target: '_blank', id: 'repository' });
-    createAndAppend('li', ul, { text: `Description :${data[0].description}`, id:'description'});
+    createAndAppend('a', repository, {
+      text: `${data[0].name}`,
+      href: data[0].html_url,
+      target: '_blank',
+      id: 'repository',
+    });
+    createAndAppend('li', ul, { text: `Description :${data[0].description}`, id: 'description' });
     createAndAppend('li', ul, { text: `Forks :${data[0].forks}`, id: 'forks' });
-    createAndAppend('li', ul, { text: `Update :${data[0].updated_at}`, id: 'update'});
+    createAndAppend('li', ul, { text: `Update :${data[0].updated_at}`, id: 'update' });
     //CONTRIBUTOR
     const contributorDiv = createAndAppend('div', container, { class: 'contributorDiv' });
-    createAndAppend('p', contributorDiv, {text:'Contributions'});
-    const contributorUl= createAndAppend('ul', contributorDiv, { class: 'contributorUl'})
-    function contributor() {
-      fetchJSON(data[headerSelect.value].contributors_url, (err, dt) => {
-      //REMOVE CHILD
-      if (err) {
-        createAndAppend('div', root, { text: err.message, class: 'alert-error' });
-      } else {
-        while (contributorUl.firstChild) {
-          contributorUl.removeChild(contributorUl.firstChild);
-        }
-      }
+    createAndAppend('p', contributorDiv, { text: 'Contributions' });
+    const contributorUl = createAndAppend('ul', contributorDiv, { class: 'contributorUl' });
+    function contributor(contributorData) {
       //ADD CONTRIBUTORS
-      for (let elem in dt) {
+      for (let i = 0; i < contributorData.length; i++) {
         const li = createAndAppend('li', contributorUl);
-        const aForLi = createAndAppend('a', li, { href: dt[elem].html_url, target: '_blank' });
-        createAndAppend('img', aForLi, { src: dt[elem].avatar_url });
-        createAndAppend('p', aForLi, { text: dt[elem].login });
-        createAndAppend('p', aForLi, { text: dt[elem].contributions, class: 'contNumber' })
+        const aForLi = createAndAppend('a', li, {
+          href: contributorData[i].html_url,
+          target: '_blank',
+        });
+        createAndAppend('img', aForLi, { src: contributorData[i].avatar_url });
+        createAndAppend('p', aForLi, { text: contributorData[i].login });
+        createAndAppend('p', aForLi, {
+          text: contributorData[i].contributions,
+          class: 'contNumber',
+        });
       }
-      });
     }
-    contributor();
+    const contributorUrl = data[0].contributors_url;
+    fetchJSON(contributorUrl).then(contributorData => contributor(contributorData));
+
     //EVENTLISTENER
     headerSelect.addEventListener('change', () => {
+      const change = data[event.target.value];
       const newA = document.getElementById('repository');
-      newA.textContent = data[event.target.value].name;
-      newA.href = data[event.target.value].html_url;
+      newA.textContent = change.name;
+      newA.href = change.html_url;
       const newDescription = document.getElementById('description');
-      newDescription.textContent = `Description :${data[event.target.value].description}`;
+      newDescription.textContent = `Description :${change.description}`;
       const newForks = document.getElementById('forks');
-      newForks.textContent = `Forks :${data[event.target.value].forks}`;
+      newForks.textContent = `Forks :${change.forks}`;
       const newUpdate = document.getElementById('update');
-      newUpdate.textContent = `Update :${data[event.target.value].updated_at}`;
-      contributor();
-    })
-  }
+      newUpdate.textContent = `Update :${change.updated_at}`;
 
-  function main(url) {
-    fetchJSON(url, (err, data) => {
-      const root = document.getElementById('root');
-      if (err) {
-        const headerDiv = createAndAppend('div', root, { class: 'headerDiv' });
-        createAndAppend('p', headerDiv, { text: 'HYF Repositories' })
-        const headerSelect = createAndAppend('select', headerDiv, { class: 'headerSelect' });
-        createAndAppend('div', root, { text: err.message, class: 'alert-error' });
-      } else {
-        pageConstructor(data);
-      }
+      removeChildren(contributorUl);
+      const contributorUrls = change.contributors_url;
+      fetchJSON(contributorUrls).then(contributorData => contributor(contributorData));
     });
   }
 
   const HYF_REPOS_URL = 'https://api.github.com/orgs/HackYourFuture/repos?per_page=100';
 
-  window.onload = () => main(HYF_REPOS_URL);
+  fetchJSON(HYF_REPOS_URL)
+    .then(data => pageConstructor(data))
+    .catch(error => handleError(error));
 }
