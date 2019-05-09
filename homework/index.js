b75696bab4a7365956e155796de26a651f6f0272
saidcetinkaya <45071496+saidcetinkaya@users.noreply.github.com>
2019-05-09 15:48:01 +0200

    Update index.js

--- homework/index.js
+++ homework/index.js
@@ -32,43 +32,52 @@
 
   function pageConstructor(data) {
     data.sort((a, b) => a.name.localeCompare(b.name));
+    const root = document.getElementById('root');
     //HEADER
     const headerDiv = createAndAppend('div', root, { class: 'headerDiv' });
-    createAndAppend('p', headerDiv, {text:'HYF Repositories'})
-    const headerSelect = createAndAppend('select', headerDiv, {class: 'headerSelect' });
-    data.map((elem, index) => createAndAppend('option', headerSelect, { text: elem.name, value: index }));
+    createAndAppend('p', headerDiv, { text: 'HYF Repositories' });
+    const headerSelect = createAndAppend('select', headerDiv, { class: 'headerSelect' });
+    data.map((elem, index) =>
+      createAndAppend('option', headerSelect, { text: elem.name, value: index }),
+    );
     //CONTAINER
     const container = createAndAppend('div', root, { class: 'container' });
     //REPOSİTORY INFO
     const repoInfoDiv = createAndAppend('div', container, { class: 'repoInfoDiv' });
     const ul = createAndAppend('ul', repoInfoDiv);
     const repository = createAndAppend('li', ul, { text: `Repository :` });
-    createAndAppend('a', repository, { text: `${data[0].name}`, href: data[0].html_url, target: '_blank', id: 'repository' });
-    createAndAppend('li', ul, { text: `Description :${data[0].description}`, id:'description'});
+    createAndAppend('a', repository, {
+      text: `${data[0].name}`,
+      href: data[0].html_url,
+      target: '_blank',
+      id: 'repository',
+    });
+    createAndAppend('li', ul, { text: `Description :${data[0].description}`, id: 'description' });
     createAndAppend('li', ul, { text: `Forks :${data[0].forks}`, id: 'forks' });
-    createAndAppend('li', ul, { text: `Update :${data[0].updated_at}`, id: 'update'});
+    createAndAppend('li', ul, { text: `Update :${data[0].updated_at}`, id: 'update' });
     //CONTRIBUTOR
     const contributorDiv = createAndAppend('div', container, { class: 'contributorDiv' });
-    createAndAppend('p', contributorDiv, {text:'Contributions'});
-    const contributorUl= createAndAppend('ul', contributorDiv, { class: 'contributorUl'})
+    createAndAppend('p', contributorDiv, { text: 'Contributions' });
+    const contributorUl = createAndAppend('ul', contributorDiv, { class: 'contributorUl' });
     function contributor() {
       fetchJSON(data[headerSelect.value].contributors_url, (err, dt) => {
-      //REMOVE CHILD
-      if (err) {
-        createAndAppend('div', root, { text: err.message, class: 'alert-error' });
-      } else {
-        while (contributorUl.firstChild) {
-          contributorUl.removeChild(contributorUl.firstChild);
+        const root = document.getElementById('root');
+        //REMOVE CHILD
+        if (err) {
+          createAndAppend('div', root, { text: err.message, class: 'alert-error' });
+        } else {
+          while (contributorUl.firstChild) {
+            contributorUl.removeChild(contributorUl.firstChild);
+          }
         }
-      }
-      //ADD CONTRIBUTORS
-      for (let elem in dt) {
-        const li = createAndAppend('li', contributorUl);
-        const aForLi = createAndAppend('a', li, { href: dt[elem].html_url, target: '_blank' });
-        createAndAppend('img', aForLi, { src: dt[elem].avatar_url });
-        createAndAppend('p', aForLi, { text: dt[elem].login });
-        createAndAppend('p', aForLi, { text: dt[elem].contributions, class: 'contNumber' })
-      }
+        //ADD CONTRIBUTORS
+        for (const elem in dt) {
+          const li = createAndAppend('li', contributorUl);
+          const aForLi = createAndAppend('a', li, { href: dt[elem].html_url, target: '_blank' });
+          createAndAppend('img', aForLi, { src: dt[elem].avatar_url });
+          createAndAppend('p', aForLi, { text: dt[elem].login });
+          createAndAppend('p', aForLi, { text: dt[elem].contributions, class: 'contNumber' });
+        }
       });
     }
     contributor();
@@ -84,7 +93,7 @@
       const newUpdate = document.getElementById('update');
       newUpdate.textContent = `Update :${data[event.target.value].updated_at}`;
       contributor();
-    })
+    });
   }
 
   function main(url) {
@@ -92,8 +101,8 @@
       const root = document.getElementById('root');
       if (err) {
         const headerDiv = createAndAppend('div', root, { class: 'headerDiv' });
-        createAndAppend('p', headerDiv, { text: 'HYF Repositories' })
-        const headerSelect = createAndAppend('select', headerDiv, { class: 'headerSelect' });
+        createAndAppend('p', headerDiv, { text: 'HYF Repositories' });
+        createAndAppend('select', headerDiv, { class: 'headerSelect' });
         createAndAppend('div', root, { text: err.message, class: 'alert-error' });
       } else {
         pageConstructor(data);
