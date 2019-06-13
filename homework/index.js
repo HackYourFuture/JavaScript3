d3fe6e012a450784e34ee9240905356f719d3750
alquinex <alquinex2010@hotmail.com>
2019-06-13 23:59:29 +0200

    JavaScript3 homework week1

--- homework/index.js
+++ homework/index.js
@@ -30,13 +30,93 @@
     return elem;
   }
 
-  function main(url) {
-    fetchJSON(url, (err, data) => {
-      const root = document.getElementById('root');
+  function renderContributors(contributors, contributorsContainer) {
+    createAndAppend('p', contributorsContainer, { text: 'Contributors', id: 'contributors' });
+    contributors.forEach(contributor => {
+      const contributorsDetailsDiv = createAndAppend('div', contributorsContainer, {
+        class: 'contributor-details',
+      });
+      createAndAppend('img', contributorsDetailsDiv, {
+        src: contributor.avatar_url,
+        alt: contributor.login,
+        class: 'image',
+      });
+      createAndAppend('p', contributorsDetailsDiv, {
+        text: contributor.login,
+        href: contributor.html_url,
+        target: '_blank',
+        class: 'contributor-data',
+      });
+      createAndAppend('p', contributorsDetailsDiv, {
+        text: contributor.contributions,
+        class: 'contributor-badge',
+      });
+    });
+  }
+
+  function fetchAndRenderData(selectedRepo, repoContainer, contributorsContainer, root) {
+    repoContainer.innerHTML = '';
+    contributorsContainer.innerHTML = '';
+
+    const updatedAt = new Date(selectedRepo.updated_at);
+    createAndAppend('p', repoContainer, {
+      text: `Repository: ${selectedRepo.name}`,
+      class: 'repo-child',
+    });
+    createAndAppend('p', repoContainer, {
+      text: `Description: ${selectedRepo.description}`,
+      class: 'repo-child',
+    });
+    createAndAppend('p', repoContainer, {
+      text: `Fork: ${selectedRepo.forks}`,
+      class: 'repo-child',
+    });
+    createAndAppend('p', repoContainer, {
+      text: `Updated: ${updatedAt.toLocaleString()}`,
+      class: 'repo-child',
+    });
+
+    fetchJSON(selectedRepo.contributors_url, (err, contributors) => {
       if (err) {
         createAndAppend('div', root, { text: err.message, class: 'alert-error' });
       } else {
-        createAndAppend('pre', root, { text: JSON.stringify(data, null, 2) });
+        renderContributors(contributors, contributorsContainer);
+      }
+    });
+  }
+
+  function dropDown(root, repos) {
+    const header = createAndAppend('div', root, { id: 'header' });
+    createAndAppend('p', header, { text: 'HYF Repositories', class: 'header' });
+    const select = createAndAppend('select', header, { id: 'select' });
+
+    repos.forEach((repo, index) => {
+      createAndAppend('option', select, {
+        text: repo.name,
+        value: index,
+      });
+    });
+
+    const mainContainer = createAndAppend('div', root, { id: 'main' });
+    const repoContainer = createAndAppend('div', mainContainer, { id: 'repo-container' });
+    const contributorsContainer = createAndAppend('div', mainContainer, {
+      id: 'contributor-container',
+    });
+
+    select.addEventListener('change', () => {
+      const selectedRepo = repos[select.value];
+      fetchAndRenderData(selectedRepo, repoContainer, contributorsContainer, root);
+    });
+    fetchAndRenderData(repos[0], repoContainer, contributorsContainer, root);
+  }
+
+  function main(url) {
+    const root = document.getElementById('root');
+    fetchJSON(url, (err, repositories) => {
+      if (err) {
+        createAndAppend('div', root, { text: err.message, class: 'alert-error' });
+      } else {
+        dropDown(root, repositories);
       }
     });
   }
