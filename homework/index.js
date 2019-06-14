a3af11dfc5241893da0ef7f380951c1cf6fd8899
zehrakopar <zehrakopar@yahoo.com.tr>
2019-06-14 16:18:23 +0200

    week1

--- homework/index.js
+++ homework/index.js
@@ -30,13 +30,62 @@
     return elem;
   }
 
+  function reposAbstract(index, data) {
+    const repo = data[index];
+    const leftDiv = document.querySelector('.leftDiv');
+    const table = createAndAppend('table', leftDiv, { class: 'table' });
+    const tBody = createAndAppend('tbody', table);
+    const repositoryRow = createAndAppend('tr', tBody, { class: 'row' });
+    createAndAppend('td', repositoryRow, { text: 'Repository :', class: 'label' });
+    const repositoryRowSecondTd = createAndAppend('td', repositoryRow);
+    createAndAppend('a', repositoryRowSecondTd, {
+      href: repo.html_url,
+      target: '_blank',
+      text: repo.name,
+    });
+    const descriptionRow = createAndAppend('tr', tBody, { class: 'row' });
+    if (repo.description !== null) {
+      createAndAppend('td', descriptionRow, {
+        text: 'Description :',
+        class: 'label',
+      });
+      createAndAppend('td', descriptionRow, { text: repo.description });
+    }
+
+    const forksRow = createAndAppend('tr', tBody, { class: 'row' });
+    createAndAppend('td', forksRow, { text: 'Forks :', class: 'label' });
+    createAndAppend('td', forksRow, { text: repo.forks });
+    const dateRow = createAndAppend('tr', tBody, { class: 'row' });
+    createAndAppend('td', dateRow, {
+      text: 'Updated :',
+      class: 'label',
+    });
+    createAndAppend('td', dateRow, {
+      text: new Date(repo.updated_at).toLocaleString(),
+    });
+  }
+
   function main(url) {
+    const root = document.getElementById('root');
     fetchJSON(url, (err, data) => {
-      const root = document.getElementById('root');
+      const header = createAndAppend('header', root, { class: 'header' });
+      const h1 = createAndAppend('p', header, { text: 'HYF Repositories' });
+      const selector = createAndAppend('select', header, { class: 'repo-selector' });
       if (err) {
-        createAndAppend('div', root, { text: err.message, class: 'alert-error' });
+        createAndAppend('p', root, { text: err.message, class: 'alert' });
       } else {
-        createAndAppend('pre', root, { text: JSON.stringify(data, null, 2) });
+        data.sort((a, b) => a.name.toLowerCase().localeCompare(b.name.toLowerCase()));
+        for (let i = 0; i < data.length; i++) {
+          createAndAppend('option', selector, { text: data[i].name, value: i });
+        }
+        selector.addEventListener('change', () => {
+          document.querySelector('.leftDiv').innerHTML = '';
+          reposAbstract(selector.value, data);
+        });
+        const container = createAndAppend('div', root, { id: 'container' });
+        createAndAppend('div', container, { class: 'leftDiv' });
+        createAndAppend('div', container, { class: 'rightDiv' });
+        reposAbstract(selector.value, data);
       }
     });
   }
