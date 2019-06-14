ff01ab1143821ae990493130b8856ab2f0fc50dd
irfan <ikrdnz@gmail.com>
2019-06-14 22:49:55 +0200

    week1-irfan

--- homework/index.js
+++ homework/index.js
@@ -30,13 +30,103 @@
     return elem;
   }
 
+  function removeChildren(element) {
+    while (element.firstChild) {
+      element.removeChild(element.firstChild);
+    }
+  }
+
   function main(url) {
     fetchJSON(url, (err, data) => {
       const root = document.getElementById('root');
       if (err) {
         createAndAppend('div', root, { text: err.message, class: 'alert-error' });
       } else {
-        createAndAppend('pre', root, { text: JSON.stringify(data, null, 2) });
+        const jsonString = JSON.stringify(data, null, 2);
+        const menu = createAndAppend('div', root, {
+          id: 'menuSelect',
+        });
+        createAndAppend('h2', menu, { text: 'HYF Repositories' });
+        const select = createAndAppend('select', menu, {
+          id: 'selection',
+        });
+        const repoDetails = createAndAppend('div', root, {
+          id: 'repoDetails',
+        });
+        createAndAppend('p', repoDetails, {
+          id: 'repoDescriptionP',
+        });
+        createAndAppend('p', repoDetails, {
+          id: 'forks',
+        });
+
+        createAndAppend('p', repoDetails, {
+          id: 'updateDate',
+        });
+
+        const renewalSpace = createAndAppend('div', root, {
+          id: 'contributors',
+        });
+
+        let valueNumber = 0;
+        const repositories = JSON.parse(jsonString, null);
+        let contributorsURL = '';
+
+        repositories.forEach(repo => {
+          const options = createAndAppend('option', select, {
+            value: valueNumber,
+          });
+          options.textContent = repo.name;
+          valueNumber += 1;
+        });
+        const selectElement = document.getElementById('selection');
+        selectElement.addEventListener('change', event => {
+          removeChildren(renewalSpace);
+          const description = document.getElementById('repoDescriptionP');
+          const forks = document.getElementById('forks');
+          const updateDate = document.getElementById('updateDate');
+          description.innerHTML = `Description: ${repositories[event.target.value].description}`;
+          forks.innerHTML = `Forks: ${repositories[event.target.value].forks}`;
+          updateDate.innerHTML = `Updated at: ${repositories[event.target.value].updated_at}`;
+
+          contributorsURL = repositories[event.target.value].contributors_url;
+
+          fetchJSON(contributorsURL, (err1, data1) => {
+            const contributor = document.getElementById('root');
+
+            createAndAppend('div', contributor, {
+              id: 'contributors',
+            });
+
+            if (err1) {
+              createAndAppend('div', contributor, {
+                text: err1.message,
+                class: 'alert-error',
+              });
+            } else {
+              const contributorsJSON = JSON.stringify(data1, null, 2);
+              const parsedContributorsData = JSON.parse(contributorsJSON, null);
+
+              const contributorDetails = document.getElementById('contributors');
+              createAndAppend('h2', contributorDetails, {
+                text: 'Contributors',
+              });
+              parsedContributorsData.forEach(repo => {
+                createAndAppend('img', contributorDetails, {
+                  src: `${repo.avatar_url}`,
+                });
+                createAndAppend('div', contributorDetails, {
+                  id: 'repoContribNames',
+                  text: `${repo.login}`,
+                });
+                createAndAppend('div', contributorDetails, {
+                  id: 'repoContribNumbers',
+                  text: `${repo.contributions}`,
+                });
+              });
+            }
+          });
+        });
       }
     });
   }
