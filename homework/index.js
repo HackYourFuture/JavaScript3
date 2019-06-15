e719ecdc88a518c958e41f1e9b1bf8acf6071497
Ghufran Deiri <ghufran@Ghufrans-MacBook-Pro.local>
2019-06-15 22:16:59 +0200

    Add some improvements

--- homework/index.js
+++ homework/index.js
@@ -29,8 +29,20 @@
     });
     return elem;
   }
+
+  // a function to create td without link
+  function createTd(textKey, textContent, tbody) {
+    const parent = createAndAppend('tr', tbody);
+    createAndAppend('td', parent, {
+      text: `${textKey}: `,
+      class: 'label',
+    });
+    createAndAppend('td', parent, {
+      text: textContent,
+    });
+  }
   // table of repository's basic information
-  function createDetailsTable(index, data, container) {
+  function createDetailsTable(repository, container) {
     const repositoryData = createAndAppend('div', container, { class: 'left-div white-frame' });
     const table = createAndAppend('table', repositoryData);
     const tbody = createAndAppend('tbody', table);
@@ -42,67 +54,55 @@
     // manually create td with a link
     const td = createAndAppend('td', tr);
     createAndAppend('a', td, {
-      href: data[index].html_url,
-      text: data[index].name,
+      href: repository.html_url,
+      text: repository.name,
       target: '_blank',
     });
-    // a function to create td without link
-    function createTd(textKey, textContent) {
-      const parent = createAndAppend('tr', tbody);
-      createAndAppend('td', parent, {
-        text: `${textKey}: `,
-        class: 'label',
-      });
-      createAndAppend('td', parent, {
-        text: textContent,
-      });
-    }
-    createTd('Description', data[index].description);
-    createTd('Forks', data[index].forks);
-    createTd('Updated', data[index].updated_at);
+    createTd('Description', repository.description, tbody);
+    createTd('Forks', repository.forks, tbody);
+    createTd('Updated', repository.updated_at, tbody);
   }
 
   // contributors list
-  function listContributors(index, data, container) {
-    const contributorUrl = data[index].contributors_url;
+  function listContributors(repository, container) {
+    const contributorUrl = repository.contributors_url;
     fetchJSON(contributorUrl, (err, contributors) => {
       const root = document.getElementById('root');
       if (err) {
         createAndAppend('div', root, { text: err.message, class: 'alert-error' });
-      } else {
-        const contributorsData = createAndAppend('div', container, {
-          class: 'right-div white-frame',
-        });
-        createAndAppend('p', contributorsData, {
-          text: 'contributions',
-          class: 'contributor-header',
-        });
-        const ul = createAndAppend('ul', contributorsData, {
-          class: 'contributor-list',
-        });
-        contributors.forEach(contributor => {
-          const li = createAndAppend('li', ul, {
-            class: 'contributor-item',
-          });
-          // The open() method creates a new secondary browser window,
-          // a new blank, empty window (URL about:blank) is created with the default toolbars of the main window.
-          li.addEventListener('click', () => {
-            window.open(contributor.html_url);
-          });
-
-          createAndAppend('img', li, {
-            src: contributor.avatar_url,
-            alt: "contributor's profile",
-            class: 'contributor-avatar',
-          });
-          const liDiv = createAndAppend('div', li, { class: 'contributor-data' });
-          createAndAppend('div', liDiv, { text: contributor.login });
-          createAndAppend('div', liDiv, {
-            text: contributor.contributions,
-            class: 'contributor-badge',
-          });
-        });
       }
+      const contributorsData = createAndAppend('div', container, {
+        class: 'right-div white-frame',
+      });
+      createAndAppend('p', contributorsData, {
+        text: 'contributions',
+        class: 'contributor-header',
+      });
+      const ul = createAndAppend('ul', contributorsData, {
+        class: 'contributor-list',
+      });
+      contributors.forEach(contributor => {
+        const li = createAndAppend('li', ul, {
+          class: 'contributor-item',
+        });
+        // The open() method creates a new secondary browser window,
+        // a new blank, empty window (URL about:blank) is created with the default toolbars of the main window.
+        li.addEventListener('click', () => {
+          window.open(contributor.html_url);
+        });
+
+        createAndAppend('img', li, {
+          src: contributor.avatar_url,
+          alt: "contributor's profile",
+          class: 'contributor-avatar',
+        });
+        const liDiv = createAndAppend('div', li, { class: 'contributor-data' });
+        createAndAppend('div', liDiv, { text: contributor.login });
+        createAndAppend('div', liDiv, {
+          text: contributor.contributions,
+          class: 'contributor-badge',
+        });
+      });
     });
   }
 
@@ -114,18 +114,16 @@
     repositories.forEach((repository, index) => {
       createAndAppend('option', select, { text: repository.name, value: index });
     });
-    createDetailsTable(0, data, container);
-    listContributors(0, data, container);
+    createDetailsTable(data[0], container);
+    listContributors(data[0], container);
 
-    select.addEventListener('change', event => {
+    select.addEventListener('change', () => {
       while (container.firstChild) {
         container.removeChild(container.firstChild);
       }
-      // event.target refers to the clicked select repository
-      // It will fire when each select repository is clicked
-      const index = event.target.value;
-      createDetailsTable(index, data, container);
-      listContributors(index, data, container);
+      const index = select.value;
+      createDetailsTable(data[index], container);
+      listContributors(data[index], container);
     });
   }
 
@@ -134,15 +132,14 @@
       const root = document.getElementById('root');
       if (err) {
         createAndAppend('div', root, { text: err.message, class: 'alert-error' });
-      } else {
-        const header = createAndAppend('header', root, { class: 'header' });
-        createAndAppend('p', header, { text: 'HYF Repositories' });
-        const container = createAndAppend('div', root, {
-          class: 'container',
-          id: 'container',
-        });
-        selectAndChange(data, container, header);
       }
+      const header = createAndAppend('header', root, { class: 'header' });
+      createAndAppend('p', header, { text: 'HYF Repositories' });
+      const container = createAndAppend('div', root, {
+        class: 'container',
+        id: 'container',
+      });
+      selectAndChange(data, container, header);
     });
   }
 
