9c6992061ae9ba3c718cc33c13b4958cb8dbcc0f
7alip <talipaltas@gmail.com>
2019-06-20 03:34:49 +0200

    spa_week2

--- homework/index.js
+++ homework/index.js
@@ -3,19 +3,21 @@
 'use strict';
 
 {
-  function fetchJSON(url, cb) {
-    const xhr = new XMLHttpRequest();
-    xhr.open('GET', url);
-    xhr.responseType = 'json';
-    xhr.onload = () => {
-      if (xhr.status < 400) {
-        cb(null, xhr.response);
-      } else {
-        cb(new Error(`Network error: ${xhr.status} - ${xhr.statusText}`));
-      }
-    };
-    xhr.onerror = () => cb(new Error('Network request failed'));
-    xhr.send();
+  function fetchJSON(url) {
+    return new Promise((resolve, reject) => {
+      const xhr = new XMLHttpRequest();
+      xhr.open('GET', url);
+      xhr.responseType = 'json';
+      xhr.onload = () => {
+        if (xhr.status < 400) {
+          resolve(xhr.response);
+        } else {
+          reject(new Error(`Network error: ${xhr.status} - ${xhr.statusText}`));
+        }
+      };
+      xhr.onerror = () => reject(new Error('Network request failed'));
+      xhr.send();
+    });
   }
 
   function createAndAppend(name, parent, options = {}) {
@@ -24,7 +26,7 @@
     Object.keys(options).forEach(key => {
       const value = options[key];
       if (key === 'text') {
-        elem.innerHTML = value;
+        elem.textContent = value;
       } else {
         elem.setAttribute(key, value);
       }
@@ -123,8 +125,7 @@
   // Creates html elements from an object
   function createStaticComponentsFromModel(model, root) {
     model.forEach(parent => {
-      createAndAppend(parent.tag, root, parent.attributes);
-      const parentComponent = document.querySelector(`.${parent.attributes.class}`);
+      const parentComponent = createAndAppend(parent.tag, root, parent.attributes);
 
       if (parent.children) {
         createStaticComponentsFromModel(parent.children, parentComponent);
@@ -132,70 +133,67 @@
     });
   }
 
-  // Passes given data into dynamic html parts
-  function fillDataIntoComponents(selectedRepository) {
-    (function fillRepositoryDetailsTable() {
-      const tableModel = [
-        [
-          { class: 'label', text: 'Repository' },
-          { text: `<a href="${selectedRepository.html_url}">${selectedRepository.name}</a>` },
-        ],
-        [
-          { class: 'label', text: 'Description' },
-          {
-            text: selectedRepository.description
-              ? selectedRepository.description
-              : 'No description!',
-          },
-        ],
-        [
-          { class: 'label', text: 'Forks' },
-          { text: selectedRepository.forks ? selectedRepository.forks : 'No forks!' },
-        ],
-        [
-          { class: 'label', text: 'Updated At' },
-          { text: new Date(selectedRepository.updated_at).toLocaleString() },
-        ],
-      ];
+  function fillRepositoryDetailsTable(selectedRepository) {
+    const tableBody = document.querySelector('#table-body');
 
-      const tableBody = document.querySelector('#table-body');
+    const tableContent = [
+      ['Repository', 'name'],
+      ['Description', 'description'],
+      ['Forks', 'forks'],
+      ['Updated', 'updated_at'],
+    ];
 
-      tableModel.forEach(row => {
-        const tableRow = createAndAppend('tr', tableBody);
+    tableContent.forEach((item, index) => {
+      const tableRow = createAndAppend('tr', tableBody);
 
-        row.forEach(column => createAndAppend('td', tableRow, column));
+      if (index === 0) {
+        createAndAppend('td', tableRow, { class: 'label', text: item[0] });
+        const td = createAndAppend('td', tableRow);
+        createAndAppend('a', td, {
+          text: selectedRepository[item[1]],
+          href: selectedRepository.html_url,
+          target: '_blank',
+        });
+      } else {
+        createAndAppend('td', tableRow, { class: 'label', text: item[0] });
+        createAndAppend('td', tableRow, {
+          text: selectedRepository[item[1]]
+            ? selectedRepository[item[1]]
+            : `No ${item[0].toLowerCase()}`,
+        });
+      }
+    });
+  }
+
+  function fillContributorsList(selectedRepository) {
+    const list = document.querySelector('.contributors__list');
+    fetchJSON(selectedRepository.contributors_url).then(contributors => {
+      if (!contributors.length) {
+        createAndAppend('li', list, {
+          class: 'alert-warning',
+          text: 'There is no contributor for this repository!',
+        });
+        return;
+      }
+
+      contributors.forEach(contributor => {
+        const listItem = createAndAppend('li', list, {
+          class: 'contributors__item',
+        });
+        createAndAppend('img', listItem, {
+          class: 'contributors__avatar',
+          src: contributor.avatar_url,
+          alt: contributor.login,
+        });
+        createAndAppend('span', listItem, { class: 'contributors__name', text: contributor.login });
+        createAndAppend('span', listItem, {
+          class: 'contributors__badge',
+          text: contributor.contributions,
+        });
+
+        listItem.addEventListener('click', () => window.open(contributor.html_url));
       });
-    })();
-
-    (function fillContributorsList() {
-      fetchJSON(selectedRepository.contributors_url, (err, contributors) => {
-        const list = document.querySelector('.contributors__list');
-        if (err) {
-          createAndAppend('li', list, { class: 'text-error', text: err.message });
-        } else if (contributors.length) {
-          contributors.forEach(contributor => {
-            const listContent = `
-              <img class="contributors__avatar"
-                src="${contributor.avatar_url}" 
-                alt="${contributor.login}">
-              <span class="contributors__name">${contributor.login}</span>
-              <span class="contributors__badge">${contributor.contributions}</span>
-            `;
-            const listItem = createAndAppend('li', list, {
-              class: 'contributors__item',
-              text: listContent,
-            });
-
-            listItem.addEventListener('click', () => window.open(contributor.html_url));
-          });
-        } else {
-          createAndAppend('li', list, {
-            class: 'alert-warning',
-            text: 'There is no contributor for this repository!',
-          });
-        }
-      });
-    })();
+    });
   }
 
   // Starts all actions
@@ -213,10 +211,13 @@
         createAndAppend('option', select, { text: repository.name, value: index });
       });
 
+      const selectedRepository = repositories[select.selectedIndex];
+
       select.addEventListener('change', () => {
         document.querySelector('tbody').innerHTML = '';
         document.querySelector('.contributors__list').innerHTML = '';
-        fillDataIntoComponents(repositories[select.selectedIndex]);
+        fillRepositoryDetailsTable(selectedRepository);
+        fillContributorsList(selectedRepository);
       });
     })();
 
@@ -225,19 +226,18 @@
       const initialRepositoryIndex = Math.floor(Math.random() * repositories.length);
 
       select.selectedIndex = initialRepositoryIndex;
-      fillDataIntoComponents(repositories[initialRepositoryIndex]);
+      fillRepositoryDetailsTable(repositories[initialRepositoryIndex]);
+      fillContributorsList(repositories[initialRepositoryIndex]);
     })();
   }
 
   function main(url) {
-    fetchJSON(url, (err, data) => {
-      const root = document.getElementById('root');
-      if (err) {
-        createAndAppend('div', root, { text: err.message, class: 'alert-error' });
-      } else {
+    const root = document.getElementById('root');
+    fetchJSON(url)
+      .then(data => {
         init(data, root);
-      }
-    });
+      })
+      .catch(err => createAndAppend('div', root, { text: err.message }));
   }
 
   const HYF_REPOS_URL = 'https://api.github.com/orgs/HackYourFuture/repos?per_page=100';
