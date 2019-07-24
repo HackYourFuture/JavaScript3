46665a36962b453a0c5fdc7b9f3386b9bda46ccd
elicarcar <elisabetta.carcar@hotmail.com>
2019-07-24 23:23:51 +0200

    Basic code construction is almost done, still working on CSS

--- homework/index.js
+++ homework/index.js
@@ -7,6 +7,7 @@
     xhr.responseType = 'json';
     xhr.onload = () => {
       if (xhr.status >= 200 && xhr.status <= 299) {
+        console.log(xhr.response);
         cb(null, xhr.response);
       } else {
         cb(new Error(`Network error: ${xhr.status} - ${xhr.statusText}`));
@@ -14,6 +15,11 @@
     };
     xhr.onerror = () => cb(new Error('Network request failed'));
     xhr.send();
+  }
+
+  function renderError(error) {
+    const root = document.getElementById('root');
+    createAndAppend('h1', root, { text: error.message });
   }
 
   function createAndAppend(name, parent, options = {}) {
@@ -30,14 +36,102 @@
     return elem;
   }
 
-  function main(url) {
-    fetchJSON(url, (err, repositories) => {
-      const root = document.getElementById('root');
+  function createOptions(repoNames, select) {
+    createAndAppend('option', select, { text: 'Select module', disabled: 'disabled' });
+    repoNames.sort((a, b) => a.name.localeCompare(b.name));
+    repoNames.forEach((repository, i) => {
+      createAndAppend('option', select, {
+        text: repository.name,
+        value: repository.name,
+      });
+    });
+  }
+
+  function renderRepoInformation(repo, ul) {
+    const url = `https://api.github.com/repos/HackYourFuture/${repo}`;
+    fetchJSON(url, (err, repoInfos) => {
       if (err) {
-        createAndAppend('div', root, { text: err.message, class: 'alert-error' });
+        renderError(err);
         return;
       }
-      createAndAppend('pre', root, { text: JSON.stringify(repositories, null, 2) });
+
+      const li = createAndAppend('li', ul);
+      const table = createAndAppend('table', li, { class: 'infoTable' });
+      const tbody = createAndAppend('tbody', table);
+      const repoName = createAndAppend('tr', tbody);
+      createAndAppend('td', repoName, { text: 'Repository:', class: 'explanations' });
+      const repoNameTd = createAndAppend('td', repoName, {
+        class: 'list-item',
+      });
+      createAndAppend('a', repoNameTd, {
+        href: `${repoInfos.html_url}`,
+        target: 'blank',
+        text: `${repoInfos.name}`,
+      });
+      const description = createAndAppend('tr', tbody);
+      createAndAppend('td', description, { text: 'Description:', class: 'explanations' });
+      createAndAppend('td', description, { text: `${repoInfos.description}` });
+      const fork = createAndAppend('tr', tbody);
+      createAndAppend('td', fork, { text: 'Forks:', class: 'explanations' });
+      createAndAppend('td', fork, { text: `${repoInfos.forks}` });
+      const updateTime = createAndAppend('tr', tbody);
+      createAndAppend('td', updateTime, { text: 'Updated:', class: 'explanations' });
+      createAndAppend('td', updateTime, { text: `${repoInfos.updated_at}` });
+    });
+  }
+
+  function renderContributorsInformation(subjects) {
+    const url = `https://api.github.com/repos/HackYourFuture/${subjects}/contributors`;
+    fetchJSON(url, (error, contributorDetails) => {
+      if (error) {
+        renderError(error);
+        return;
+      }
+
+      const contrTable = createAndAppend('ul', bodyDiv);
+      const li = createAndAppend('li', contrTable);
+      const table = createAndAppend('table', li, { class: 'contributors-table' });
+      const tbody = createAndAppend('tbody', table);
+      contributorDetails.forEach(contributor => {
+        const tr = createAndAppend('tr', tbody);
+        createAndAppend('img', tr, {
+          src: `${contributor.avatar_url}`,
+          class: 'images',
+        });
+        const contributorsLogin = createAndAppend('td', tr);
+        createAndAppend('a', contributorsLogin, {
+          text: `${contributor.login}`,
+          href: `${contributor.html_url}`,
+          target: 'blank',
+        });
+        createAndAppend('span', tr, {
+          text: `${contributor.contributions}`,
+          class: 'contributions',
+        });
+      });
+    });
+  }
+
+  function main(url) {
+    const root = document.getElementById('root');
+    const bodyDiv = document.getElementById('bodyDiv');
+    createAndAppend('p', root, { text: 'HYF Repositories', id: 'hyfText' });
+    const select = createAndAppend('select', root, { id: 'selectButton' });
+    const ul = createAndAppend('ul', bodyDiv);
+
+    fetchJSON(url, (err, repositories) => {
+      if (err) {
+        renderError(err);
+        return;
+      }
+
+      createOptions(repositories, select);
+
+      select.addEventListener('change', () => {
+        const selectId = select.value;
+        renderRepoInformation(selectId, ul);
+        renderContributorsInformation(selectId);
+      });
     });
   }
 
