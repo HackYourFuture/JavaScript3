befc8cd2cd6cb755101894600675d4253fa0deb2
Oguzhan <oguzhanturgut@outlook.com>
2019-07-24 12:42:08 +0200

    Submit Week1 homework

--- homework/index.js
+++ homework/index.js
@@ -23,6 +23,8 @@
       const value = options[key];
       if (key === 'text') {
         elem.textContent = value;
+      } else if (key === 'html') {
+        elem.innerHTML = value;
       } else {
         elem.setAttribute(key, value);
       }
@@ -30,17 +32,130 @@
     return elem;
   }
 
-  function main(url) {
-    fetchJSON(url, (err, repositories) => {
-      const root = document.getElementById('root');
-      if (err) {
-        createAndAppend('div', root, { text: err.message, class: 'alert-error' });
+  const renderError = error => {
+    const wrapper = document.getElementById('wrapper');
+    wrapper.innerHTML = '';
+    createAndAppend('div', wrapper, {
+      text: error.message,
+      class: 'alert',
+    });
+  };
+
+  const addRepoInfoRow = (parent, label, content) => {
+    const row = createAndAppend('tr', parent);
+    createAndAppend('th', row, {
+      text: label,
+      class: 'table-header',
+    });
+    createAndAppend('td', row, {
+      html: content,
+    });
+  };
+
+  function renderRepoInfo(repository) {
+    const leftSide = document.getElementById('left-side');
+    leftSide.innerHTML = '';
+    const repoTable = createAndAppend('table', leftSide);
+    const repoTableBody = createAndAppend('tbody', repoTable);
+    addRepoInfoRow(
+      repoTableBody,
+      'Repository',
+      `<a href="${repository.html_url}" target="_blank">${repository.name}</a>`,
+    );
+    addRepoInfoRow(repoTableBody, 'Description', repository.description);
+    addRepoInfoRow(
+      repoTableBody,
+      'Created At',
+      new Date(repository.created_at).toLocaleString('en-GB'),
+    );
+    addRepoInfoRow(
+      repoTableBody,
+      'Updated At',
+      new Date(repository.updated_at).toLocaleString('en-GB'),
+    );
+    addRepoInfoRow(repoTableBody, 'Forks', repository.forks_count);
+    addRepoInfoRow(repoTableBody, 'Watchers', repository.watchers_count);
+  }
+
+  function fetchAndRender(repository) {
+    const rightSide = document.getElementById('right-side');
+    rightSide.innerHTML = '';
+    fetchJSON(repository.contributors_url, (error, contributors) => {
+      if (error) {
+        renderError(error);
         return;
       }
-      createAndAppend('pre', root, { text: JSON.stringify(repositories, null, 2) });
+      createAndAppend('h3', rightSide, {
+        text: 'Contributors',
+        class: 'contributors-title',
+      });
+      const contributorsList = createAndAppend('ul', rightSide, {
+        class: 'contributors-list',
+      });
+      contributors.forEach(contributor => {
+        const listItem = createAndAppend('li', contributorsList, {
+          class: 'contributor',
+        });
+        createAndAppend('img', listItem, {
+          class: 'contributor-avatar',
+          src: contributor.avatar_url,
+        });
+        createAndAppend('span', listItem, {
+          class: 'contributor-name',
+          text: contributor.login,
+        });
+        createAndAppend('span', listItem, {
+          class: 'contribution-count',
+          text: contributor.contributions,
+        });
+        listItem.addEventListener('click', () => {
+          window.open(contributor.html_url, '_blank');
+        });
+      });
     });
   }
 
+  function main(url) {
+    const root = document.getElementById('root');
+    const header = createAndAppend('header', root, {
+      class: 'header',
+    });
+    createAndAppend('h1', header, {
+      class: 'header-title',
+      text: 'Hack Your Future Repositories',
+    });
+    const wrapper = createAndAppend('main', root, {
+      id: 'wrapper',
+    });
+    const select = createAndAppend('select', header);
+    fetchJSON(url, (error, repositories) => {
+      if (error) {
+        renderError(error);
+        return;
+      }
+      repositories.sort((a, b) => a.name.localeCompare(b.name));
+      repositories.forEach((repository, index) => {
+        createAndAppend('option', select, {
+          value: index,
+          text: repository.name,
+        });
+      });
+      createAndAppend('main', wrapper, {
+        class: 'left-side',
+        id: 'left-side',
+      });
+      renderRepoInfo(repositories[select.value]);
+      createAndAppend('main', wrapper, {
+        class: 'right-side',
+        id: 'right-side',
+      });
+      fetchAndRender(repositories[select.value]);
+      select.addEventListener('change', () => {
+        renderRepoInfo(repositories[select.value]);
+        fetchAndRender(repositories[select.value]);
+      });
+    });
+  }
   const HYF_REPOS_URL = 'https://api.github.com/orgs/HackYourFuture/repos?per_page=100';
   window.onload = () => main(HYF_REPOS_URL);
 }
