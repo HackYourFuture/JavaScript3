25b675c899e51f49fe0863adabc85831b6f93e03
mustafa <mayqf@mail.misssouri.edu>
2019-07-29 17:59:40 +0200

    revised

--- homework/index.js
+++ homework/index.js
@@ -2,7 +2,7 @@
 
 {
   const HYF_REPOS_URL = 'https://api.github.com/orgs/HackYourFuture/repos?per_page=100';
-  // defined for taking data from url
+
   function fetchJSON(url, callBackFunction) {
     const xhr = new XMLHttpRequest();
     xhr.open('GET', url);
@@ -17,7 +17,7 @@
     xhr.onerror = () => callBackFunction(new Error('Network request failed'));
     xhr.send();
   }
-  // defined for new element creat&append
+
   function createAndAppend(name, parent, options = {}) {
     const child = document.createElement(name);
     parent.appendChild(child);
@@ -31,7 +31,7 @@
     });
     return child;
   }
-  // defined for error handling
+
   function renderError(error) {
     const root = document.getElementById('root');
     createAndAppend('div', root, {
@@ -39,12 +39,8 @@
       class: 'alert',
     });
   }
-  // for creating select element
+
   function createOptionElements(repositories, select) {
-    createAndAppend('option', select, {
-      text: 'Select a repository',
-      disabled: 'disabled',
-    });
     repositories
       .sort((a, b) => a.name.localeCompare(b.name))
       .forEach((repository, index) => {
@@ -55,40 +51,32 @@
       });
   }
 
+  function addRow(tbody, label, value) {
+    const tr = createAndAppend('tr', tbody);
+    createAndAppend('td', tr, {
+      text: `${label}:`,
+      class: 'label',
+    });
+    createAndAppend('td', tr, {
+      text: value,
+    });
+  }
+
   function renderRepositories(repository) {
     const leftBox = document.getElementById('left-box');
     leftBox.innerHTML = '';
-    const rTable = createAndAppend('table', leftBox);
-    const tbody = createAndAppend('tbody', rTable, {
+    const repTable = createAndAppend('table', leftBox);
+    const tBody = createAndAppend('tbody', repTable, {
       class: 'table_elms',
     });
-    const tr1 = createAndAppend('tr', tbody);
-    createAndAppend('td', tr1, {
-      text: 'Repository      :',
-    });
-    createAndAppend('td', tr1, {
+    addRow(tBody, 'Repository', '');
+    addRow(tBody, 'Description', repository.description);
+    addRow(tBody, 'Forks', repository.forks);
+    addRow(tBody, 'Updated', new Date(repository.updated_at).toLocaleString('en-GB'));
+    createAndAppend('a', tBody.firstChild.lastChild, {
+      href: repository.html_url,
+      target: '_blank',
       text: repository.name,
-    });
-    const tr2 = createAndAppend('tr', tbody);
-    createAndAppend('td', tr2, {
-      text: 'Description    :',
-    });
-    createAndAppend('td', tr2, {
-      text: repository.description,
-    });
-    const tr3 = createAndAppend('tr', tbody);
-    createAndAppend('td', tr3, {
-      text: 'Forks          :',
-    });
-    createAndAppend('td', tr3, {
-      text: repository.forks,
-    });
-    const tr4 = createAndAppend('tr', tbody);
-    createAndAppend('td', tr4, {
-      text: 'Updated        :',
-    });
-    createAndAppend('td', tr4, {
-      text: new Date(repository.updated_at).toLocaleString('en-GB'),
     });
   }
 
@@ -110,6 +98,9 @@
       contributions.forEach(contribution => {
         const listItem = createAndAppend('li', contributionsList, {
           class: 'contrib',
+        });
+        listItem.addEventListener('click', () => {
+          window.open(contribution.html_url, '_blank');
         });
         createAndAppend('img', listItem, {
           class: 'contrib_img',
@@ -137,7 +128,7 @@
       text: 'HYF Repositories',
     });
     const select = createAndAppend('select', header);
-    const container = createAndAppend('container', root, {
+    const container = createAndAppend('main', root, {
       id: 'container',
     });
     fetchJSON(url, (error, repositories) => {
