c70d45241f578a211dd7ca8a39e2e649c7e553af
20twa07 <48910851+20twa07@users.noreply.github.com>
2019-07-30 14:08:38 +0200

    update after review

--- homework/index.js
+++ homework/index.js
@@ -15,18 +15,16 @@
     return elem;
   }
 
-  function addRow(tbody, label, value) {
-    const tr = createAndAppend('tr', tbody);
-    createAndAppend('td', tr, { text: `${label}:`, class: 'label' });
-    createAndAppend('td', tr, { text: value });
+  function renderError(error) {
+    const root = document.getElementById('root');
+    createAndAppend('h1', root, { text: error.message, class: 'alert-error' });
   }
 
-  function addHref(value, parent, text) {
-    createAndAppend('a', value, {
-      href: parent.html_url,
-      target: '_blank',
-      text: `${text}`,
-    });
+  function addRow(tbody, labelText, value) {
+    const tr = createAndAppend('tr', tbody);
+    createAndAppend('td', tr, { text: `${labelText}:`, class: 'label' });
+    createAndAppend('td', tr, { text: value });
+    return tr;
   }
 
   function fetchJSON(url, cb) {
@@ -44,78 +42,85 @@
     xhr.send();
   }
 
-  function renderRepositories(repo, ulRepoInfo) {
-    const url = `https://api.github.com/repos/HackYourFuture/${repo}`;
-    fetchJSON(url, (err, repoInfo) => {
-      ulRepoInfo.innerHTML = '';
-      const li = createAndAppend('li', ulRepoInfo, { id: 'li1' });
-      const table = createAndAppend('table', li);
-      const tbody = createAndAppend('tbody', table);
+  function renderRepositories(repos, divRepoInfo) {
+    divRepoInfo.innerHTML = '';
+    const { html_url, name, description, forks, updated_at } = repos;
+    const table = createAndAppend('table', divRepoInfo);
+    const tbody = createAndAppend('tbody', table);
 
-      const tr1 = createAndAppend('tr', tbody);
-      createAndAppend('td', tr1, { text: 'Repository:', class: 'label' });
-      createAndAppend('td', tr1);
-      addHref(tr1.lastChild, repoInfo, repoInfo.name);
-
-      addRow(tbody, 'Description', repoInfo.description);
-      addRow(tbody, 'Forks', repoInfo.forks);
-      addRow(tbody, 'Updated', new Date(repoInfo.updated_at).toLocaleString());
+    const firstRow = addRow(tbody, 'Repository', '');
+    createAndAppend('a', firstRow.lastChild, {
+      href: html_url,
+      target: '_blank',
+      text: name,
     });
+
+    addRow(tbody, 'Description', description);
+    addRow(tbody, 'Forks', forks);
+    addRow(tbody, 'Updated', new Date(updated_at).toLocaleString());
   }
 
-  function renderContributors(repoName, ulContributorInfo) {
-    const url = `https://api.github.com/repos/HackYourFuture/${repoName}/contributors`;
+  function renderContributors(contributorRepo, ulContributorInfo) {
+    const url = contributorRepo.contributors_url;
     fetchJSON(url, (err, contributors) => {
+      if (err) {
+        renderError(err);
+        return;
+      }
       ulContributorInfo.innerHTML = '';
+
       contributors.forEach(contributor => {
-        const listItems = createAndAppend('li', ulContributorInfo, { class: 'li2' });
-        createAndAppend('img', listItems, {
-          src: contributor.avatar_url,
-          alt: `${contributor.login} photo`,
+        const { html_url, avatar_url, login, contributions } = contributor;
+        const contributorLink = createAndAppend('a', ulContributorInfo, {
+          href: html_url,
+          target: '_blank',
         });
-        createAndAppend('h1', listItems);
-        addHref(listItems.lastChild, contributor, contributor.login);
-        createAndAppend('p', listItems, { text: `${contributor.contributions}` });
+        const listItem = createAndAppend('li', contributorLink, {
+          class: 'contributor-list-item',
+        });
+        createAndAppend('img', listItem, {
+          src: avatar_url,
+          alt: `${login} photo`,
+        });
+        createAndAppend('h1', listItem, { text: login });
+        createAndAppend('p', listItem, { text: contributions });
       });
     });
   }
 
   function createOptionElements(repositories, select) {
-    createAndAppend('option', select, {
-      text: 'Select a repository',
-      disabled: 'disabled',
+    repositories.forEach((repo, index) => {
+      createAndAppend('option', select, { text: repo.name, value: index });
     });
-    repositories
-      .sort((a, b) => a.name.localeCompare(b.name))
-      .forEach(repo => {
-        createAndAppend('option', select, { text: repo.name, value: repo.name });
-      });
   }
 
   function main(url) {
     const root = document.getElementById('root');
-    const header = createAndAppend('div', root, { class: 'header' });
-    const infoSection = createAndAppend('div', root, { class: 'info-section-div' });
-    const repoInfoSection = createAndAppend('div', infoSection);
-    const contributorInfoSection = createAndAppend('div', infoSection);
-
-    createAndAppend('h1', header, { text: 'HYF Repositories' });
-    const select = createAndAppend('select', header);
-    const ulRepoInfo = createAndAppend('ul', repoInfoSection, { class: 'li-container' });
-    const ulContributorInfo = createAndAppend('ul', contributorInfoSection, {
-      class: 'li-container',
+    const header = createAndAppend('header', root);
+    const infoSection = createAndAppend('section', root, { class: 'wrapper' });
+    const headerWrapper = createAndAppend('div', header, { class: 'header-wrapper' });
+    createAndAppend('h1', headerWrapper, { text: 'HYF Repositories' });
+    const select = createAndAppend('select', headerWrapper);
+    const repoInfo = createAndAppend('div', infoSection, { class: 'flex-container' });
+    const contributorInfo = createAndAppend('ul', infoSection, {
+      class: 'flex-container',
     });
 
-    fetchJSON(url, (error, data) => {
-      if (error) {
-        createAndAppend('div', root, { text: error.message, class: 'alert-error' });
+    fetchJSON(url, (err, repositories) => {
+      if (err) {
+        renderError(err);
         return;
       }
-      createOptionElements(data, select);
+      repositories.sort((a, b) => a.name.localeCompare(b.name));
+      createOptionElements(repositories, select);
+      const repo = repositories[select.value];
+      renderRepositories(repo, repoInfo);
+      renderContributors(repo, contributorInfo);
+
       select.addEventListener('change', () => {
-        const repoName = select.value;
-        renderRepositories(repoName, ulRepoInfo);
-        renderContributors(repoName, ulContributorInfo);
+        // const repo = repositories[select.value];
+        renderRepositories(repo, repoInfo);
+        renderContributors(repo, contributorInfo);
       });
     });
   }
