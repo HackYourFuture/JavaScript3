43965211457c031d80c207988db829c3dc7cf2c9
Remzi <49848099+Remzi1@users.noreply.github.com>
2019-08-10 15:02:28 +0200

    very late homework

--- homework/index.js
+++ homework/index.js
@@ -30,14 +30,95 @@
     return elem;
   }
 
+  function createOptionElement(repositories, select) {
+    repositories
+      .sort((a, b) => a.name.localeCompare(b.name))
+      .forEach((repositorie, index) => {
+        createAndAppend('option', select, { text: repositorie.name, value: index });
+      });
+  }
+
+  function addingRow(tbody, title, value) {
+    const tr = createAndAppend('tr', tbody);
+    createAndAppend('td', tr, { text: title, class: 'title' });
+    createAndAppend('td', tr, { text: ':' });
+    createAndAppend('td', tr, { text: value });
+    return tr;
+  }
+
+  function renderRepositories(repo, leftSection) {
+    leftSection.innerHTML = '';
+    const table = createAndAppend('table', leftSection);
+    const tbody = createAndAppend('tbody', table);
+
+    const firstRow = addingRow(tbody, 'Repository', '');
+
+    createAndAppend('a', firstRow.lastChild, {
+      href: repo.html_url,
+      target: '_blank',
+      text: repo.name,
+    });
+
+    addingRow(tbody, 'Description', repo.description);
+    addingRow(tbody, 'Forks', repo.forks);
+    addingRow(tbody, 'Updated', new Date(repo.updated_at).toLocaleString());
+  }
+
+  function renderContributors(repo, rightSection) {
+    const url = repo.contributors_url;
+
+    fetchJSON(url, (err, contributors) => {
+      if (err) {
+        const root = document.getElementById('root');
+        createAndAppend('h1', root, { text: err.message, class: 'alert-error' });
+        return;
+      }
+
+      rightSection.innerHTML = '';
+      contributors.forEach(contributor => {
+        const listItem = createAndAppend('li', rightSection, { class: 'contributorLi' });
+
+        const contributorLink = createAndAppend('a', listItem, {
+          class: 'contributorLink',
+          href: contributor.html_url,
+          target: '_blank',
+        });
+
+        createAndAppend('img', contributorLink, {
+          src: contributor.avatar_url,
+          alt: `${contributor.login} photo`,
+        });
+        createAndAppend('h1', contributorLink, { text: contributor.login });
+        createAndAppend('p', contributorLink, { text: contributor.contributions });
+      });
+    });
+  }
+
   function main(url) {
     fetchJSON(url, (err, repositories) => {
       const root = document.getElementById('root');
+      const header = createAndAppend('header', root, { class: 'header' });
+      createAndAppend('h1', header, {
+        class: 'headTitle',
+        text: 'Hack Your Future Repositories',
+      });
+      const select = createAndAppend('select', header, { class: 'select' });
+      const mainPage = createAndAppend('main', root, { class: 'mainPage' });
+      const leftSection = createAndAppend('section', mainPage, { class: 'leftSide' });
+      const rightSection = createAndAppend('section', mainPage, { class: 'rightSide' });
+
       if (err) {
         createAndAppend('div', root, { text: err.message, class: 'alert-error' });
-        return;
       }
-      createAndAppend('pre', root, { text: JSON.stringify(repositories, null, 2) });
+      createOptionElement(repositories, select);
+
+      renderRepositories(repositories[0], leftSection);
+      renderContributors(repositories[0], rightSection);
+
+      select.addEventListener('change', () => {
+        renderRepositories(repositories[select.value], leftSection);
+        renderContributors(repositories[select.value], rightSection);
+      });
     });
   }
 
