798da832c9317c8bf9916bf8f46ceb4727b7801d
mehmetekinci <mehmetekinci1989@gmail.com>
2019-09-19 22:26:19 +0200

    add Js3 Week1 Homework

--- homework/index.js
+++ homework/index.js
@@ -29,13 +29,54 @@
     return elem;
   }
 
+  function makeText(stringDateAndTime) {
+    const dt = new Date(stringDateAndTime);
+    return dt.toLocaleString();
+  }
+
   function renderRepoDetails(repo, ul) {
-    createAndAppend('li', ul, { text: repo.name });
+    const li = createAndAppend('li', ul, { class: 'repos' });
+    const table = createAndAppend('table', li, { class: 'table' });
+    const tr1 = createAndAppend('tr', table);
+    const tr2 = createAndAppend('tr', table);
+    const tr3 = createAndAppend('tr', table);
+    const tr4 = createAndAppend('tr', table);
+
+    createAndAppend('td', tr1, { text: 'Repository :', class: 'tableTitle' });
+    const link = createAndAppend('td', tr1, { class: 'tableText' });
+    createAndAppend('a', link, { text: repo.name, href: repo.html_url });
+    createAndAppend('td', tr2, { text: 'Description :', class: 'tableTitle' });
+    if (repo.description === null) {
+      createAndAppend('td', tr2, {
+        text: 'No Information',
+        class: 'tableText',
+      });
+    } else {
+      createAndAppend('td', tr2, {
+        text: repo.description,
+        class: 'tableText',
+      });
+    }
+    createAndAppend('td', tr3, { text: 'Forks :', class: 'tableTitle' });
+    createAndAppend('td', tr3, { text: repo.forks, class: 'tableText' });
+    createAndAppend('td', tr4, { text: 'Updated :', class: 'tableTitle' });
+    createAndAppend('td', tr4, {
+      text: makeText(repo.updated_at),
+      class: 'tableText',
+    });
+  }
+
+  function sortReposByNameProp(firstRepo, secondRepo) {
+    return firstRepo.name.localeCompare(secondRepo.name);
   }
 
   function main(url) {
     fetchJSON(url, (err, repos) => {
       const root = document.getElementById('root');
+      createAndAppend('div', root, {
+        class: 'header',
+        text: 'HYF Repositories',
+      });
       if (err) {
         createAndAppend('div', root, {
           text: err.message,
@@ -44,7 +85,9 @@
         return;
       }
       const ul = createAndAppend('ul', root);
-      repos.forEach(repo => renderRepoDetails(repo, ul));
+      repos.sort(sortReposByNameProp).forEach(repo => {
+        renderRepoDetails(repo, ul);
+      });
     });
   }
 
