ea6029a2e29009ae4c20a4364cb0c565c113b011
ee <ersarue@gmail.com>
2019-09-22 21:32:57 +0200

    changing repo count to 100

--- homework/index.js
+++ homework/index.js
@@ -110,6 +110,6 @@
     mainTitle.innerText = 'HYF Repositories';
   }
   const HYF_REPOS_URL =
-    'https://api.github.com/orgs/HackYourFuture/repos?per_page=10';
+    'https://api.github.com/orgs/HackYourFuture/repos?per_page=100';
   window.onload = () => main(HYF_REPOS_URL);
 }
