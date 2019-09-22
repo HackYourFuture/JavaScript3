1bd618dfa0f0c2cb9fe4efdf7fd581d0a93badb6
ee <ersarue@gmail.com>
2019-09-22 21:21:56 +0200

    Js3 W1 Homework(resubmission)

--- homework/index.js
+++ homework/index.js
@@ -29,10 +29,68 @@
     return elem;
   }
 
-  function renderRepoDetails(repo, ul) {
-    createAndAppend('li', ul, { text: repo.name });
+  function changeDateFormat(date) {
+    const timeFormat = new Date(date);
+    return timeFormat.toLocaleString();
   }
 
+  function sortRepos(a, b) {
+    const first = a.name.toLowerCase();
+    const second = b.name.toLowerCase();
+    const key = first < second ? -1 : first > second ? 1 : 0;
+    return key;
+  }
+
+  function renderRepoDetails(repo, ul) {
+    const grouping = createAndAppend('li', ul, { class: 'listItem' });
+    const name = createAndAppend('li', grouping, {
+      text: 'Repository name: ',
+      class: 'title',
+    });
+    const repoLang = createAndAppend('li', grouping, {
+      text: 'Repository language: ',
+      class: 'title',
+    });
+    const description = createAndAppend('li', grouping, {
+      text: 'Description: ',
+      class: 'title',
+    });
+    const forkCount = createAndAppend('li', grouping, {
+      text: 'Fork count: ',
+      class: 'title',
+    });
+    const createTime = createAndAppend('li', grouping, {
+      text: 'Created on: ',
+      class: 'title',
+    });
+    const updateTime = createAndAppend('li', grouping, {
+      text: 'Updated on: ',
+      class: 'title',
+    });
+
+    createAndAppend('a', name, {
+      text: repo.name,
+      href: repo.html_url,
+      class: 'content',
+    });
+    createAndAppend('span', repoLang, {
+      text: repo.language,
+      class: 'content',
+    });
+    createAndAppend('span', description, {
+      text: repo.description,
+      class: 'content',
+    });
+    createAndAppend('span', forkCount, { text: repo.forks, class: 'content' });
+    createAndAppend('span', createTime, {
+      text: changeDateFormat(repo.created_at),
+      class: 'content',
+    });
+    createAndAppend('span', updateTime, {
+      text: changeDateFormat(repo.updated_at),
+      class: 'content',
+    });
+  }
   function main(url) {
     fetchJSON(url, (err, repos) => {
       const root = document.getElementById('root');
@@ -44,11 +102,14 @@
         return;
       }
       const ul = createAndAppend('ul', root);
-      repos.forEach(repo => renderRepoDetails(repo, ul));
+      repos.sort(sortRepos).forEach(repo => renderRepoDetails(repo, ul));
     });
+    const mainTitle = document.createElement('header');
+    document.getElementById('root').appendChild(mainTitle);
+    mainTitle.id = 'header';
+    mainTitle.innerText = 'HYF Repositories';
   }
-
   const HYF_REPOS_URL =
-    'https://api.github.com/orgs/HackYourFuture/repos?per_page=100';
+    'https://api.github.com/orgs/HackYourFuture/repos?per_page=10';
   window.onload = () => main(HYF_REPOS_URL);
 }
