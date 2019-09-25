45d258203c53084ca016c2bb394980df75f7693f
Ann-Grabetski <ann.grabetski@gmail.com>
2019-09-25 23:25:45 +0200

    JS3-Week1-UpDate git status git commit -m JS3-Week1-UpDate
    q
    
    clear
    git commit -m JS3-Week1-UpDate

--- homework/index.js
+++ homework/index.js
@@ -29,61 +29,74 @@
     return elem;
   }
 
-  function renderRepoDetails(repo) {
-    const reposList = document.getElementById('reposList');
-    const repoItem = createAndAppend('li', reposList);
-    const commonBlock = createAndAppend('div', repoItem, {
-      class: 'commonBlock',
+  function createTag(name, options = {}) {
+    const elem = document.createElement(name);
+    Object.entries(options).forEach(([key, value]) => {
+      if (key === 'text') {
+        elem.textContent = value;
+      } else {
+        elem.setAttribute(key, value);
+      }
     });
-    const dataTypeList = createAndAppend('ul', commonBlock, {
-      class: 'dataTypeList',
-    });
-    createAndAppend('li', dataTypeList, { text: 'Repository:' });
-    createAndAppend('li', dataTypeList, { text: 'Description:' });
-    createAndAppend('li', dataTypeList, { text: 'Forks:' });
-    createAndAppend('li', dataTypeList, { text: 'Updated:' });
-    const dataList = createAndAppend('ul', commonBlock, { class: 'dataList' });
-    const repoLink = createAndAppend('li', dataList);
-    createAndAppend('a', repoLink, {
-      href: repo.html_url,
-      target: '_blank',
-      class: 'app-link',
-      text: repo.name,
-    });
-    if (repo.description === null) {
-      createAndAppend('li', dataList, {
-        text: 'No Description',
-        class: 'alert-no-description',
-      });
-    } else {
-      createAndAppend('li', dataList, { text: repo.description });
-    }
-    createAndAppend('li', dataList, { text: repo.forks });
-    createAndAppend('li', dataList, { text: repo.updated_at });
+    return elem;
   }
 
-  function sortRepos(repos) {
-    repos.sort((a, b) => a.name.localeCompare(b.name));
-    return repos;
+  function buildTableRow(title, parent, content) {
+    const row = createAndAppend(`tr`, parent);
+    createAndAppend(`th`, row, { text: `${title}:` });
+    let data;
+    if (typeof content === `string` || typeof content === `number`) {
+      createAndAppend(`td`, row, { text: `${content}` });
+    } else {
+      data = createAndAppend(`td`, row);
+      data.appendChild(content);
+    }
+  }
+
+  function renderRepoDetails(repo, parent) {
+    const detailsDiv = createAndAppend(`div`, parent, { class: `details` });
+    const table = createAndAppend(`table`, detailsDiv);
+
+    const repoLink = createTag(`a`, {
+      href: repo.html_url,
+      target: '_blank',
+      text: repo.name,
+    });
+    buildTableRow(`Repository`, table, repoLink);
+
+    const description = repo.description
+      ? { text: repo.description }
+      : { text: 'No Description', class: 'alert-no-description' };
+
+    const repoDescription = createTag(`span`, description);
+    buildTableRow(`Description`, table, repoDescription);
+
+    buildTableRow(`Forks`, table, repo.forks);
+
+    const date = new Date(repo.updated_at);
+    buildTableRow(`Updated`, table, date.toLocaleDateString());
   }
 
   function main(url) {
+    const root = document.getElementById('root');
+    const header = createAndAppend(`header`, root);
+    createAndAppend(`span`, header, { text: `HYF Repositories` });
+
+    const mainTag = createAndAppend(`main`, root, {
+      class: `main-container`,
+    });
+    const section1 = createAndAppend(`section`, mainTag);
+
     fetchJSON(url, (err, repos) => {
-      const root = document.getElementById('root');
       if (err) {
         createAndAppend('div', root, {
           text: err.message,
           class: 'alert-error',
         });
-        return;
       }
-      createAndAppend('div', root, {
-        id: 'title',
-        text: 'HYF Repositories',
-      });
-      createAndAppend('ul', root, { id: 'reposList' });
-      sortRepos(repos);
-      repos.forEach(repo => renderRepoDetails(repo));
+      repos
+        .sort((a, b) => a.name.localeCompare(b.name))
+        .forEach(repo => renderRepoDetails(repo, section1));
     });
   }
 
