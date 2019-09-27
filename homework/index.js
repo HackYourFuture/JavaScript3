a96216932f3d9eb8cdf2df7f18e0e7ab0e6bb8c8
husenjanobul <husenjanobul@gmail.com>
2019-09-27 04:39:31 +0200

    JS3-Week2-HomeWork

--- homework/index.js
+++ homework/index.js
@@ -1,19 +1,21 @@
 'use strict';
 
 {
-  function fetchJSON(url, cb) {
-    const xhr = new XMLHttpRequest();
-    xhr.open('GET', url);
-    xhr.responseType = 'json';
-    xhr.onload = () => {
-      if (xhr.status >= 200 && xhr.status <= 299) {
-        cb(null, xhr.response);
-      } else {
-        cb(new Error(`Network error: ${xhr.status} - ${xhr.statusText}`));
-      }
-    };
-    xhr.onerror = () => cb(new Error('Network request failed'));
-    xhr.send();
+  function fetchJSON(url) {
+    return new Promise((resolve, reject) => {
+      const xhr = new XMLHttpRequest();
+      xhr.open('GET', url);
+      xhr.responseType = 'json';
+      xhr.onload = () => {
+        if (xhr.status >= 200 && xhr.status <= 299) {
+          resolve(xhr.response);
+        } else {
+          reject(new Error(`Network error: ${xhr.status} - ${xhr.statusText}`));
+        }
+      };
+      xhr.onerror = () => reject(new Error('Network request failed'));
+      xhr.send();
+    });
   }
 
   function createAndAppend(name, parent, options = {}) {
@@ -29,48 +31,138 @@
     return elem;
   }
 
-  function createLiElement(ul, element1, element2, options1, options2) {
-    const li = createAndAppend('li', ul, {class: 'list'});
-    return createAndAppend(element1, li, options1),
-      createAndAppend(element2, li, options2);
+  function createElement(ul, element1, element2, options1, options2) {
+    const li = createAndAppend('li', ul, { class: 'list' });
+    createAndAppend(element1, li, options1);
+    createAndAppend(element2, li, options2);
   }
 
-  function dateChange(date){
+  function dateChange(date) {
     return new Date(date).toLocaleString();
   }
 
   function renderRepoDetails(repo, ul) {
-    createLiElement(ul, 'h3', 'a', { text: ' Repository:  ' }, {
-      text: repo.name,
-      href: repo.html_url,
-      target: '_blank',
-    });
-    createLiElement(ul, 'h3', 'span', { text: ' Description:  ' }, { text: repo.description });
-    createLiElement(ul, 'h3', 'span', { text: ' Forks:  ' }, { text: repo.forks });
-    createLiElement(ul, 'h3', 'span', { text: ' Updated:  ' }, { text: dateChange(repo.updated_at) });
+    ul.innerHTML = ' ';
+    createElement(
+      ul,
+      'h3',
+      'a',
+      { text: ' Repository:  ' },
+      {
+        text: repo.name,
+        href: repo.html_url,
+        target: '_blank',
+      },
+    );
+    createElement(
+      ul,
+      'h3',
+      'span',
+      { text: ' Description:  ' },
+      { text: repo.description },
+    );
+    createElement(
+      ul,
+      'h3',
+      'span',
+      { text: ' Forks:  ' },
+      { text: repo.forks },
+    );
+    createElement(
+      ul,
+      'h3',
+      'span',
+      { text: ' Updated:  ' },
+      { text: dateChange(repo.updated_at) },
+    );
   }
 
-  function main(url) {
-    fetchJSON(url, (err, repos) => {
-      const root = document.getElementById('root');
-      createAndAppend('div', root, {
-        text: 'HYF Repositories',
-        class: 'title',
-      });
-      if (err) {
+  function renderContributes(repositories, ul) {
+    const url = repositories.contributors_url;
+    fetchJSON(url)
+      .then(contributions => {
+        ul.innerHTML = ' ';
+        contributions.forEach(contributor => {
+          const li = createAndAppend('li', ul);
+          const infoImg = createAndAppend('div', li, { class: 'infoImg' });
+          createAndAppend('img', infoImg, {
+            src: contributor.avatar_url,
+            alt: contributor.login,
+          });
+          const infoName = createAndAppend('div', li, { class: 'infoName' });
+          createAndAppend('a', infoName, {
+            text: contributor.login,
+            href: contributor.html_url,
+            target: '_blank',
+          });
+          createAndAppend('span', infoName, {
+            text: contributor.contributions,
+            class: 'number',
+          });
+        });
+      })
+      .catch(err => {
+        const root = document.getElementById('root');
         createAndAppend('div', root, {
           text: err.message,
           class: 'alert-error',
         });
-        return;
-      }
-      repos
-        .sort((a, b) => a.name.localeCompare(b.name))
-        .forEach(repo => {
-          const ul = createAndAppend('ul', root, {class: 'listContainer'});
-          renderRepoDetails(repo, ul);
       });
-    });
+  }
+
+  function main(url) {
+    fetchJSON(url)
+      .then(repos => {
+        const root = document.getElementById('root');
+        const header = createAndAppend('header', root, {
+          text: 'HYF Repositories',
+          class: 'title',
+        });
+        const select = createAndAppend('select', header, {
+          class: 'selection',
+        });
+        repos
+          .sort((a, b) => a.name.localeCompare(b.name))
+          .forEach((repo, index) =>
+            createAndAppend('option', select, {
+              text: repo.name,
+              value: index,
+            }),
+          );
+        const mainContainer = createAndAppend('main', root, {
+          class: 'main-container',
+        });
+        const repoContainer = createAndAppend('div', mainContainer, {
+          class: 'repo-container',
+        });
+        const ul = createAndAppend('ul', repoContainer, {
+          class: 'listContainer',
+        });
+        renderRepoDetails(repos[0], ul);
+        const contribContainer = createAndAppend('div', mainContainer, {
+          class: 'contributors-container',
+        });
+        createAndAppend('h3', contribContainer, {
+          text: 'Contributions',
+        });
+        const ulContributes = createAndAppend('ul', contribContainer, {
+          class: 'itemList',
+        });
+        renderContributes(repos[0], ulContributes);
+
+        select.addEventListener('change', () => {
+          const repo = repos[select.value];
+          renderRepoDetails(repo, ul);
+          renderContributes(repo, ulContributes);
+        });
+      })
+      .catch(err => {
+        const root = document.getElementById('root');
+        createAndAppend('div', root, {
+          text: err.message,
+          class: 'alert-error',
+        });
+      });
   }
 
   const HYF_REPOS_URL =
