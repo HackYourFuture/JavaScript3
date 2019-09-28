b7aa632146ec1fb128094e507f5dcce49bb29113
NesibeInce <nesybe.ince@gmail.com>
2019-09-28 18:09:52 +0200

    is working but no promise and css is not complete

--- homework/index.js
+++ homework/index.js
@@ -29,8 +29,55 @@
     return elem;
   }
 
-  function renderRepoDetails(repo, ul) {
-    createAndAppend('li', ul, { text: repo.name });
+  function renderRepoTable(table, name, path) {
+    const tr = createAndAppend('tr', table);
+    createAndAppend('th', tr, {
+      text: name,
+    });
+    createAndAppend('td', tr, {
+      text: path,
+    });
+    return tr;
+  }
+
+  function renderRepoDetails(repo, table) {
+    const firstRow = renderRepoTable(table, 'Repository', '');
+    createAndAppend('a', firstRow.lastChild, {
+      text: repo.name,
+      href: repo.html_url,
+      target: '_blank',
+    });
+    renderRepoTable(table, 'Description:', repo.description);
+    renderRepoTable(table, 'Forks:', repo.forks);
+    const date = new Date(repo.updated_at).toLocaleString('en-US');
+    renderRepoTable(table, 'Updated:', date);
+  }
+
+  function renderContributors(url, div) {
+    fetchJSON(url, (err, repos) => {
+      if (err) {
+        createAndAppend('div', div, {
+          text: err.message,
+          class: 'alert-error',
+        });
+        return;
+      }
+
+      repos.forEach(repo => {
+        const divContributor = createAndAppend('div', div, {
+          class: 'contributor-details',
+        });
+        createAndAppend('img', divContributor, {
+          src: repo.avatar_url,
+        });
+        createAndAppend('h4', divContributor, {
+          text: repo.login,
+        });
+        createAndAppend('p', divContributor, {
+          text: repo.contributions,
+        });
+      });
+    });
   }
 
   function main(url) {
@@ -43,8 +90,44 @@
         });
         return;
       }
-      const ul = createAndAppend('ul', root);
-      repos.forEach(repo => renderRepoDetails(repo, ul));
+      const header = createAndAppend('header', root, {
+        class: 'header-class',
+      });
+      createAndAppend('h1', header, {
+        text: 'HYF Repository',
+        class: 'title',
+      });
+      const select = createAndAppend('select', header, {
+        class: 'select-class',
+      });
+      const mainContainer = createAndAppend('section', root, {
+        class: 'main-container',
+      });
+      const reposContainer = createAndAppend('section', mainContainer, {
+        class: 'repos-container',
+      });
+      const contributorContainer = createAndAppend('section', mainContainer, {
+        class: 'contributor-container',
+      });
+      repos
+        .sort((a, b) => {
+          return a.name.localeCompare(b.name);
+        })
+        .map((elem, index) => {
+          createAndAppend('option', select, {
+            value: index,
+            text: repos[index].name,
+          });
+        });
+
+      select.addEventListener('change', () => {
+        reposContainer.innerHTML = '';
+        contributorContainer.innerHTML = '';
+        const repo = repos[select.value];
+        renderRepoDetails(repo, reposContainer);
+        contributorContainer.innerHTML = 'Contributors';
+        renderContributors(repo.contributors_url, contributorContainer);
+      });
     });
   }
 
