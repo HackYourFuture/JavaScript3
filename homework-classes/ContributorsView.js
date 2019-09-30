abef6fe4b45437f53d427a20d95ddc37c3ce8e2f
msnmz <snmez.muharrem@gmail.com>
2019-09-30 14:30:57 +0200

    ContributorsView: Renders contributors

--- homework-classes/ContributorsView.js
+++ homework-classes/ContributorsView.js
@@ -19,8 +19,30 @@
      * @param {Object[]} contributors An array of contributor objects
      */
     render(contributors) {
-      // TODO: replace this comment and the console.log with your own code
-      console.log('ContributorsView', contributors);
+      this.container.innerHTML = '';
+      createAndAppend('h6', this.container, {
+        text: 'Contributions:',
+      });
+      const ul = createAndAppend('ul', this.container, {
+        class: 'sub-container',
+      });
+      contributors.forEach(contributor => {
+        const repoContributorListItem = createAndAppend('li', ul, {
+          class: 'contributors-item',
+        });
+        createAndAppend('img', repoContributorListItem, {
+          src: contributor.avatar_url,
+          alt: 'Contributor avatar picture',
+        });
+        createAndAppend('a', repoContributorListItem, {
+          href: contributor.html_url,
+          text: contributor.login,
+          target: '_blank',
+        });
+        createAndAppend('span', repoContributorListItem, {
+          text: contributor.contributions,
+        });
+      });
     }
   }
 
