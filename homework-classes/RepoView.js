dd9a327c0f918e14f9017e3223ae1c2c2d1412d4
msnmz <snmez.muharrem@gmail.com>
2019-09-30 14:25:35 +0200

    RepoView: Renders repo

--- homework-classes/RepoView.js
+++ homework-classes/RepoView.js
@@ -19,8 +19,38 @@
      * @param {Object} repo A repository object.
      */
     render(repo) {
-      // TODO: replace this comment and the console.log with your own code
-      console.log('RepoView', repo);
+      const repoDetailsTable = createAndAppend('table', this.container, {
+        class: 'repository-item-table',
+      });
+      const firstRow = this.appendRepoDetail(
+        'Repository:',
+        '',
+        repoDetailsTable,
+      );
+      createAndAppend('a', firstRow.lastChild, {
+        href: repo.html_url,
+        text: repo.name,
+        target: '_blank',
+      });
+      this.appendRepoDetail('Description:', repo.description, repoDetailsTable);
+      this.appendRepoDetail('Forks:', repo.forks, repoDetailsTable);
+      this.appendRepoDetail(
+        'Updated:',
+        this.getDateTimeText(repo.updated_at),
+        repoDetailsTable,
+      );
+    }
+
+    appendRepoDetail(header, description, parentTable) {
+      const repoDetailRow = createAndAppend('tr', parentTable);
+      createAndAppend('th', repoDetailRow, { text: header });
+      createAndAppend('td', repoDetailRow, { text: description });
+      return repoDetailRow;
+    }
+
+    getDateTimeText(dateTimeStr) {
+      const dateTime = new Date(dateTimeStr);
+      return dateTime.toLocaleString();
     }
   }
 
