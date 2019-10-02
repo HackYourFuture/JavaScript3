1a122b4cfda1b2f62611047ed5697268c2716389
Rabee <rabeerd@gmail.com>
2019-10-02 10:35:29 +0200

    JS3 week3 Part2 + Week1 style update

--- homework-classes/ContributorsView.js
+++ homework-classes/ContributorsView.js
@@ -19,8 +19,39 @@
      * @param {Object[]} contributors An array of contributor objects
      */
     render(contributors) {
-      // TODO: replace this comment and the console.log with your own code
-      console.log('ContributorsView', contributors);
+      this.container.innerHTML = '';
+      createAndAppend('p', this.container, {
+        text: `Contributors:`,
+        id: 'contributors-title',
+        role: 'heading',
+      });
+      const div = createAndAppend('div', this.container, {
+        id: 'list-contributions',
+        role: 'list',
+      });
+
+      contributors.forEach(contributor => {
+        const contributorDiv = createAndAppend('div', div, {
+          class: 'contributor-details',
+          role: 'listitem',
+        });
+        createAndAppend('img', contributorDiv, {
+          src: contributor.avatar_url,
+          alt: 'Contributor avatar image',
+          role: 'img',
+        });
+        createAndAppend('a', contributorDiv, {
+          text: contributor.login,
+          href: contributor.html_url,
+          target: '_blank',
+          class: 'user-name',
+          role: 'link',
+        });
+        createAndAppend('div', contributorDiv, {
+          text: contributor.contributions,
+          class: 'contributions-count',
+        });
+      });
     }
   }
 
--- homework-classes/RepoView.js
+++ homework-classes/RepoView.js
@@ -19,8 +19,30 @@
      * @param {Object} repo A repository object.
      */
     render(repo) {
-      // TODO: replace this comment and the console.log with your own code
-      console.log('RepoView', repo);
+      this.container.innerHTML = '';
+      const table = createAndAppend('table', this.container, {
+        class: 'repos-table',
+      });
+
+      function addRow(table, labelText, value) {
+        const tr = createAndAppend('tr', table);
+        createAndAppend('th', tr, { text: `${labelText}:` });
+        createAndAppend('td', tr, { text: value });
+        return tr;
+      }
+      const firstRow = addRow(table, 'Name', '');
+      createAndAppend('a', firstRow.lastChild, {
+        text: repo.name,
+        href: repo.html_url,
+        target: '_blank',
+      });
+      addRow(table, 'Description', repo.description);
+      addRow(table, 'Forks', repo.forks);
+      addRow(
+        table,
+        'Updated at',
+        new Date(repo.updated_at).toLocaleString('en-GB', { timeZone: 'UTC' }),
+      );
     }
   }
 
