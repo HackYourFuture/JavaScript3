b231cab2fca44981e4a12e5a973b25b6e027c494
mehmetekinci <mehmetekinci1989@gmail.com>
2019-10-04 13:31:30 +0200

    JS3 week3 final homework

--- homework-classes/ContributorsView.js
+++ homework-classes/ContributorsView.js
@@ -19,8 +19,27 @@
      * @param {Object[]} contributors An array of contributor objects
      */
     render(contributors) {
-      // TODO: replace this comment and the console.log with your own code
-      console.log('ContributorsView', contributors);
+      this.container.innerHTML = '';
+      contributors.forEach(contributor => {
+        const li = createAndAppend('li', this.container, {
+          class: 'contribution',
+        });
+        createAndAppend('img', li, {
+          src: contributor.avatar_url,
+          alt: `${contributor.login}'s image`,
+          class: 'img',
+        });
+        createAndAppend('a', li, {
+          href: contributor.html_url,
+          text: contributor.login,
+          target: '_blank',
+          class: 'link',
+        });
+        createAndAppend('span', li, {
+          text: contributor.contributions,
+          class: 'span-elm',
+        });
+      });
     }
   }
 
--- homework-classes/RepoView.js
+++ homework-classes/RepoView.js
@@ -19,8 +19,42 @@
      * @param {Object} repo A repository object.
      */
     render(repo) {
-      // TODO: replace this comment and the console.log with your own code
-      console.log('RepoView', repo);
+      this.container.innerHTML = '';
+      const table = createAndAppend('table', this.container, {
+        class: 'table',
+      });
+      const firstRow = this.addRepoDetailsInfo(table, 'Repository :', '');
+      createAndAppend('a', firstRow.lastChild, {
+        text: repo.name,
+        href: repo.html_url,
+      });
+      this.addRepoDetailsInfo(table, 'Description :', repo.description);
+      this.addRepoDetailsInfo(table, 'Forks :', repo.forks);
+      this.addRepoDetailsInfo(
+        table,
+        'Updated :',
+        this.formatDate(repo.updated_at),
+      );
+    }
+
+    addRepoDetailsInfo(listElm, title, value) {
+      const trElm = createAndAppend('tr', listElm, { class: 'tr-elm' });
+      createAndAppend('td', trElm, {
+        class: 'titles',
+        text: title,
+      });
+
+      createAndAppend('td', trElm, {
+        class: 'values',
+        text: value !== null ? value : 'No Information',
+      });
+
+      return trElm;
+    }
+
+    formatDate(date) {
+      const dateTime = new Date(date);
+      return dateTime.toDateString();
     }
   }
 
