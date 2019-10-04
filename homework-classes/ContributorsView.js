489b44badf1e140ecde3713340d8397a9e9695b0
faruktercan <trcnfaruk@gmail.com>
2019-10-04 23:38:01 +0200

    JS3 Week 3 Part2

--- homework-classes/ContributorsView.js
+++ homework-classes/ContributorsView.js
@@ -19,8 +19,29 @@
      * @param {Object[]} contributors An array of contributor objects
      */
     render(contributors) {
-      // TODO: replace this comment and the console.log with your own code
-      console.log('ContributorsView', contributors);
+      this.container.innerHTML = '';
+
+      contributors.forEach(contributor => {
+        const contributorLi = createAndAppend('li', this.container, {
+          class: 'contributor-list',
+        });
+        createAndAppend('img', contributorLi, {
+          src: contributor.avatar_url,
+          alt: contributor.login,
+          class: 'contributor-image ',
+        });
+        createAndAppend('a', contributorLi, {
+          href: contributor.html_url,
+          text: contributor.login,
+          target: '_blank',
+          class: 'contributor-link ',
+        });
+        createAndAppend('div', contributorLi, {
+          href: contributor.html_url,
+          text: contributor.contributions,
+          class: 'contributor-div ',
+        });
+      });
     }
   }
 
--- homework-classes/RepoView.js
+++ homework-classes/RepoView.js
@@ -19,8 +19,34 @@
      * @param {Object} repo A repository object.
      */
     render(repo) {
-      // TODO: replace this comment and the console.log with your own code
-      console.log('RepoView', repo);
+      this.container.innerHTML = '';
+      const table = createAndAppend('table', this.container, {
+        class: 'table',
+      });
+      const tr1 = this.addTableRow(table, 'Repository:', '');
+      createAndAppend('a', tr1.lastChild, {
+        href: repo.html_url,
+        text: repo.name,
+      });
+      this.addTableRow(table, 'Description:', repo.description);
+      this.addTableRow(table, 'Fork: ', repo.forks);
+      this.addTableRow(
+        table,
+        'Updated:',
+        this.changeDateTimeFormat(repo.updated_at),
+      );
+    }
+
+    changeDateTimeFormat(dateTime) {
+      const timeFormat = new Date(dateTime);
+      return timeFormat.toLocaleString();
+    }
+
+    addTableRow(table, header, value) {
+      const tr = createAndAppend('tr', table, { class: 'tr' });
+      createAndAppend('th', tr, { text: header, class: 'keys' });
+      createAndAppend('td', tr, { text: value, class: 'values' });
+      return tr;
     }
   }
 
