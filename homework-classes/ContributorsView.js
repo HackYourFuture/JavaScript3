380c1e878902baaa98e847de93300f7529cdbc98
ihk2018 <karatasibrahimhalil@gmail.com>
2019-10-04 17:58:37 +0200

    JS3W3 Second Part of the Homework

--- homework-classes/ContributorsView.js
+++ homework-classes/ContributorsView.js
@@ -19,8 +19,38 @@
      * @param {Object[]} contributors An array of contributor objects
      */
     render(contributors) {
-      // TODO: replace this comment and the console.log with your own code
-      console.log('ContributorsView', contributors);
+      this.container.innerHTML = '';
+      createAndAppend('h2', this.container, {
+        text: 'Contributors',
+      });
+
+      const ulContributors = createAndAppend('ul', this.container);
+      contributors.forEach(contributor => {
+        const li = createAndAppend('li', ulContributors, {
+          class: 'contributors-li',
+        });
+        const contributorImage = createAndAppend('div', li, {
+          class: 'contributor-image',
+        });
+        createAndAppend('img', contributorImage, {
+          src: contributor.avatar_url,
+          alt: contributor.login,
+        });
+
+        const contributorName = createAndAppend('div', li, {
+          class: 'contributor-name',
+        });
+        createAndAppend('a', contributorName, {
+          text: contributor.login,
+          href: contributor.html_url,
+          target: '_blank',
+        });
+
+        createAndAppend('span', contributorName, {
+          text: contributor.contributions,
+          class: 'added-contributions',
+        });
+      });
     }
   }
 
--- homework-classes/RepoView.js
+++ homework-classes/RepoView.js
@@ -19,8 +19,54 @@
      * @param {Object} repo A repository object.
      */
     render(repo) {
-      // TODO: replace this comment and the console.log with your own code
-      console.log('RepoView', repo);
+      function convertDateTime(dateTimeTxt) {
+        const dateTime = new Date(dateTimeTxt);
+        return dateTime.toLocaleString();
+      }
+      this.container.innerHTML = '';
+      const repoList = createAndAppend('div', this.container, {
+        class: 'repo-list',
+      });
+      const repositoryName = createAndAppend('p', repoList, {
+        text: ' Repository: ',
+        class: 'repo-details',
+      });
+
+      const description = createAndAppend('p', repoList, {
+        text: ' Description: ',
+        class: 'repo-details',
+      });
+
+      const forkedNumbers = createAndAppend('p', repoList, {
+        text: ' Forks: ',
+        class: 'repo-details',
+      });
+
+      const updatedTime = createAndAppend('p', repoList, {
+        text: ' Updated: ',
+        class: 'repo-details',
+      });
+
+      createAndAppend('a', repositoryName, {
+        text: repo.name,
+        href: repo.html_url,
+        target: '_blank',
+      });
+
+      createAndAppend('span', description, {
+        text: repo.description || 'No Description Added Yet',
+        class: 'texts',
+      });
+
+      createAndAppend('span', forkedNumbers, {
+        text: repo.forks,
+        class: 'texts',
+      });
+
+      createAndAppend('span', updatedTime, {
+        text: convertDateTime(repo.updated_at),
+        class: 'texts',
+      });
     }
   }
 
