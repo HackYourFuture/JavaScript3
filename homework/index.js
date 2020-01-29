4cfd0680e7c2362584245a4f0b6ca5fbc4b42273
Mahmoud Badran <mahmoudbd@gmail.com>
2020-01-29 02:03:28 +0100

    project is done

--- homework/index.js
+++ homework/index.js
@@ -1,54 +1,84 @@
 'use strict';
 
 {
-  function fetchJSON(url, cb) {
-    const xhr = new XMLHttpRequest();
-    xhr.open('GET', url);
-    xhr.responseType = 'json';
-    xhr.onload = () => {
-      if (xhr.status >= 200 && xhr.status <= 299) {
-        cb(null, xhr.response);
-      } else {
-        cb(new Error(`Network error: ${xhr.status} - ${xhr.statusText}`));
-      }
-    };
-    xhr.onerror = () => cb(new Error('Network request failed'));
-    xhr.send();
-  }
+	function fetchJSON(url, cb) {
+		const xhr = new XMLHttpRequest();
+		xhr.open('GET', url);
+		xhr.responseType = 'json';
+		xhr.onload = () => {
+			if (xhr.status >= 200 && xhr.status <= 299) {
+				cb(null, xhr.response);
+			} else {
+				cb(new Error(`Network error: ${xhr.status} - ${xhr.statusText}`));
+			}
+		};
+		xhr.onerror = () => cb(new Error('Network request failed'));
+		xhr.send();
+	}
 
-  function createAndAppend(name, parent, options = {}) {
-    const elem = document.createElement(name);
-    parent.appendChild(elem);
-    Object.entries(options).forEach(([key, value]) => {
-      if (key === 'text') {
-        elem.textContent = value;
-      } else {
-        elem.setAttribute(key, value);
-      }
-    });
-    return elem;
-  }
+	function createAndAppend(name, parent, options = {}) {
+		const elem = document.createElement(name);
+		parent.appendChild(elem);
+		Object.entries(options).forEach(([ key, value ]) => {
+			if (key === 'text') {
+				elem.textContent = value;
+			} else {
+				elem.setAttribute(key, value);
+			}
+		});
+		return elem;
+	}
 
-  function renderRepoDetails(repo, ul) {
-    createAndAppend('li', ul, { text: repo.name });
-  }
+	function renderRepoDetails(repo, ul) {
+		const li = createAndAppend('li', ul);
+		const table = createAndAppend('table', li);
 
-  function main(url) {
-    fetchJSON(url, (err, repos) => {
-      const root = document.getElementById('root');
-      if (err) {
-        createAndAppend('div', root, {
-          text: err.message,
-          class: 'alert-error',
-        });
-        return;
-      }
-      const ul = createAndAppend('ul', root);
-      repos.forEach(repo => renderRepoDetails(repo, ul));
-    });
-  }
+		//const titles = [ 'Repository:', 'Description:', 'Forks:', 'Updated:' ];
+		//const keys = [ 'name', 'description', 'forks', 'update_at' ];
 
-  const HYF_REPOS_URL =
-    'https://api.github.com/orgs/HackYourFuture/repos?per_page=100';
-  window.onload = () => main(HYF_REPOS_URL);
+		//for (let i of title ) { try to use loop of
+		let tr = createAndAppend('tr', table); // i can not use const variable  here
+		//Repository
+		createAndAppend('th', tr, { text: 'Repository:' });
+		let td = createAndAppend('td', tr);
+		createAndAppend('a', td, {
+			href: repo.html_url,
+			text: repo.name,
+			target: '_blank'
+		});
+		//Description
+		tr = createAndAppend('tr', table);
+		createAndAppend('th', tr, { text: 'Description:' });
+		td = createAndAppend('td', tr, { text: repo.description });
+		//Forks
+		tr = createAndAppend('tr', table);
+		createAndAppend('th', tr, { text: 'Forks:' });
+		td = createAndAppend('td', tr, { text: repo.forks });
+		//Updated
+		tr = createAndAppend('tr', table);
+		createAndAppend('th', tr, { text: 'Updated:' });
+		td = createAndAppend('td', tr, { text: repo.updated_at });
+	}
+
+	function main(url) {
+		createAndAppend('header', root, { text: 'HYF Repositories' });
+		fetchJSON(url, (err, repos) => {
+			const root = document.getElementById('root');
+			if (err) {
+				createAndAppend('div', root, {
+					text: err.message,
+					class: 'alert-error'
+				});
+				return;
+			}
+
+			const ul = createAndAppend('ul', root);
+			// .sort and .localeCompare used then .forEach
+			repos.sort((a, b) => a.name.localeCompare(b.name)).forEach((repo) => renderRepoDetails(repo, ul));
+		});
+	}
+
+	const HYF_REPOS_URL = 'https://api.github.com/orgs/HackYourFuture/repos?per_page=100';
+
+	window.onload = () => main(HYF_REPOS_URL);
 }
