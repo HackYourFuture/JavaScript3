ef25bfc6030f262a83a17f41fd535bebb8d4dad9
Mahmoud Badran <mahmoudbd@gmail.com>
2020-02-10 04:23:12 +0100

    Homework-classes

--- homework-classes/ContributorsView.js
+++ homework-classes/ContributorsView.js
@@ -1,28 +1,62 @@
 'use strict';
 
 {
-  const { createAndAppend } = window.Util;
+	const { createAndAppend } = window.Util;
 
-  class ContributorsView {
-    constructor(container) {
-      this.container = container;
-    }
+	class ContributorsView {
+		constructor(container) {
+			this.container = container;
+		}
 
-    update(state) {
-      if (!state.error) {
-        this.render(state.contributors);
-      }
-    }
+		update(state) {
+			if (!state.error) {
+				this.render(state.contributors);
+			}
+		}
 
-    /**
+		/**
      * Renders the list of contributors
      * @param {Object[]} contributors An array of contributor objects
      */
-    render(contributors) {
-      // TODO: replace this comment and the console.log with your own code
-      console.log('ContributorsView', contributors);
-    }
-  }
+		render(contributors) {
+			// TODO: replace this comment and the console.log with your own code
+			this.container.innerHTML = '';
+			contributors.forEach((ele) => {
+				const mainDiv = document.querySelector('.contributors-container');
+				console.log(mainDiv);
 
-  window.ContributorsView = ContributorsView;
+				const contributorsDiv = createAndAppend('div', mainDiv, {
+					class: 'contributorsDiv'
+				});
+
+				const contArray = [ ele.avatar_url, ele.login, ele.contributions ];
+
+				const div = createAndAppend('div', contributorsDiv, {
+					class: 'ImgDiv'
+				});
+				const img = createAndAppend('img', div);
+
+				img.src = contArray[0];
+
+				const loginDiv = createAndAppend('div', contributorsDiv, {
+					class: 'login'
+				});
+
+				createAndAppend('a', loginDiv, {
+					text: contArray[1],
+					href: ele.html_url,
+					target: '_blank'
+				});
+
+				const buttonDiv = createAndAppend('div', contributorsDiv, {
+					class: 'buttonDiv'
+				});
+				createAndAppend('button', buttonDiv, { text: contArray[2] });
+			});
+
+			console.log('ContributorsView', contributors);
+		}
+	}
+
+	window.ContributorsView = ContributorsView;
 }
--- homework-classes/RepoView.js
+++ homework-classes/RepoView.js
@@ -1,28 +1,51 @@
 'use strict';
 
 {
-  const { createAndAppend } = window.Util;
+	const { createAndAppend } = window.Util;
 
-  class RepoView {
-    constructor(container) {
-      this.container = container;
-    }
+	class RepoView {
+		constructor(container) {
+			this.container = container;
+		}
 
-    update(state) {
-      if (!state.error) {
-        this.render(state.selectedRepo);
-      }
-    }
+		update(state) {
+			if (!state.error) {
+				this.render(state.selectedRepo);
+			}
+		}
 
-    /**
+		/**
      * Renders the repository details.
      * @param {Object} repo A repository object.
      */
-    render(repo) {
-      // TODO: replace this comment and the console.log with your own code
-      console.log('RepoView', repo);
-    }
-  }
+		render(repo) {
+			// TODO: replace this comment and the console.log with your own code
+			this.container.innerHTML = '';
 
-  window.RepoView = RepoView;
+			const table = createAndAppend('table', this.container);
+
+			table.innerHTML += `
+			  <tr>
+				<td> <strong> Repository: </td> </strong>
+				<td><a href='${repo.html_url}' target='_blank'>${repo.name}</a></td>
+			  </tr>
+			  <tr>
+				<td> <strong>Description: </td> </strong>
+				<td>${repo.description}</td>
+			  </tr>
+			  <tr>
+				<td> <strong> Forks:</td> <strong>
+				<td>${repo.forks}</td>
+			  </tr>
+			  <tr>
+				<td> <strong> Updated:</td> <strong>
+				<td>${repo.updated_at}</td>
+			  </tr>
+			`;
+
+			console.log('RepoView', repo);
+		}
+	}
+
+	window.RepoView = RepoView;
 }
