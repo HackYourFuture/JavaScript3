b8f064afc1677beb4606ef2db8ce1a62c966cce9
ismaiel <ismaiel.nab@gmail.com>
2020-09-17 13:07:09 +0200

    do the first exercise

---
+++ Week3/homework/js-exercises/promiseMeToWait.js
@@ -0,0 +1,40 @@
+// Exercise A
+// async function getData(url) {
+//   let response = await fetch(url);
+//   let json = await response.json();
+//   try {
+//     json = json.image;
+//   } catch (err) {
+//     console.log('hello ' + error);
+//   }
+//   return json;
+// }
+
+// getData('https://randomfox.ca/floof/').then(json => console.log(json));
+
+// Exercise B
+// const arrayOfWords = ['cucumber', 'tomatos', 'avocado'];
+
+// const makeAllCaps = array => {
+//   return new Promise((resolve, reject) => {
+//     let capsArray = array.map(word => {
+//       if (typeof word === 'string') {
+//         return word.toUpperCase();
+//       } else {
+//         reject('Error: Not all items in the array are strings!');
+//       }
+//     });
+//     resolve(capsArray);
+//   });
+// };
+
+// async function toPrint() {
+//   let response = await makeAllCaps(arrayOfWords);
+//   try {
+//     console.log(response);
+//   } catch (error) {
+//     console.log('there is an error' + error);
+//   }
+// }
+
+// toPrint();
