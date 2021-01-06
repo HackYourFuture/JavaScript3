d3197d985d9be56b0bdde05fdae6f7b01312c080
Nizami Mursudlu <nizamimursudlu@gmail.com>
2021-01-06 13:08:32 +0100

    rewrote all html in js for the project

--- Week2/homework/pokemon-app/script.js
+++ Week2/homework/pokemon-app/script.js
@@ -1,8 +1,30 @@
+
 function main() {
+
+  function addPokemonToDOM() {
+    const buttonElement = document.createElement("button");
+    document.body.appendChild(buttonElement);
+    buttonElement.id = "button";
+    buttonElement.innerHTML = "GetPokemon!";
+
+    const formElement = document.createElement("form");
+    document.body.appendChild(formElement);
+
+    const selectElement = document.createElement("select");
+    formElement.appendChild(selectElement);
+    selectElement.id = "select";
+    selectElement.setAttribute("onchange", "fetchImage()");
+
+    const divElement = document.createElement("div");
+    document.body.appendChild(divElement);
+    divElement.id = "div";
+  }
+  addPokemonToDOM()
+
   const select = document.getElementById("select");
   const url = "https://pokeapi.co/api/v2/pokemon/?limit=151"
   const button = document.getElementById("button");
-
+  const div = document.getElementById("div")
   img = document.createElement("img")
   button.addEventListener('click',
 
@@ -16,6 +38,7 @@ function main() {
             select.appendChild(option)
             option.value = element.name
             option.innerHTML = element.name
+
           })
         });
     })
@@ -23,16 +46,19 @@ function main() {
 
 function fetchImage() {
   fetch(`https://pokeapi.co/api/v2/pokemon/${select.value}`)
+
     .then(res1 => res1.json())
     .then(function (imageData) {
-      const div = document.getElementById("div")
       div.appendChild(img)
       img.src = imageData.sprites.front_default
-      img.style.width = "250px"
     })
-
 }
 
-
+// HELLO I DID GET THE GOOD OUTCOME,
+// BUT I DONT THINK I MEET ALL THE REQUIREMENTS OF THE EXERSISE
+// <select id="button" onchange="fetchImage"()"></select>
+// DOESN'T WORK INSIDE OF THE MAIN() FUNCTION
+// IN CASE THAT HAS TO BE FIX, I HAVE NO IDEA HOW... :))))
+// PLAESE GIVE ME A HIT!!!   
 
 
